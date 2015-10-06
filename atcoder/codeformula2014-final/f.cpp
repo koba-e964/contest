8ec2bf5a91b4fe2837624d62f9bcf4bb028d4b97
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2015-10-06 17:24:43 +0900

    Add f.cpp

--- atcoder/codeformula2014-final/f.cpp
+++ atcoder/codeformula2014-final/f.cpp
@@ -1,22 +1,54 @@
-#include <algorithm>
 #include <cassert>
 #include <cmath>
-#include <cstdio>
 #include <cstdlib>
 #include <iostream>
-#include <map>
-#include <set>
-#include <string>
 #include <vector>
 
 #define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
 
 using namespace std;
-typedef long long int ll;
-const double EPS=1e-9;
 
+
+vector<pair<int, int> > gacha(void) {
+	const int W = 1500;
+	const int N = 100;
+	const int TH = 100;
+	vector<pair<int, int> > res(N);
+	for (int i = N; i >= 1; --i) {
+		bool found = 0;
+		REP(t, 0, TH) {
+			int x = rand() % (W - 2 * i + 1) + i;
+			int y = rand() % (W - 2 * i + 1) + i;
+			bool ok = 1;
+			REP(j, i, N) {
+				int dist = pow(x - res[j].first, 2) + pow(y - res[j].second, 2);
+				if (dist < pow(i + j + 1, 2)) { ok = 0; break; }
+			}
+			if (ok) {
+				found = 1;
+				res[i - 1].first = x;
+				res[i - 1].second = y;
+				break;
+			}
+		}
+		if (! found) {
+			return vector<pair<int, int> >();
+		}
+	}
+	return res;
+}
 
 
 int main(void){
-	
+	srand(0xdeadc0de);
+	REP(loop_cnt, 0, 1000) {
+		vector<pair<int, int> > res = gacha();
+		if (res.size() > 0) {
+			REP(i, 0, 100) {
+				cout << res[i].first << " " << res[i].second << endl;
+			}
+			return 0;
+		}
+	}
+	assert (!"__unreachable__");
 }
