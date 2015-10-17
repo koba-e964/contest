3962af1b9287da43df90a74127ccad0e1b15ec9d
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2015-10-17 12:36:41 +0900

    Add atcoder/arc045/{b,c}

---
+++ atcoder/arc045/b.cpp
@@ -0,0 +1,135 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+/**
+ * Segment Tree. This data structure is useful for fast folding on intervals of an array
+ * whose elements are elements of monoid M. Note that constructing this tree requires the identity
+ * element of M and the operation of M.
+ * Header requirement: vector, algorithm
+ */
+template<class I, class BiOp = I (*) (I, I)>
+class SegTree {
+  int n;
+  std::vector<I> dat;
+  BiOp op;
+  I e;
+public:
+  SegTree(int n_, BiOp op, I e) : op(op), e(e) {
+    n = 1;
+    while (n < n_) { n *= 2; } // n is a power of 2
+    dat.resize(2 * n);
+    for (int i = 0; i < 2 * n - 1; i++) {
+      dat[i] = e;
+    }
+  }
+  /* ary[k] <- v */
+  void update(int k, I v) {
+    k += n - 1;
+    dat[k] = v;
+    while (k > 0) {
+      k = (k - 1) / 2;
+      dat[k] = op(dat[2 * k + 1], dat[2 * k + 2]);
+    }
+  }
+  void update_array(int k, int len, const I *vals) {
+    for (int i = 0; i < len; ++i) {
+      update(k + i, vals[i]);
+    }
+  }
+  /*
+    Updates all elements. O(n)
+   */
+  void update_all(const I *vals, int len) {
+    for (int k = 0; k < std::min(n, len); ++k) {
+      dat[k + n - 1] = vals[k];
+    }
+    for (int k = std::min(n, len); k < n; ++k) {
+      dat[k + n - 1] = e;
+    }
+    for (int b = n / 2; b >= 1; b /= 2) {
+      for (int k = 0; k < b; ++k) {
+	dat[k + b - 1] = op(dat[k * 2 + b * 2 - 1], dat[k * 2 + b * 2]);
+      }
+    }
+  }
+  /* l,r are for simplicity */
+  I querySub(int a, int b, int k, int l, int r) const {
+    // [a,b) and  [l,r) intersects?
+    if (r <= a || b <= l) return e;
+    if (a <= l && r <= b) return dat[k];
+    I vl = querySub(a, b, 2 * k + 1, l, (l + r) / 2);
+    I vr = querySub(a, b, 2 * k + 2, (l + r) / 2, r);
+    return op(vl, vr);
+  }
+  /* [a, b] (note: inclusive) */
+  I query(int a, int b) const {
+    return querySub(a, b + 1, 0, 0, n);
+  }
+};
+
+int mymin(int x,int y) { return std::min(x, y);}
+
+
+#define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
+
+using namespace std;
+typedef long long int ll;
+typedef vector<int> VI;
+typedef pair<int, int> PI;
+const double EPS=1e-9;
+
+const int M = 1e5 + 10;
+int s[M],t[M];
+
+const int N = 3e5 + 10;
+
+int main(void){
+  int n, m;
+  cin >> n >> m;
+  VI b(N);
+  REP(i, 0, m) {
+    cin >> s[i] >> t[i];
+    s[i]--;
+    t[i]--;
+    b[s[i]]++;
+    if (t[i] < N - 1) {
+      b[t[i] + 1]--;
+    }
+  }
+  REP(i, 1, N) {
+    b[i] += b[i - 1];
+  }
+  SegTree<int> st(N, mymin, N * 10);
+  st.update_all(&b[0], N);
+  VI dat;
+  REP(i, 0, m) {
+    if (st.query(s[i], t[i]) >= 2) {
+      dat.push_back(i + 1);
+    }
+  }
+  cout << dat.size() << endl;
+  REP(i, 0, dat.size()) {
+    cout << dat[i] << endl;
+  }
+}
