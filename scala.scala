f4714e5c7a49f2d568fe65bd99b56e697a028d1d
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2015-12-01 12:42:19 +0900

    Add contest template for Scala language

---
+++ scala.scala
@@ -0,0 +1,15 @@
+/*
+ * Reference: http://lizan.asia/blog/2012/12/11/scala-competitive/
+ */
+
+object Main extends App {
+  import java.{util => ju}
+  import scala.annotation._
+  import scala.collection._
+  import scala.collection.{mutable => mu}
+  import scala.collection.JavaConverters._
+  import scala.math._
+
+  val sc = new ju.Scanner(System.in)
+
+}
