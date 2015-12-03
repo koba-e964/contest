87eb4bb4174f967fbf991b38e0a071c54fa3ee51
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2015-12-04 03:18:31 +0900

    Enhance 294.scala

--- yukicoder/294.scala
+++ yukicoder/294.scala
@@ -11,26 +11,17 @@ object Main extends App {
   import scala.math._
 
   val sc = new ju.Scanner(System.in)
-  def calc(n : Int) : (Long, Int) = {
-    var t : Long = 1
-    var l : Int = 1
-    var rem : Int = n
-    while (true) {
-      if (t >>> l != 0) {
-        t = 1
-        l += 1
-      } else if (java.lang.Long.bitCount(t) % 3 != 0) {
-        t += 2
-      } else if (rem == 0) return (t, l)
-      else {
-        t += 2
-        rem -= 1
-      }
+  @tailrec
+  def calc(t : Long, l : Int, rem : Int) : (Long, Int) = {
+    () match {
+      case _ if (t >>> l != 0) => calc(1, l + 1, rem)
+      case _ if (java.lang.Long.bitCount(t) % 3 != 0) => calc(t + 2, l, rem)
+      case _ if (rem == 0) => (t, l)
+      case _ => calc(t + 2, l, rem - 1)
     }
-    return (0,0)
   }
   val n = sc.nextInt
-  val (res, len) = calc(n - 1)
+  val (res, len) = calc(1, 1, n - 1)
   val str = (0 until len).map(i => if ((res & (1 << (len - i - 1))) != 0) "5" else "3").mkString
   println(str)
 }
