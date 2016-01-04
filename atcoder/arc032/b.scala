1566368e4acb90c710d8f8168d9091082b14fefb
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2016-01-04 17:14:47 +0900

    Add arc032/b

--- atcoder/arc032/b.scala
+++ atcoder/arc032/b.scala
@@ -18,3 +18,24 @@ class UnionFind(n: Int) {
     disj.toArray
   }
 }
+/*
+ * Reference: http://lizan.asia/blog/2012/12/11/scala-competitive/
+ */
+
+object Main extends App {
+  import java.{util => ju}
+  import scala.annotation._
+  import scala.collection._
+  import scala.collection.{mutable => mu}
+  import scala.collection.JavaConverters._
+  import scala.math._
+
+  val sc = new ju.Scanner(System.in)
+  val n, m = sc.nextInt
+  val uf = new UnionFind(n)
+  for (_ <- 0 until m) {
+    val a, b = sc.nextInt - 1
+    uf.unite(a, b)
+  }
+  println(uf.toArray.toSet.size - 1)
+}
