0755e8e73cea464cedf49a5454485f9b413d7cb9
koba-e964 <koba_max@kobas-Air.pohe>
2016-03-10 01:28:02 +0900

    Update Rust template file

---
+++ rust.rs
@@ -0,0 +1,17 @@
+use std::cmp::*;
+fn getline() -> String {
+    let mut ret = String::new();
+    std::io::stdin().read_line(&mut ret).ok();
+    return ret;
+}
+
+fn parse<T : std::str::FromStr>(s : &str) -> T {
+    match s.parse::<T>() {
+        Ok(t) => t,
+        _    => panic!(),
+    }
+}
+
+fn main() {
+    
+}
