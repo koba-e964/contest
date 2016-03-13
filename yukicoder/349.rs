664edfafe60efd9e76de8a3292f0f554c65fadc5
koba-e964 <koba_max@kobas-Air.pohe>
2016-03-13 15:23:06 +0900

    Add yukicoder/349.rs

---
+++ yukicoder/349.rs
@@ -0,0 +1,28 @@
+use std::cmp::*;
+use std::collections::*;
+fn getline() -> String {
+    let mut ret = String::new();
+    std::io::stdin().read_line(&mut ret).ok();
+    return ret;
+}
+
+fn parse<T : std::str::FromStr>(s : &str) -> T {
+    match s.parse::<T>() {
+        Ok(t) => t,
+        _    => panic!(),
+    }
+}
+
+fn main() {
+    let n = parse(getline().trim());
+    let mut hm: HashMap<String, i32, _> = HashMap::new();
+    for _ in 0 .. n {
+        let a: String = String::from(getline().trim());
+        if let Some(x) = hm.clone().get(&a) { // TODO ugly cloning. 
+            hm.insert(a, x + 1);
+        } else {
+            hm.insert(a, 1);
+        }
+    }
+    println!("{}", if hm.values().all(|&x| x <= (n + 1) / 2) { "YES" } else { "NO" });
+}
