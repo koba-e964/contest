cf9c05adca45298ec36300244ac60b74d5639926
koba-e964 <koba_max@kobas-Air.pohe>
2016-07-18 17:13:16 +0900

    Add agc001/{b,c}.rs

---
+++ atcoder/agc001/c.rs
@@ -0,0 +1,71 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::*;
+#[allow(dead_code)]
+fn getline() -> String {
+    let mut ret = String::new();
+    std::io::stdin().read_line(&mut ret).ok();
+    return ret;
+}
+fn get_word() -> String {
+    let mut stdin = std::io::stdin();
+    let mut u8b: [u8; 1] = [0];
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(16);
+        loop {
+            let res = stdin.read(&mut u8b);
+            if res.is_err() || res.ok().unwrap() == 0 || u8b[0] <= ' ' as u8 {
+                break;
+            } else {
+                buf.push(u8b[0]);
+            }
+        }
+        if buf.len() >= 1 {
+            let ret = std::string::String::from_utf8(buf).unwrap();
+            return ret;
+        }
+    }
+}
+fn parse<T: std::str::FromStr>(s: &str) -> T { s.parse::<T>().ok().unwrap() }
+
+#[allow(dead_code)]
+fn get<T: std::str::FromStr>() -> T { parse(&get_word()) }
+
+fn dfs(adj: &[Vec<usize>], x: usize, y: usize, r: usize) -> usize {
+    let mut sum = 0;
+    if r > 0 {
+        for v in &adj[x] {
+            if *v != y {
+                sum += dfs(adj, *v, x, r - 1);
+            }
+        }
+    }
+    return sum + 1;
+}
+
+fn main() {
+    let n: usize = get();
+    let k: usize = get();
+    let mut e: Vec<(usize, usize)> = Vec::new();
+    let mut adj: Vec<Vec<usize>> = vec![Vec::new(); n];
+    for _ in 0 .. n - 1 {
+        let x: usize = get();
+        let y: usize = get();
+        e.push((x - 1, y - 1));
+        adj[x - 1].push(y - 1);
+        adj[y - 1].push(x - 1);
+    }
+    let mut ma = 0;
+    if k % 2 == 0 {
+        for v in 0 .. n {
+            ma = max(ma, dfs(&adj, v, n, k / 2));
+        }
+    } else {
+        for (x, y) in e {
+            ma = max(ma, dfs(&adj, x, y, k / 2) + dfs(&adj, y, x, k / 2));
+        }
+    }
+    println!("{}", n - ma);
+}
