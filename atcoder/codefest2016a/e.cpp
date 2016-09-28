592fb361528e2adaa0d6b053ddb4ca3cb0ff6711
koba-e964 <koba_max@kobas-Air.pohe>
2016-09-28 20:26:29 +0900

    Add atcoder/codefest2016a/e.cpp

---
+++ atcoder/codefest2016a/e.cpp
@@ -0,0 +1,114 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
+
+using namespace std;
+typedef long long int ll;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef pair<int, int> PI;
+const ll mod = 1e9 + 7;
+const int DEBUG = 0;
+
+const int N = 100010;
+bool done[N];
+
+bool match(int n, int m, const VI &target, const VI &op, int tol) {
+  assert (0 <= tol && tol < m);
+  VI inv_t(m);
+  REP(i, 0, m) {
+    inv_t[target[i]] = i;
+  }
+  VI prog(m + 1);
+  VI pass(m + 1);
+  prog[0] = n;
+  REP(i, 0, op.size()) {
+    int idx = inv_t[op[i]];
+    if (prog[idx] == 0 && !pass[idx]) {
+      return false;
+    }
+    if (prog[idx] == 0) {
+      continue;
+    }
+    prog[idx]--;
+    prog[idx + 1]++;
+    pass[idx] = 1;
+  }
+  if (DEBUG) {
+    REP(i, 0, m + 1) {
+      cout << "prog[" << i << "]=" << prog[i] << endl;
+    }
+  }
+  REP(i, 0, tol) {
+    if (prog[i] > 0) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+int main(void){
+  int n, m, q;
+  cin >> n >> m >> q;
+  VI a(q);
+  REP(i, 0, q) {
+    cin >> a[i];
+    a[i]--;
+  }
+  reverse(a.begin(), a.end());
+  vector<bool> used(m);
+  int cur = 0;
+  VI target(m);
+  REP(i, 0, q) {
+    if (used[a[i]]) {
+      continue;
+    }
+    used[a[i]] = 1;
+    target[cur] = a[i];
+    cur++;
+  }
+  REP(i, 0, m) {
+    if (!used[i]) {
+      target[cur++] = i;
+    }
+  }
+  assert (cur == m);
+  int oldcur = m - 1;
+  while (oldcur > 0) {
+    if (target[oldcur - 1] < target[oldcur]) {
+      oldcur--;
+    } else {
+      break;
+    }
+  }
+  if (DEBUG) {
+    cerr << "target:";
+    REP(i, 0, m) {
+      cerr << " " << target[i];
+    }
+    cerr << endl << "tol = " << oldcur << endl;
+  }
+  cout << (match(n, m, target, a, oldcur) ? "Yes" : "No") << endl;
+}
