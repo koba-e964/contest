4ccb3cd4abeced481243ef47b0b056e3572b6b88
koba-e964 <koba_max@kobas-Air.pohe>
2016-11-02 16:33:19 +0900

    Add atcoder/arc023/d.cpp, comm/SparseTable.cpp

--- comm/SparseTable.cpp
+++ comm/SparseTable.cpp
@@ -1,9 +1,8 @@
-#include <vector>
-#include <cassert>
 /*
  * Sparse Table.
  * BiOp should be the type of a binary operator which is
  * associative, commutative and idempotent.
+ * (For example, min and gcd satisfy them.)
  * Header Requirement: vector, cassert
  * Verified by: AtCoder ARC023 D (http://arc023.contest.atcoder.jp/submissions/960757)
  */
@@ -30,6 +29,7 @@ private:
   }
   /*
    * Reference: https://graphics.stanford.edu/~seander/bithacks.html#IntegerLogFloat
+   * Please be aware that it only works well in case of 1 <= t <= 2^24.
    */
   static int top_bit(int t) {
     const float v = t; // find int(log2(v)), where v > 0.0 && finite(v) && isnormal(v)
@@ -55,75 +55,3 @@ public:
     return biop(st[diff][f], st[diff][s + 1 - (1 << diff)]);
   }
 };
-
-#include <iostream>
-#include <map>
-
-#define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
-
-using namespace std;
-typedef long long int ll;
-const double EPS=1e-9;
-
-const int N=100000;
-int n,m;
-int x[N];
-inline int gcd_core(int x, int y) {
-  while (y != 0) {
-    int r = x % y;
-    x = y;
-    y = r;
-  }
-  return x;
-}
-struct gcd {
-  int operator()(int x, int y) const {
-    return gcd_core(x, y);
-  }
-};
-
-inline void add(map<int, ll> &freq, int val, int f) {
-  if (freq.count(val)) {
-    freq[val] += f;
-    return;
-  }
-  freq[val] = f;
-}
-
-int main(void){
-  cin>>n>>m;
-  vector<int> a(n);
-  REP(i,0,n){
-    cin>>a[i];
-  }
-  REP(i,0,m){
-    cin>>x[i];
-  }
-  SparseTable<int, gcd> sp_tbl(gcd(), a);
-  map<int, ll> freq;
-  REP(i, 0, n - 1) { // [i, j] for i < j
-    int pos = i + 1;
-    int cur = gcd_core(a[i], a[i + 1]);
-    // cur == sp_tbl.query(i, pos);
-    while (pos < n) {
-      int lo = pos, hi = n;
-      while (hi - lo > 1) {
-	int mid = (hi + lo) / 2;
-	if (sp_tbl.query(i, mid) == cur) {
-	  lo = mid;
-	} else {
-	  hi = mid;
-	}
-      }
-      add(freq, cur, hi - pos);
-      pos = hi;
-      cur = gcd_core(cur, a[hi]);
-    }
-  }
-  REP(i, 0, n) {
-    add(freq, a[i], 1); // [i, i]
-  }
-  REP(i, 0, m) {
-    cout << freq[x[i]] << endl;
-  }
-}
