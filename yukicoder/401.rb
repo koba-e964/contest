5f079797b86a4a7760d701f1f356c22ebe53e56e
koba-e964 <koba_max@kobas-Air.pohe>
2016-11-08 11:43:34 +0900

    Add yukicoder/401.rb

--- yukicoder/401.rb
+++ yukicoder/401.rb
@@ -1,9 +1,33 @@
-n=$<.read.to_i
-for i in 0..n-1
-  for j in 1..n
-    x=if i < n/2 then i else n-1-i end
-    printf("%03d",x)
-    if j<n then print " " end
+n=gets.to_i
+ary = Array.new(n){|v| Array.new(n)}
+cnt = 1
+for i in 0..(n + 1)/2
+  ulim = n - 1 - i
+  # [i, ulim] * [i, ulim]
+  for j in i .. ulim
+    ary[i][j] = cnt
+    cnt += 1
+  end
+  for j in (i + 1) .. ulim
+    ary[j][ulim] = cnt
+    cnt += 1
+  end
+  for j in (i .. (ulim - 1)).to_a.reverse
+    ary[ulim][j] = cnt
+    cnt += 1
+  end
+  for j in ((i + 1) .. (ulim - 1)).to_a.reverse
+    ary[j][i] = cnt
+    cnt += 1
+  end
+end
+for i in 0 ... n
+  for j in 0 ... n
+    printf "%03d", ary[i][j]
+    if j != n - 1
+      print " "
+    end
   end
   puts
 end
+
