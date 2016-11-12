c03db30d66467b3327042d000e557d2dc2c65ce6
koba-e964 <koba_max@kobas-Air.pohe>
2016-11-12 17:58:53 +0900

    Shorten yukicoder/444.cpp

--- yukicoder/444.cpp
+++ yukicoder/444.cpp
@@ -1,34 +1,11 @@
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <cctype>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <deque>
-#include <functional>
-#include <iomanip>
 #include <iostream>
-#include <list>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <string>
-#include <utility>
 #include <vector>
 
 #define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
 
 using namespace std;
 typedef long long int ll;
-typedef vector<int> VI;
 typedef vector<ll> VL;
-typedef pair<int, int> PI;
 const ll mod = 1e9 + 7;
 
 vector<VL> mul(const vector<VL> &A, const vector<VL> &B) {
@@ -89,8 +66,7 @@ int main(void){
   }
   ll res = 0;
   REP(i, 0, n) {
-    res = (res + sum[0][i]) % mod;
-    res = (res - powmod(a[i], oldc) + mod) % mod;
+    res = (res + sum[0][i] - powmod(a[i], oldc) + mod) % mod;
   }
   cout << res << endl;
 }
