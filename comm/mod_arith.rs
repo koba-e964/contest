c2d9fa7e1c103ff2324290b9eaf548f7b405212d
koba-e964 <koba_max@kobas-Air.pohe>
2017-02-03 02:56:34 +0900

    Add function modsqrt to comm/mod_arith.rs

--- comm/mod_arith.rs
+++ comm/mod_arith.rs
@@ -12,3 +12,51 @@ fn powmod(x: i64, mut e: i64, m: i64) -> i64 {
     }
     sum
 }
+
+/**
+ * Calculates x s.t. x^2 = a (mod p)
+ * p is prime
+ * Verified by: CF #395 Div1-C
+ *              (http://codeforces.com/contest/763/submission/24380573)
+ */
+fn modsqrt(mut a: i64, p: i64) -> Option<i64> {
+    a %= p;
+    if a == 0 {
+        return Some(0);
+    }
+    if p == 2 {
+        return Some(a);
+    }
+    if powmod(a, (p - 1) / 2, p) != 1 {
+        return None;
+    }
+    let mut b = 1;
+    while powmod(b, (p - 1) / 2, p) == 1 {
+        b += 1;
+    }
+    let mut e = 0;
+    let mut m = p - 1;
+    while m % 2 == 0 {
+        m /= 2;
+        e += 1;
+    }
+    let mut x = powmod(a, (m - 1) / 2, p);
+    let mut y = a * (x * x % p) % p;
+    x = x * a % p;
+    let mut z = powmod(b, m, p);
+    while y != 1 {
+        let mut j = 0;
+        let mut t = y;
+        while t != 1 {
+            j += 1;
+            t = t * t % p;
+        }
+        assert!(j < e);
+        z = powmod(z, 1 << (e - j - 1), p);
+        x = x * z % p;
+        z = z * z % p;
+        y = y * z % p;
+        e = j;
+    }
+    Some(x)
+}
