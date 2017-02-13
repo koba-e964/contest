6dfc1974ea8507d014f48a3166d1264cae51dc99
koba-e964 <koba_max@kobas-Air.pohe>
2017-02-13 16:54:35 +0900

    Fix atcoder/chokudai002/a.cpp (score = 38152)

--- atcoder/chokudai002/a.cpp
+++ atcoder/chokudai002/a.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <set>
 #include <cassert>
 #include <cmath>
 #include <iostream>
@@ -11,103 +12,103 @@ typedef long long int ll;
 typedef vector<int> VI;
 typedef vector<ll> VL;
 typedef pair<int, int> PI;
-const ll mod = 1e9 + 7;
 
 const int lim = 1e9;
 
 
-const int PT = 100000;
-int prime[PT];
-
-const int Q = 9;
-
-struct state {
-  VI del;
-  int val;
-  int nf;
-  ll score;
-  bool operator < (const state &o) const {
-    return score < o.score;
+void output(const vector<int> &result) {
+  assert (result.size() == 100);
+  REP(i, 0, 100) {
+    assert (result[i] >= 1);
+    assert (result[i] <= lim);
   }
-};
-
-
-
-PI beam_search(int q) {
-  int fact[Q] = {2,3,5,7,11,13,17,19,23};
-  VI init_del = VI(Q, 1);
-  vector<state> que;
-  vector<state> next;
-  state mast = {VI(Q, 0), 0, 0, 0};
-  que.push_back((state) {init_del, 1, 1, 1});
-  int gen = 0;
-  while (not que.empty()) {
-    //cerr << "generation: " << gen << endl;
-    gen++;
-    REP(i, 0, que.size()) {
-      next.clear();
-      state st = que[i];
-      //cerr << "q = " << q << ", st.val=" << st.val << ", st.nf=" << st.nf << endl;
-      if (st.nf > mast.nf) {
-	mast = st;
-      }
-      double ls = 0;
-      REP(i, 0, Q) {
-	ls += exp(-pow(st.del[i] - 1, 2));
-      }
-      REP(i, 0, Q) {
-	int tmp = 100000 / log(fact[i]) * log(1 + 1.0 / st.del[i]);
-	if (q >= (ll) st.val * fact[i]) {
-	  state nexst = {st.del, st.val, st.nf, st.score};
-	  nexst.val *= fact[i];
-	  nexst.nf *= 1 + st.del[i];
-	  nexst.nf /= st.del[i];
-	  nexst.del[i]++;
-	  nexst.score += ls * tmp;
-	  next.push_back(nexst);
+#ifndef DEBUG
+  REP(i, 0, 100) {
+    cout << result[i] << endl;
+  }
+#else
+  {
+    set<int> factors;
+    REP(i, 0, 100) {
+      int tmp = factors.size();
+      int v = result[i];
+      for (int k = 1; k * k <= v; ++k) {
+	if (v % k == 0) {
+	  factors.insert(k);
+	  factors.insert(v / k);
 	}
       }
     }
-    sort(next.rbegin(), next.rend());
-    // retain 10 best ones
-    que = vector<state> (next.begin(), next.begin() + min(10, (int)next.size()));
+    cout << "score = " << factors.size() << endl;
   }
-  return PI(mast.nf, mast.val);
+#endif
+} 
+ 
+const int PT = 100000;
+int prime[PT];
+ 
+int monetize(int v) {
+  const int Q = 9;
+  int fact[Q] = {2,3,5,7,11,13,17,19,23};
+  int del[Q] = {1,1,1,1,1,1,1,1,1};
+  ll k = v;
+  //ll k = v * 32 * 9 * 5 * 7 * 11 * 13;
+  while (1) {
+    int q = lim / k;
+    int ma = 0, maxi = -1;
+    REP(i, 0, Q) {
+      int tmp = 100000 / log(fact[i]) / del[i];
+      if (q >= fact[i] && ma < tmp) {
+	ma = tmp;
+	maxi = i;
+      }
+    }
+    if (maxi == -1) break;
+    k *= fact[maxi];
+    del[maxi]++;
+  }
+  return k;
 }
-
-PI monetize(int v) {
-  VI del(Q, 1);
-  int q = lim / v;
-  PI result = beam_search(q);
-  return PI(result.first, v * result.second);
-}
-
+ 
 int main(void){
   REP(i, 2, PT) {
     prime[i] = 1;
   }
-
+ 
   REP(i, 2, sqrt(PT) + 1) {
     if (not prime[i]) { continue; }
     REP(j, 2, PT / i) {
       prime[i * j] = 0;
     }
   }
+  VI small_pr;
+  REP(p, 7, 38) {
+    if (prime[p]) small_pr.push_back(p);
+  }
+  int spr_size = small_pr.size();
   VI med_pr;
-  REP(i, 29, 1000) {
+  REP(i, small_pr[spr_size - 1] + 1, 1000) {
     if (prime[i]) med_pr.push_back(i);
   }
+  REP(i, 13, 1000) {
+    REP(j, 23, 1000) {
+      if (prime[i] && prime[j]) {
+	med_pr.push_back(i * j);
+      }
+    }
+  }
   sort(med_pr.begin(), med_pr.end());
-  assert (med_pr.size() >= 100);
-  vector<PI> result(med_pr.size());
-  REP(i, 0, med_pr.size()) {
-    result[i] = monetize(med_pr[i]);
+  assert (med_pr.size() >= 100 - spr_size);
+  VI result;
+  REP(i, 0, spr_size) {
+    result.push_back(monetize(64 * 27 * 25 * 7 * 11 * small_pr[i]));
   }
-  sort(result.rbegin(), result.rend());
-  REP(i, 0, 100) {
-    //cout << "seed=" << med_pr[i] << endl;
-    cout << result[i].second <<
-      " " << result[i].first <<
-      endl;
+  REP(i, 0, 100 - spr_size) {
+    result.push_back(monetize(med_pr[i]));
   }
+#ifdef DEBUG
+  cout << "small_pr.size() = " << small_pr.size() << endl;
+  cout << "med_pr.size() = " << med_pr.size() << endl;
+#endif
+  output(result);
 }
