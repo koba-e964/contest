32514819369cf2e25d0614f3bb2d9382de9f731a
koba-e964 <koba_max@kobas-Air.pohe>
2017-04-24 23:26:44 +0900

    Add codeforces/343/2d.cpp

---
+++ codeforces/343/2d.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <cstdio>
+using namespace std;
+  
+#define rep(i, n) for (int i = 0; i < int(n); ++i)
+typedef long long i64;
+/**
+ * Segment Tree. This data structure is useful for fast folding on intervals of an array
+ * whose elements are elements of monoid M. Note that constructing this tree requires the identity
+ * element of M and the operation of M.
+ * Header requirement: vector, algorithm
+ * Verified by AtCoder ABC017-D (http://abc017.contest.atcoder.jp/submissions/660402)
+ */
+template<class I, class BiOp = I (*) (I, I)>
+class SegTree {
+  int n;
+  std::vector<I> dat;
+  BiOp op;
+  I e;
+public:
+  SegTree(int n_, BiOp op, I e) : op(op), e(e) {
+    n = 1;
+    while (n < n_) { n *= 2; } // n is a power of 2
+    dat.resize(2 * n);
+    for (int i = 0; i < 2 * n - 1; i++) {
+      dat[i] = e;
+    }
+  }
+  /* ary[k] <- v */
+  void update(int k, I v) {
+    k += n - 1;
+    dat[k] = v;
+    while (k > 0) {
+      k = (k - 1) / 2;
+      dat[k] = op(dat[2 * k + 1], dat[2 * k + 2]);
+    }
+  }
+  void update_array(int k, int len, const I *vals) {
+    for (int i = 0; i < len; ++i) {
+      update(k + i, vals[i]);
+    }
+  }
+  /*
+    Updates all elements. O(n)
+   */
+  void update_all(const I *vals, int len) {
+    for (int k = 0; k < std::min(n, len); ++k) {
+      dat[k + n - 1] = vals[k];
+    }
+    for (int k = std::min(n, len); k < n; ++k) {
+      dat[k + n - 1] = e;
+    }
+    for (int b = n / 2; b >= 1; b /= 2) {
+      for (int k = 0; k < b; ++k) {
+	dat[k + b - 1] = op(dat[k * 2 + b * 2 - 1], dat[k * 2 + b * 2]);
+      }
+    }
+  }
+  /* l,r are for simplicity */
+  I querySub(int a, int b, int k, int l, int r) const {
+    // [a,b) and  [l,r) intersects?
+    if (r <= a || b <= l) return e;
+    if (a <= l && r <= b) return dat[k];
+    I vl = querySub(a, b, 2 * k + 1, l, (l + r) / 2);
+    I vr = querySub(a, b, 2 * k + 2, (l + r) / 2, r);
+    return op(vl, vr);
+  }
+  /* [a, b] (note: inclusive) */
+  I query(int a, int b) const {
+    return querySub(a, b + 1, 0, 0, n);
+  }
+};
+
+
+struct pmax{
+double operator()(double x, double y)const{
+return max(x, y);
+}
+};
+
+int main(void) {
+int n;cin>>n;
+vector<pair<double, int> > vol(n);
+rep(i, n) {
+double r, h;
+cin >> r >> h;
+vol[i] = make_pair(r * r * h, -i);
+}
+sort(vol.begin(), vol.end());
+SegTree<double, pmax> st(n, pmax(), 0.0);
+rep(i, n) {
+int idx = -vol[i].second;
+double t = vol[i].first;
+double res = st.query(0, idx - 1) + t;
+st.update(idx, res);
+}
+printf("%.15f\n", 3.1415926535897932384626 * st.query(0, n - 1));
+}
