f59a086766ecb3037c83266bcfea45f026064171
koba-e964 <koba_max@kobas-Air.pohe>
2017-05-19 22:56:26 +0900

    Add atcoder/indeednow-quala/a.cpp

---
+++ atcoder/indeednow-quala/a.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
+
+using namespace std;
+typedef long long int ll;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef pair<int, int> PI;
+const ll mod = 1e9 + 7;
+
+int calc(int a) {
+  int c = 0;
+  while (a > 0) {
+    a /= 10;
+    c += 1;
+  }
+  return c;
+}
+
+int main(void){
+  int a, b;
+  cin >> a >> b;
+  cout << calc(a) * calc(b) << endl;
+}
