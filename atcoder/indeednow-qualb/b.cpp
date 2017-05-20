c9500d755d829d76a911c48f279a81e1abd361ac
koba-e964 <koba_max@kobas-Air.pohe>
2017-05-20 15:11:28 +0900

    Add atcoder/indeednow-qualb/b.cpp

---
+++ atcoder/indeednow-qualb/b.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
+
+using namespace std;
+typedef long long int ll;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef pair<int, int> PI;
+const ll mod = 1e9 + 7;
+
+
+
+int main(void){
+  string s, t;
+  cin >> s >> t;
+  int n = s.length();
+  if (t.length() != n) {
+    cout << -1 << endl;
+    return 0;
+  }
+  REP(i, 0, n) {
+    if (s == t) {
+      cout << i << endl;
+      return 0;
+    }
+    s = s[n - 1] + s.substr(0, n - 1);
+  }
+  cout << -1 << endl;
+}
