6d5dc6f28d1903df9a7d3e412bca446feea79237
koba-e964 <koba_max@kobas-Air.pohe>
2017-07-03 16:37:59 +0900

    Add atcoder/mujin-pc-2016/{a.rb,c.cpp}

---
+++ atcoder/mujin-pc-2016/a.rb
@@ -0,0 +1,6 @@
+c = gets.chomp
+if 'KLOP'.include?(c)
+  puts "Right"
+else
+  puts "Left"
+end
---
+++ atcoder/mujin-pc-2016/c.cpp
@@ -0,0 +1,111 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
+
+using namespace std;
+typedef long long int ll;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef pair<int, int> PI;
+
+/*
+ * Union-Find tree
+ * header requirement: vector
+ */
+class UnionFind {
+private:
+  std::vector<int> disj;
+  std::vector<int> rank;
+public:
+  UnionFind(int n) : disj(n), rank(n) {
+    for (int i = 0; i < n; ++i) {
+      disj[i] = i;
+      rank[i] = 0;
+    }
+  }
+  int root(int x) {
+    if (disj[x] == x) {
+      return x;
+    }
+    return disj[x] = root(disj[x]);
+  }
+  void unite(int x, int y) {
+    x = root(x);
+    y = root(y);
+    if (x == y) {
+      return;
+    }
+    if (rank[x] < rank[y]) {
+      disj[x] = y;
+    } else {
+      disj[y] = x;
+      if (rank[x] == rank[y]) {
+	++rank[x];
+      }
+    }
+  }
+  bool is_same_set(int x, int y) {
+    return root(x) == root(y);
+  }
+};
+
+
+
+int main(void){
+  int n, m;
+  cin >> n >> m;
+  vector<VI> edges(n);
+  REP(i, 0, m) {
+    int x, y;
+    cin >> x >> y;
+    x--, y--;
+    edges[x].push_back(y);
+    edges[y].push_back(x);
+  }
+  // Try all possibilities to make G a bipartite graph
+  int tot = 0;
+  REP(bits, 0, 1 << n) {
+    // Construct
+    UnionFind uf(n);
+    int conn = n;
+    REP(i, 0, n) {
+      if ((bits & 1 << i) == 0) { continue; }
+      REP(j, 0, edges[i].size()) {
+	int w = edges[i][j];
+	if (bits & 1 << w) { continue; }
+	if (not uf.is_same_set(i, w)) {
+	  uf.unite(i, w);
+	  conn -= 1;
+	}
+      }
+    }
+    // Are all degrees of vertices in this subgraph >0?
+    // Is this subgraph connected?
+    if (conn == 1) {
+      tot += 1;
+    }
+  }
+  cout << tot / 2 << endl;
+}
