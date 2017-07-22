cf5575a750ac16071817195d131de6730d2dd25d
koba-e964 <koba_max@kobas-Air.pohe>
2017-07-22 21:48:31 +0900

    Add yukicoder/523.cpp

---
+++ yukicoder/523.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
+
+using namespace std;
+typedef long long int ll;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef pair<int, int> PI;
+const ll mod = 1e9 + 7;
+
+
+ll powmod(ll a, ll e) {
+  ll sum = 1;
+  ll cur = a;
+  while (e > 0) {
+    if (e % 2) {
+      sum = sum * cur % mod;
+    }
+    cur = cur * cur % mod;
+    e /= 2;
+  }
+  return sum;
+}
+
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  ll prod = 1;
+  REP(i, 1, 2 * n + 1) {
+    prod = prod * i % mod;
+  }
+  prod *= powmod(2, mod - 1 - n);
+  prod %= mod;
+  cout << prod << "\n";
+}
