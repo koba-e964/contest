1883e9b8c1f7c23e7c80fcd546f0cd980108a561
koba-e964 <koba_max@kobas-Air.pohe>
2017-07-26 13:17:37 +0900

    Add codeforces/425/2a.cpp

---
+++ codeforces/425/2a.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
+
+using namespace std;
+typedef long long int ll;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef pair<int, int> PI;
+const ll mod = 1e9 + 7;
+
+
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  ll n, k;
+  cin >> n >> k;
+  ll q = n / k;
+  cout << (q % 2 == 0 ? "NO" : "YES") << "\n";
+}
