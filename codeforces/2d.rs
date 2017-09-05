603cdda4b139d3d5b8ad71f9bb9a933038e94410
koba-e964 <koba_max@kobas-Air.pohe>
2017-09-05 16:48:23 +0900

    Add codeforces/427/2d.rs, fix comm/SuffixArray.rs

--- codeforces/2d.rs
+++ codeforces/2d.rs
@@ -1,3 +1,40 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::Read;
+#[allow(dead_code)]
+fn getline() -> String {
+    let mut ret = String::new();
+    std::io::stdin().read_line(&mut ret).ok().unwrap();
+    ret
+}
+fn get_word() -> String {
+    let mut stdin = std::io::stdin();
+    let mut u8b: [u8; 1] = [0];
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(16);
+        loop {
+            let res = stdin.read(&mut u8b);
+            if res.unwrap_or(0) == 0 || u8b[0] <= b' ' {
+                break;
+            } else {
+                buf.push(u8b[0]);
+            }
+        }
+        if buf.len() >= 1 {
+            let ret = String::from_utf8(buf).unwrap();
+            return ret;
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn get<T: std::str::FromStr>() -> T {
+    get_word().parse().ok().unwrap()
+}
+
+
 /*
  * Suffix Array by Manber & Myers.
  * Verified by: AtCoder ARC050 (http://arc050.contest.atcoder.jp/submissions/818912)
@@ -108,4 +145,51 @@ impl LCP {
         return std::cmp::min(self.spt[diff as usize][f],
                              self.spt[diff as usize][s - 1_usize.wrapping_shl(diff)]);
     }
+}
+
+
+fn solve() {
+    let mut s: Vec<char> = get_word().chars().collect();
+    let n = s.len();
+    {
+        let mut revs = s.clone();
+        revs.reverse();
+        s.append(&mut revs);
+    }
+    let sa = create_sa(&s);
+    let salcp = LCP::new(&s, &sa);
+    let mut ans = vec![0; n + 1];
+    let mut cand = vec![vec![0i16; n]; n];
+    for s in 0 .. n {
+        for i in 0 .. n - s {
+            // is_pal?
+            let j = i + s;
+            let lcp_len = salcp.get_lcp(i, 2 * n - 1 - j);
+            if lcp_len < j - i + 1 {
+                continue;
+            }
+            let mut res = 1;
+            if s >= 1 {
+                let half = (s + 1) / 2;
+                let sub = cand[i][i + half - 1];
+                res = sub + 1;
+            }
+            cand[i][j] = res;
+            ans[res as usize] += 1;
+        }
+    }
+    for i in (1 .. n).rev() {
+        ans[i] += ans[i + 1];
+    }
+    for i in 1 .. n + 1 {
+        print!("{}{}", ans[i], if i == n { "\n" } else { " " });
+    }
+    
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
 }
