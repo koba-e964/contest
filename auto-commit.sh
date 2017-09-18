4ff493d82fc2e74dcc763de1f86c34964da26e5d
koba-e964 <koba_max@kobas-Air.pohe>
2017-09-18 14:47:46 +0900

    Fix auto-commit.sh

--- auto-commit.sh
+++ auto-commit.sh
@@ -3,4 +3,4 @@ BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 # Automatically generate the commit message,
 # assuming that all changed files are added, not modified.
-git commit -m "Add "$(git diff --name-only --cached)
+git commit -m "Add ""$(git diff --name-only --cached | xargs echo)"
