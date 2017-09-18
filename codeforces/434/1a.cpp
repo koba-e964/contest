1d4aa7a1ce85fe506291ee1b4acd59dbad27b3f6
koba-e964 <koba_max@kobas-Air.pohe>
2017-09-18 12:27:04 +0900

    Fix codeforces/434/1a.cpp

--- codeforces/434/1a.cpp
+++ codeforces/434/1a.cpp
@@ -1,34 +1,11 @@
 #include <algorithm>
-#include <cassert>
-#include <cctype>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
-#include <deque>
-#include <functional>
 #include <iomanip>
 #include <iostream>
-#include <list>
-#include <map>
-#include <queue>
-#include <random>
-#include <set>
-#include <sstream>
 #include <string>
-#include <utility>
-#include <vector>
 
 #define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
 
 using namespace std;
-typedef long long int ll;
-typedef vector<int> VI;
-typedef vector<ll> VL;
-typedef pair<int, int> PI;
-const ll mod = 1e9 + 7;
-
-
 
 int main(void) {
   ios::sync_with_stdio(false);
@@ -42,17 +19,18 @@ int main(void) {
   REP(i, 0, s.length()) {
     bool is_cons = find(vowel.begin(), vowel.end(), s[i]) == vowel.end();
     if (not is_cons) {
+      eq = true;
       conc = 0;
       pool += s[i];
       continue;
     }
-    if (conc >= 2 && not (eq && pool[0] == s[i])) {
+    if (conc >= 2 && not (eq && pool[pool.size() - 1] == s[i])) {
       cout << pool << " ";
       eq = true;
       pool = "";
       conc = 0;
     }
-    if (pool.size() >= 1 && pool[0] != s[i]) {
+    if (conc >= 1 && pool[pool.size() - 1] != s[i]) {
       eq = false;
     }
     pool += s[i];
