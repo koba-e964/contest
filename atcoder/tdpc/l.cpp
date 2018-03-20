cf5a50b2407aff4b46531d3b7e081b90e005ca15
koba-e964 <koba_max@kobas-Air.pohe>
2018-03-21 00:37:24 +0900

    Fix atcoder/tdpc/l.cpp

--- atcoder/tdpc/l.cpp
+++ atcoder/tdpc/l.cpp
@@ -1,20 +1,63 @@
-#include <vector>
-#include <string>
 #include <algorithm>
-#include <iostream>
-#include <cstdio>
 #include <cassert>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
 #include <cstdlib>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
+#define DEBUGP(val) cerr << #val << "=" << val << "\n"
 
 using namespace std;
-const int N=1000;
-int f[N][N];
-int n;
-int main(void){
-	cin>>n;
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			cin>>f[i][j];
-		}
-	}
+typedef long long int ll;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef pair<int, int> PI;
+
+const int inf = 1e8;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  vector<VI> f(n, VI(n));
+  REP(i, 0, n) {
+    REP(j, 0, n) {
+      cin >> f[i][j];
+    }
+  }
+  vector<VI> dp(n, VI(n, -inf));
+  dp[0][0] = 0;
+  REP(i, 1, n) {
+    int acc = 0;
+    REP(j, 0, i) {
+      acc += f[j][i];
+    }
+    int ma = -inf;
+    REP(k, 0, i + 1) {
+      ma = max(ma, dp[i - 1][k]);
+      dp[i][k] = ma + acc;
+      acc -= f[k][i];
+    }
+  }
+  int ma = -inf;
+  REP(i, 0, n) {
+    ma = max(ma, dp[n - 1][i]);
+  }
+  cout << 2 * ma << endl;
 }
