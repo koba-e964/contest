a3bd253493535aba37dccf712db94b04ee57c70b
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2018-06-26 01:38:46 +0900

    Add atcoder/arc099/f.cpp, comm/ModInt.cpp

--- atcoder/arc099/f.cpp
+++ atcoder/arc099/f.cpp
@@ -19,9 +19,8 @@
 #include <utility>
 #include <vector>
 
-const int DEBUG = 1;
+const int DEBUG = 0;
 
-#define MOCK 1
 
 #define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
 #define DEBUGP(val) cerr << #val << "=" << val << "\n"
@@ -34,24 +33,60 @@ typedef pair<int, int> PI;
 typedef pair<ll, ll> PL;
 
 
-ll powmod(ll a, ll e, ll mod) {
-  ll sum = 1;
-  ll cur = a;
-  while (e > 0) {
-    if (e % 2) {
-      sum = sum * cur % mod;
-    }
-    cur = cur * cur % mod;
-    e /= 2;
+const int A = 7;
+
+const ll mod = 1e9 + 7;
+
+template<ll mod = (ll)1e9 + 7>
+struct ModInt {
+  ll x;
+  ModInt(void): x(0) {}
+  ModInt(ll x): x(x % mod){}
+  ModInt(const ModInt &x): x(x.x) {}
+  ModInt operator+(ModInt o) const {
+    ll y = x + o.x;
+    if (y >= mod) y -= mod;
+    return ModInt(y);
   }
-  return sum;
+  ModInt operator-(ModInt o) const {
+    ll y = x - o.x + mod;
+    if (y >= mod) y -= mod;
+    return ModInt(y);
+  }
+  ModInt operator*(ModInt o) const {
+    return ModInt((x * o.x) % mod);
+  }
+  ll to_ll() const {
+    return x;
+  }
+  bool operator<(ModInt o) const {
+    return x < o.x;
+  }
+  ModInt pow(ll e) {
+    assert (e >= 0);
+    ModInt sum = 1;
+    ModInt cur = *this;
+    while (e > 0) {
+      if (e % 2) {
+        sum = sum * cur;
+      }
+      cur = cur * cur;
+      e /= 2;
+    }
+    return sum;
+  }
+  ModInt inv(void) {
+    return pow(mod - 2);
+  }
+};
+
+template<ll mod>
+ostream &operator<<(ostream &os, ModInt<mod> mi) {
+  return os << mi.x;
 }
 
-const int A = 20;
 
 int main(void) {
-  const ll mod = 1e9 + 7;
-  const ll mod2 = 1e9 + 9;
   ios::sync_with_stdio(false);
   cin.tie(0);
   int n;
@@ -60,66 +95,66 @@ int main(void) {
   int p = 0;
   VI pos(n + 1);
   mt19937 mt;
-  VL b(A);
-  ll mods[A] = {};
-  REP(i, 0, A) mods[i] = 1e9 + 7;
+  vector<ModInt<> > b(A);
   
   REP(c, 0, A) {
     do {
-      b[c] = mt() % mods[c];
-    } while (b[c] == 0);
+      b[c] = mt();
+    } while (b[c].to_ll() == 0);
   }
-  vector<VL> hsh(A, VL(n + 1));
-  ll inv[A] = { 0 };
-  REP(c, 0, A) inv[c] = powmod(b[c], mods[c] - 2, mods[c]);
+  vector<vector<ModInt<> > > hsh(A, vector<ModInt<> >(n + 1));
+  ModInt<> inv[A] = { 0 };
+  REP(c, 0, A) inv[c] = b[c].inv();
   REP(c, 0, A) {
     p = 0;
-    ll cur = 1;
+    ModInt<> cur = 1;
     REP(i, 0, n) {
-      ll tmp = hsh[c][i];
+      ModInt<> tmp = hsh[c][i];
       if (s[i] == '>') {
         p++;
-        cur = cur * b[c] % mods[c];
+        cur = cur * b[c];
       }
       if (s[i] == '<') {
         p--;
-        cur = cur * inv[c] % mods[c];
+        cur = cur * inv[c];
       }
       if (s[i] == '+') {
-        tmp = (tmp + cur) % mods[c];
+        tmp = tmp + cur;
       }
       if (s[i] == '-') {
-        tmp = (tmp + mods[c] - cur) % mods[c];
+        tmp = tmp - cur;
       }
       hsh[c][i + 1] = tmp;
       pos[i + 1] = p;
     }
   }
   if (DEBUG) {
-    REP(c, 0, A) {
-      REP(i, 0, n + 1) {
-        cerr << i << " " << hsh[c][i] << endl;
-      }
-      cerr << endl;
+    REP(i, 0, n + 1) {
+      cerr << i << " " << hsh[0][i] << endl;
     }
   }
-  VL meguru(A);
+  vector<ModInt<> > meguru(A);
   REP(c, 0, A) meguru[c] = hsh[c][n];
-  map<VL, int> kirika;
+  map<vector<ModInt<> >, int> kirika;
   kirika[meguru] = 1;
   ll tot = 0;
-  VL cur(A);
-  REP(c, 0, A) cur[c] = powmod(b[c], pos[n], mods[c]);
+  vector<ModInt<> > cur(A);
+  REP(c, 0, A) cur[c] = b[c].pow(mod - 1 + pos[n]);
   for (int i = n - 1; i >= 0; --i) {
     if (DEBUG) {
       DEBUGP(i);
       DEBUGP(pos[i]);
     }
-    REP(c, 0, A) cur[c] = powmod(b[c], pos[i], mods[c]);
-    VL ken(A);
-    REP(c, 0, A) ken[c] = cur[c] * meguru[c] + hsh[c][i] % mods[c];
+    if (pos[i + 1] - pos[i] == 1) {
+      REP(c, 0, A) cur[c] = cur[c] * inv[c];
+    }
+    else if (pos[i + 1] - pos[i] == -1) {
+      REP(c, 0, A) cur[c] = cur[c] * b[c];
+    }
+    vector<ModInt<> > ken(A);
+    REP(c, 0, A) ken[c] = cur[c] * meguru[c] + hsh[c][i];
     tot += kirika[ken];
-    VL tt(A);
+    vector<ModInt<> > tt(A);
     REP(c, 0, A) tt[c] = hsh[c][i];
     kirika[tt] += 1;
   }
---
+++ comm/ModInt.cpp
@@ -0,0 +1,54 @@
+/*
+ * Dependencies: typedef long long ll
+ * Headers: iostream
+ * Verified by: ARC099-F
+ *              https://beta.atcoder.jp/contests/arc099/submissions/2743855
+ */
+
+template<ll mod = (ll)1e9 + 7>
+struct ModInt {
+  ll x;
+  ModInt(void): x(0) {}
+  ModInt(ll x): x(x % mod){}
+  ModInt(const ModInt &x): x(x.x) {}
+  ModInt operator+(ModInt o) const {
+    ll y = x + o.x;
+    if (y >= mod) y -= mod;
+    return ModInt(y);
+  }
+  ModInt operator-(ModInt o) const {
+    ll y = x - o.x + mod;
+    if (y >= mod) y -= mod;
+    return ModInt(y);
+  }
+  ModInt operator*(ModInt o) const {
+    return ModInt((x * o.x) % mod);
+  }
+  ll to_ll() const {
+    return x;
+  }
+  bool operator<(ModInt o) const {
+    return x < o.x;
+  }
+  ModInt pow(ll e) {
+    assert (e >= 0);
+    ModInt sum = 1;
+    ModInt cur = *this;
+    while (e > 0) {
+      if (e % 2) {
+        sum = sum * cur;
+      }
+      cur = cur * cur;
+      e /= 2;
+    }
+    return sum;
+  }
+  ModInt inv(void) {
+    return pow(mod - 2);
+  }
+};
+
+template<ll mod>
+ostream &operator<<(ostream &os, ModInt<mod> mi) {
+  return os << mi.x;
+}
