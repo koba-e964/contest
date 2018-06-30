f7bfa54a6268b2981a1703f926c357b7a634eb2e
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2018-07-01 01:14:47 +0900

    Fix atcoder/bitflyer2018-final/f.cpp

--- atcoder/bitflyer2018-final/f.cpp
+++ atcoder/bitflyer2018-final/f.cpp
@@ -14,6 +14,7 @@
 #include <queue>
 #include <random>
 #include <set>
+#include <unordered_set>
 #include <sstream>
 #include <string>
 #include <utility>
@@ -27,23 +28,22 @@ typedef long long int ll;
 typedef vector<int> VI;
 typedef vector<ll> VL;
 typedef pair<int, int> PI;
-const ll mod = 1e9 + 7;
 
 const int N = 2100;
 int a[N][N], dif[N][N];
 int h, w;
-set<PI> on;
+unordered_set<int> on;
 
 void flip(int x, int y) {
   if (x < 0 || y < 0 || x >= h || y >= w) return;
   if (dif[x][y]) {
     dif[x][y] = 0;
-    on.erase(PI(x, y));
+    on.erase(x * w + y);
     return;
   }
   dif[x][y] = 1;
   if (x >= 1 && y >= 1) {
-    on.insert(PI(x, y));
+    on.insert(x * w + y);
   }
 }
 
@@ -52,10 +52,17 @@ void debug(void) {
     REP(j, 0, w) cerr << dif[i][j];
     cerr << endl;
   }
-  for (PI e: on) {
-    cerr << " (" << e.first << "," << e.second << ")";
-  }
   cerr << endl;
+}
+
+vector<PI> get_packed(void) {
+  vector<int> pack(on.begin(), on.end());
+  vector<PI> ret(on.size());
+  REP(i, 0, on.size()) {
+    ret[i] = PI(pack[i] / w, pack[i] % w);
+  }
+  sort(ret.begin(), ret.end());
+  return ret;
 }
 
 bool check(void) {
@@ -63,11 +70,11 @@ bool check(void) {
   if (on.size() <= 1) return true;
   if (on.size() <= 2) {
     // Do they share row or column?
-    vector<PI> pack(on.begin(), on.end());
+    vector<PI> pack = get_packed();
     return pack[0].first == pack[1].first || pack[0].second == pack[1].second;
   }
   if (on.size() == 4) {
-    vector<PI> pack(on.begin(), on.end());
+    vector<PI> pack = get_packed();
     sort(pack.begin(), pack.end());
     int x1 = pack[0].first, x2 = pack[2].first;
     int y1 = pack[0].second, y2 = pack[1].second;
