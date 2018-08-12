01d4763f83eda1f5d89de08e3eb1bcdde4a40fd4
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2018-08-12 22:59:35 +0900

    Add aizu/2751.rs, fix comm/ModInt.rs

--- aizu/2751.rs
+++ aizu/2751.rs
@@ -1,3 +1,37 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Read, Write, BufWriter};
+#[allow(dead_code)]
+fn getline() -> String {
+    let mut ret = String::new();
+    std::io::stdin().read_line(&mut ret).ok().unwrap();
+    ret
+}
+fn get_word() -> String {
+    let mut stdin = std::io::stdin();
+    let mut u8b: [u8; 1] = [0];
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(16);
+        loop {
+            let res = stdin.read(&mut u8b);
+            if res.unwrap_or(0) == 0 || u8b[0] <= b' ' {
+                break;
+            } else {
+                buf.push(u8b[0]);
+            }
+        }
+        if buf.len() >= 1 {
+            let ret = String::from_utf8(buf).unwrap();
+            return ret;
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn get<T: std::str::FromStr>() -> T { get_word().parse().ok().unwrap() }
+const MOD: i64 = 1_000_000_007;
 /// Refers external ::MOD.
 /// Verified by: https://beta.atcoder.jp/contests/arc099/submissions/2893648
 mod mod_int {
@@ -96,3 +130,66 @@ mod mod_int {
         }
     }
 } // mod mod_int
+
+use mod_int::*;
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($format:expr) => (write!(out,$format).unwrap());
+        ($format:expr, $($args:expr),+) => (write!(out,$format,$($args),*).unwrap())
+    }
+    const N: usize = 4000000;
+    let mut fac = [ModInt::new(1); N];
+    let mut invfac = [ModInt::new(0); N];
+    for i in 1 .. N {
+        fac[i] = fac[i - 1] * ModInt::new(i as i64);
+    }
+    invfac[N - 1] = fac[N - 1].inv();
+    for i in (0 .. N - 1).rev() {
+        invfac[i] = invfac[i + 1] * ModInt::new(i as i64 + 1);
+    }
+    macro_rules! distr {
+        ($a: expr, $b: expr) => {{
+            let a = $a as usize;
+            let b = $b as usize;
+            if b == 0 {
+                ModInt::new(if a == 0 { 1 } else { 0 })
+            } else {
+                fac[a + b - 1] * invfac[a] * invfac[b - 1]
+            }
+        }};
+    }
+    loop {
+        let a: i64 = get();
+        let b: i64 = get();
+        let c: i64 = get();
+        let mut sx: i64 = get();
+        let mut sy: i64 = get();
+        if a == 0 && b == 0 && c == 0 { break; }
+        let mut ans = ModInt::new(0);
+        sx -= a;
+        sy -= b;
+        if sx >= 0 && sy >= 0 {
+            for u in 0 .. min(sx, sy) + 1 {
+                let mut tmp = distr!(sx - u, a);
+                tmp *= distr!(sy - u, b);
+                tmp *= distr!(u, a + b + c);
+                ans += tmp;
+            }
+        }
+        let a = a as usize;
+        let b = b as usize;
+        let c = c as usize;
+        ans *= fac[a + b + c] * invfac[a] * invfac[b] * invfac[c];
+        puts!("{}\n", ans);
+    }
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
