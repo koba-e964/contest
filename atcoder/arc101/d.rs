1d076e27de527294325a70bf87779136b9bcce9b
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2018-08-26 12:41:18 +0900

    Update rust.rs

--- atcoder/arc101/d.rs
+++ atcoder/arc101/d.rs
@@ -2,31 +2,59 @@
 use std::cmp::*;
 #[allow(unused_imports)]
 use std::collections::*;
-use std::io::{Read, Write, BufWriter};
-#[allow(dead_code)]
-fn getline() -> String {
-    let mut ret = String::new();
-    std::io::stdin().read_line(&mut ret).ok().unwrap();
-    ret
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
 }
-fn get_word() -> String {
-    let mut stdin = std::io::stdin();
-    let mut u8b: [u8; 1] = [0];
-    loop {
-        let mut buf: Vec<u8> = Vec::with_capacity(16);
-        loop {
-            let res = stdin.read(&mut u8b);
-            if res.unwrap_or(0) == 0 || u8b[0] <= b' ' {
-                break;
-            } else {
-                buf.push(u8b[0]);
-            }
-        }
-        if buf.len() >= 1 {
-            let ret = String::from_utf8(buf).unwrap();
-            return ret;
-        }
-    }
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
 }
 
 /**
@@ -82,9 +110,6 @@ impl<I, BiOp> SegTree<I, BiOp>
 }
 
 
-#[allow(dead_code)]
-fn get<T: std::str::FromStr>() -> T { get_word().parse().ok().unwrap() }
-
 fn solve() {
     let out = std::io::stdout();
     let mut out = BufWriter::new(out.lock());
@@ -92,8 +117,10 @@ fn solve() {
         ($format:expr) => (write!(out,$format).unwrap());
         ($format:expr, $($args:expr),+) => (write!(out,$format,$($args),*).unwrap())
     }
-    let n = get();
-    let a: Vec<i64> = (0 .. n).map(|_| get()).collect();
+    input!{
+        n: usize,
+        a: [i64; n],
+    }
     let mut fail: i64 = 1 << 30;
     let mut pass: i64 = 0;
     let nn = n as i64;
--- rust.rs
+++ rust.rs
@@ -2,35 +2,60 @@
 use std::cmp::*;
 #[allow(unused_imports)]
 use std::collections::*;
-use std::io::{Read, Write, BufWriter};
-#[allow(dead_code)]
-fn getline() -> String {
-    let mut ret = String::new();
-    std::io::stdin().read_line(&mut ret).ok().unwrap();
-    ret
-}
-fn get_word() -> String {
-    let mut stdin = std::io::stdin();
-    let mut u8b: [u8; 1] = [0];
-    loop {
-        let mut buf: Vec<u8> = Vec::with_capacity(16);
-        loop {
-            let res = stdin.read(&mut u8b);
-            if res.unwrap_or(0) == 0 || u8b[0] <= b' ' {
-                break;
-            } else {
-                buf.push(u8b[0]);
-            }
-        }
-        if buf.len() >= 1 {
-            let ret = String::from_utf8(buf).unwrap();
-            return ret;
-        }
-    }
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
 }
 
-#[allow(dead_code)]
-fn get<T: std::str::FromStr>() -> T { get_word().parse().ok().unwrap() }
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
 
 fn solve() {
     let out = std::io::stdout();
