15553cf6f4a72d4c2e912af3931d0b0787730380
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2018-09-11 11:08:24 +0900

    Add comm/convolution.cpp

--- comm/convolution.cpp
+++ comm/convolution.cpp
@@ -1,33 +1,4 @@
-#include <algorithm>
-#include <cassert>
-#include <cctype>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
-#include <deque>
-#include <functional>
-#include <iomanip>
-#include <iostream>
-#include <list>
-#include <map>
-#include <queue>
-#include <random>
-#include <set>
-#include <sstream>
-#include <string>
-#include <utility>
-#include <vector>
-
-#define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
-#define DEBUGP(val) cerr << #val << "=" << val << "\n"
-
-using namespace std;
-typedef long long int ll;
-typedef vector<int> VI;
-typedef vector<ll> VL;
-typedef pair<int, int> PI;
-
+//http://math314.hateblo.jp/entry/2015/05/07/014908 からコピー && 改造
 template<class T> T ext_gcd(T a, T b, T& x, T& y) { for (T u = y = 1, v = x = 0; a;) { T q = b / a; swap(x -= q * u, u); swap(y -= q * v, v); swap(b -= q * a, a); } return b; }
 
 template<class T> T zmod(T a, T b) {
@@ -42,34 +13,12 @@ template<class T> T invmod(T a, T m) {
   return zmod(x, m);
 }
 
-const int N = 200100;
-ll fac_m[N], fac_e[N];
-
-void init(void) {
-  int e = 0;
-  fac_m[0] = 1;
-  fac_e[0] = 0;
-  int mi = 0;
-  REP(i, 0, N) {
-    int v = i + 1;
-    while (v % 2 == 0) {
-      e += 1;
-      v /= 2;
-      fac_e[i + 1] += 1;
-    }
-    if (i < N - 1) {
-      fac_m[i + 1] = fac_m[i] * v;
-      fac_e[i + 1] += fac_e[i];
-    }
-    mi = min(mi, (int) fac_e[i] - i - 1);
-  }
-}
-
+// ここを mod に応じて適切に変える
 ll mulmod(ll x, ll y, ll mod) {
   return x * y % mod;
 }
 
-//http://math314.hateblo.jp/entry/2015/05/07/014908 からコピー && 改造
+
 // garnerのアルゴリズム
 // x % m[i] == r[i] を満たす，最小の0以上の整数x について， x % mod を求める
 // ex)
@@ -117,7 +66,7 @@ public:
     const int n = a.size();
     assert((n ^ (n&-n)) == 0); //n = 2^k
 
-    const int g = primitive_root; //g is primitive root of mod
+    const int g = primitive_root; //g is a primitive root of mod
     ll h = powmod(g, (mod - 1) / n); // h^n = 1
     if (sign == -1) h = powmod(h, mod-2); //h = h^-1 % mod
 
@@ -181,6 +130,7 @@ public:
   }
 };
 
+// Up to 2^20
 typedef NTT<1012924417, 5> NTT_1;
 typedef NTT<1224736769, 3> NTT_2;
 typedef NTT<1007681537, 3> NTT_3;
@@ -208,44 +158,4 @@ vector<ll> arbmod_convolution(vector<ll> a, vector<ll> b, ll mod){
   }
 
   return ret;
-}
-
-const ll lim = 1LL << 50;
-
-int main(void) {
-  init();
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  int n;
-  cin >> n;
-  VL a(n + 1), b(n + 1);
-  REP(i, 0, n + 1) cin >> a[i];
-  REP(i, 0, n + 1) cin >> b[i];
-  // u[i] = a[i] / i! * 2^i mod 2^32
-  // v[i] = b[i] / i! * 2^i mod 2^32
-  vector<ll> u(n + 1), v(n + 1);
-  ll tmp = 1;
-  int e = 0;
-  fac_m[0] = 1;
-  fac_e[0] = 0;
-  REP(i, 0, n + 1) {
-    u[i] = (tmp * a[i]) << (i - e) & (lim - 1);
-    v[i] = (tmp * b[i]) << (i - e) & (lim - 1);
-    int v = i + 1;
-    while (v % 2 == 0) {
-      e += 1;
-      v /= 2;
-    }
-    tmp *= invmod<ll>(v, lim);
-    tmp %= lim;
-  }
-  VL conv = arbmod_convolution(u, v, lim);
-  VL ans(n + 1);
-  REP(i, 0, n + 1) {
-    ll u = (conv[i] * fac_m[i]) >> (i - fac_e[i]);
-    ans[i] = u & ((1LL << 32) - 1);
-  }
-  REP(i, 0, n + 1) {
-    cout << ans[i] << (i == n ? "\n" : " ");
-  }
 }
