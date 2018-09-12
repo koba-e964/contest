967655cd10c245c427dd38b5b4682606da2960e3
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2018-09-12 11:05:06 +0900

    Fix comm/line.rs

--- codeforces/edu50/e.rs
+++ codeforces/edu50/e.rs
@@ -78,15 +78,19 @@ fn ccw((x1, y1): (i64, i64), (x2, y2): (i64, i64), (x3, y3): (i64, i64))
         },
     }
 }
+fn det<T: std::ops::Mul<Output = T> + std::ops::Sub<Output = T>>
+    ((x, y): (T, T), (z, w): (T, T)) -> T {
+    x * w - y * z
+}
 
 fn intersect((p11, p12): ((i64, i64), (i64, i64)), (p21, p22): ((i64, i64), (i64, i64)))
              -> Option<(i64, i64)> {
     if ccw(p11, p12, p21) * ccw(p11, p12, p22) <= 0
         && ccw(p21, p22, p11) * ccw(p21, p22, p12) <= 0 {
-            let (a00, a01, a10, a11) = (p12.0 - p11.0, p22.0 - p21.0,
-                                        p12.1 - p11.1, p22.1 - p21.1);
-            let (v0, v1) = (p21.0 - p11.0, p21.1 - p11.1);
-            let (s_num, s_den) = (v0 * a11 - v1 * a01, a00 * a11 - a10 * a01);
+            let (a00, a10) = (p12.0 - p11.0, p12.1 - p11.1);
+            let a1 = (p22.0 - p21.0, p22.1 - p21.1);
+            let v = (p21.0 - p11.0, p21.1 - p11.1);
+            let (s_num, s_den) = (det(v, a1), det((a00, a10), a1));
             if s_num * a00 % s_den == 0 && s_num * a10 % s_den == 0 {
                 Some((p11.0 + s_num * a00 / s_den, p11.1 + s_num * a10 / s_den))
             } else {
