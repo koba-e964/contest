50604179a2bb7f7fa3b8dc62ecf89d6e6df866d8
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2018-10-27 19:47:57 +0900

    Add codeforces/edu9/f.rs

---
+++ codeforces/edu9/f.rs
@@ -0,0 +1,139 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+/**
+ * Union-Find tree.
+ * Verified by yukicoder No.94 (http://yukicoder.me/submissions/82111)
+ */
+struct UnionFind { disj: Vec<usize> }
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        let mut disj = vec![0; n];
+        for i in 0 .. n {
+            disj[i] = i;
+        }
+        UnionFind { disj: disj }
+    }
+    fn root(self: &mut Self, x: usize) -> usize {
+        if x != self.disj[x] {
+            let par = self.disj[x];
+            let r = self.root(par);
+            self.disj[x] = r;
+        }
+        return self.disj[x];
+    }
+    fn unite(self: &mut Self, x: usize, y: usize) {
+        let xr = self.root(x);
+        let yr = self.root(y);
+        self.disj[xr] = yr;
+    }
+    fn is_same_set(self: &mut Self, x: usize, y: usize) -> bool {
+        return self.root(x) == self.root(y);
+    }
+}
+
+fn dfs(dist: &mut [Vec<i64>], tree: &[Vec<(usize, i64)>],
+       v: usize, par: usize, init: usize, d: i64) {
+    dist[init][v] = d;
+    for &(w, c) in tree[v].iter() {
+        if w == par { continue; }
+        dfs(dist, tree, w, v, init, max(d, c));
+    }
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($format:expr) => (write!(out,$format).unwrap());
+        ($format:expr, $($args:expr),+) => (write!(out,$format,$($args),*).unwrap())
+    }
+    input! {
+        n: usize,
+        a: [[i64; n]; n],
+    }
+    let mut pool = Vec::new();
+    for i in 0 .. n {
+        for j in 0 .. i {
+            pool.push((a[i][j], i, j));
+        }
+    }
+    pool.sort();
+    let mut uf = UnionFind::new(n);
+    let mut tree = vec![Vec::new(); n];
+    for (dist, i, j) in pool {
+        if !uf.is_same_set(i, j) {
+            uf.unite(i, j);
+            tree[i].push((j, dist));
+            tree[j].push((i, dist));
+        }
+    }
+    let mut dist2 = vec![vec![0; n]; n];
+    for i in 0 .. n {
+        dfs(&mut dist2, &tree, i, usize::max_value(), i, 0);
+    }
+    puts!("{}\n", if a == dist2 { "MAGIC" } else { "NOT MAGIC" });
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
