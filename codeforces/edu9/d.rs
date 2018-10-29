656e5f53569276dfaef8e1f69dd23a972e812319
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2018-10-29 09:47:26 +0900

    Add codeforces/edu9/d.rs

---
+++ codeforces/edu9/d.rs
@@ -0,0 +1,105 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($format:expr) => (write!(out,$format).unwrap());
+        ($format:expr, $($args:expr),+) => (write!(out,$format,$($args),*).unwrap())
+    }
+    input! {
+        n: usize,
+        m: usize,
+        a: [usize; n],
+    }
+    let mut freq = vec![0; m + 1];
+    for &a in a.iter() {
+        if a <= m {
+            freq[a] += 1;
+        }
+    }
+    let mut inex = vec![0; m + 1];
+    for i in 1 .. m + 1 {
+        for j in 1 .. m / i + 1 {
+            let j = i * j;
+            inex[j] += freq[i];
+        }
+    }
+    let mut ma = (0, Reverse(1));
+    for i in 1 .. m + 1 {
+        ma = max(ma, (inex[i], Reverse(i)));
+    }
+    let (cnt, Reverse(lcm)) = ma;
+    puts!("{} {}\n", lcm, cnt);
+    let mut idx = Vec::new();
+    for i in 0 .. n {
+        if lcm % a[i] == 0 { idx.push(i); }
+    }
+    for i in 0 .. cnt {
+        puts!("{}{}", idx[i] + 1, if i + 1 == cnt { "\n" } else { " " });
+    }
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
