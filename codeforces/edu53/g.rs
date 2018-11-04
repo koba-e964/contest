39f75394e5b464e4a59c7ba119b9cc5b419ea73f
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2018-11-05 01:42:20 +0900

    Add codeforces/edu53/g.rs

---
+++ codeforces/edu53/g.rs
@@ -0,0 +1,292 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, usizes2) => {{
+        let len1 = read_value!($next, usize);
+        let len2 = read_value!($next, usize);
+        let v1 = read_value!($next, [usize1; len1]);
+        let v2 = read_value!($next, [usize1; len2]);
+        (v1, v2)
+    }};
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+/*
+ * Suffix Array by Manber & Myers.
+ * Verified by: AtCoder ARC050 (http://arc050.contest.atcoder.jp/submissions/818912)
+ * Reference: http://mayokoex.hatenablog.com/entry/2016/04/03/145845
+ */
+fn create_sa(s: &[char]) -> Vec<usize> {
+    let n = s.len();
+    let mut sa: Vec<usize> = (0 .. n + 1).collect();
+    let mut rank: Vec<usize> = vec![0; n + 1];
+    let mut tmp = vec![0; n + 1];
+
+    for i in 0 .. n + 1 {
+        rank[i] = if i < n { s[i] as usize + 1 } else { 0_usize };
+    }
+    let mut k = 1;
+    while k <= n {
+        {
+            let key = |i: &usize| {
+                let ri = if i + k <= n { rank[i + k] as i32 } else { -1 };
+                (rank[*i], ri)
+            };
+            sa.sort_by_key(&key);
+            tmp[sa[0]] = 0;
+            for i in 1 .. n + 1 {
+                tmp[sa[i]] = tmp[sa[i - 1]]
+                    + if key(&sa[i - 1]) < key(&sa[i]) { 1 } else { 0 };
+            }
+        }
+        rank.clone_from_slice(&tmp);
+        k *= 2;
+    }
+    return sa;
+}
+
+struct LCP {
+    inv_sa: Vec<usize>,
+    spt: Vec<Vec<usize>>
+}
+
+impl LCP {
+    pub fn new(s: &[char], sa: &[usize]) -> LCP {
+        let n = sa.len() - 1;
+        let mut inv_sa = vec![0; n + 1];
+        for i in 0 .. n + 1 {
+            inv_sa[sa[i]] = i;
+        }
+        let lcp = Self::create_lcp(s, sa);
+        let spt = Self::create_sparse_table(&lcp);
+        LCP {
+            inv_sa: inv_sa,
+            spt: spt,
+        }
+    }
+    fn create_lcp(s: &[char], sa: &[usize]) -> Vec<usize> {
+        let n = s.len();
+        let mut rank = vec![0; n + 1];
+        let mut lcp = vec![0; n];
+        for i in 0 .. n + 1 {
+            rank[sa[i]] = i;
+        }
+        let mut h: usize = 0;
+        lcp[0] = 0;
+        for i in 0 .. n {
+            let j = sa[rank[i] - 1];
+            h = h.saturating_sub(1);
+            while j + h < n && i + h < n {
+                if s[j + h] != s[i + h] {
+                    break;
+                }
+                h += 1;
+            }
+            lcp[rank[i] - 1] = h;
+        }
+        return lcp;
+    }
+    
+    fn create_sparse_table(lcp: &[usize]) -> Vec<Vec<usize>> {
+        let n = lcp.len();
+        let mut h: usize = 1;
+        while (1 << h) <= n {
+            h += 1;
+        }
+        let mut st: Vec<Vec<usize>> = vec![Vec::new(); h];
+        st[0] = Vec::from(lcp);
+        for j in 1 .. h {
+            st[j] = vec![0; n + 1 - (1 << j)];
+            for i in 0 .. n + 1 - (1 << j) {
+                st[j][i] = std::cmp::min(
+                    st[j - 1][i],
+                    st[j - 1][i + 1_usize.wrapping_shl(j as u32 - 1)]);
+            }
+        }
+        return st;
+    }
+    
+    pub fn get_lcp(&self, f: usize, s: usize) -> usize {
+        let f = self.inv_sa[f];
+        let s = self.inv_sa[s];
+        let (f, s) = 
+            if f > s {
+                (s, f)
+            } else {
+                (f, s)
+            };
+        assert!(f < s);
+        let usize_size = usize::max_value().count_ones();
+        let diff = usize_size - 1 - (s - f).leading_zeros(); // topmost 1
+        return std::cmp::min(self.spt[diff as usize][f],
+                             self.spt[diff as usize][s - 1_usize.wrapping_shl(diff)]);
+    }
+}
+
+/**
+ * Segment Tree. This data structure is useful for fast folding on intervals of an array
+ * whose elements are elements of monoid I. Note that constructing this tree requires the identity
+ * element of I and the operation of I.
+ * Verified by: yukicoder No. 259 (http://yukicoder.me/submissions/100581)
+ *              AGC015-E (http://agc015.contest.atcoder.jp/submissions/1461001)
+ */
+struct SegTree<I, BiOp> {
+    n: usize,
+    dat: Vec<I>,
+    op: BiOp,
+    e: I,
+}
+
+impl<I, BiOp> SegTree<I, BiOp>
+    where BiOp: Fn(I, I) -> I,
+          I: Copy {
+    pub fn new(n_: usize, op: BiOp, e: I) -> Self {
+        let mut n = 1;
+        while n < n_ { n *= 2; } // n is a power of 2
+        SegTree {n: n, dat: vec![e; 2 * n - 1], op: op, e: e}
+    }
+    /* ary[k] <- v */
+    pub fn update(&mut self, idx: usize, v: I) {
+        let mut k = idx + self.n - 1;
+        self.dat[k] = v;
+        while k > 0 {
+            k = (k - 1) / 2;
+            self.dat[k] = (self.op)(self.dat[2 * k + 1], self.dat[2 * k + 2]);
+        }
+    }
+    /* [a, b) (note: half-inclusive)
+     * http://proc-cpuinfo.fixstars.com/2017/07/optimize-segment-tree/ */
+    pub fn query(&self, mut a: usize, mut b: usize) -> I {
+        let mut left = self.e;
+        let mut right = self.e;
+        a += self.n - 1;
+        b += self.n - 1;
+        while a < b {
+            if (a & 1) == 0 {
+                left = (self.op)(left, self.dat[a]);
+            }
+            if (b & 1) == 0 {
+                right = (self.op)(self.dat[b - 1], right);
+            }
+            a = a / 2;
+            b = (b - 1) / 2;
+        }
+        (self.op)(left, right)
+    }
+}
+
+
+fn calc(mut a: Vec<usize>, lcp: &LCP, len: usize) -> i64 {
+    a.sort_by_key(|val| lcp.inv_sa[*val]);
+    let n = a.len();
+    if n == 1 { return 0; }
+    let mut diff = vec![0; n - 1];
+    for i in 0 .. n - 1 {
+        if a[i] == a[i + 1] {
+            diff[i] = len - a[i];
+        } else {
+            diff[i] = lcp.get_lcp(a[i], a[i + 1]);
+        }
+    }
+    let mut tot = 0;
+    let mut st = SegTree::new(n - 1, |x, y| min(x, y), (1 << 28, 1));
+    for i in 0 .. n - 1 {
+        st.update(i, (diff[i], i));
+    }
+    let mut que = BinaryHeap::new();
+    que.push((Reverse(st.query(0, n - 1)), 0, n - 2));
+    while let Some((Reverse((val, idx)), l, r)) = que.pop() {
+        tot += (r + 1 - idx) as i64 *  (idx - l + 1) as i64 * val as i64;
+        if idx > l {
+            que.push((Reverse(st.query(l, idx)), l, idx - 1));
+        }
+        if idx < r {
+            que.push((Reverse(st.query(idx + 1, r + 1)), idx + 1, r));
+        }
+    }
+    tot
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($format:expr) => (write!(out,$format).unwrap());
+        ($format:expr, $($args:expr),+) => (write!(out,$format,$($args),*).unwrap())
+    }
+    input! {
+        n: usize,
+        q: usize,
+        s: chars,
+        ab: [usizes2; q],
+    }
+    let sa = create_sa(&s);
+    let lcp = LCP::new(&s, &sa);
+    for (a, b) in ab {
+        let mut meld = a.clone();
+        meld.append(&mut b.clone());
+        puts!("{}\n", calc(meld, &lcp, n) - calc(a, &lcp, n) - calc(b, &lcp, n));
+    }
+        
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
