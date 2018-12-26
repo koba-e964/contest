bc2e1ae61ef43a1e4a1d0db6bc27b61f1722ade6
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2018-12-27 01:06:45 +0900

    Add atcoder/pakencamp-2018-day3/{f.rs,remain.txt}, fix mod/ModInt.rs

--- atcoder/pakencamp-2018-day3/f.rs
+++ atcoder/pakencamp-2018-day3/f.rs
@@ -1,3 +1,67 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
 /// Verified by: https://atcoder.jp/contests/pakencamp-2018-day3/submissions/3878249
 mod mod_int {
     use std::ops::*;
@@ -109,3 +173,59 @@ macro_rules! define_mod {
 const MOD: i64 = 1_000_000_007;
 define_mod!(P, MOD);
 type ModInt = mod_int::ModInt<P>;
+
+fn calc(a: i64, b: i64) -> ModInt {
+    let one = ModInt::new(1);
+    let two = ModInt::new(2);
+    let am = ModInt::new(a);
+    let bm = ModInt::new(b);
+    let mut tot = (bm + am + one) * (bm - am) * two.inv() + one;
+    let half = (b - a) / 2;
+    let mut pass = 0;
+    let mut fail = half + 1;
+    while fail - pass > 1 {
+        let mid = (fail + pass) / 2;
+        if mid * (a - b + mid) + a - 1 >= 0 {
+            pass = mid;
+        } else {
+            fail = mid;
+        }
+    }
+    if pass == half {
+        // i^2 + (a - b)i + a - 1 (i = 1 .. b - a)
+        let bma = bm - am;
+        tot -= bma * (bma + one) * (two * bma + one) * ModInt::new(6).inv();
+        tot += bma * bma * (bma + one) * two.inv();
+        tot -= (am - one) * bma;
+    } else {
+        // 2 * (i^2 + (a - b)i + a - 1) (i = 1 .. pass) + (a - 1)
+        let x = ModInt::new(pass);
+        tot -= x * (x + one) * (two * x + one) * ModInt::new(3).inv();
+        tot -= (am - bm) * x * (x + one);
+        tot -= two * (am - one) * x;
+        tot -= am - one;
+    }
+    tot
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($format:expr) => (write!(out,$format).unwrap());
+        ($format:expr, $($args:expr),+) => (write!(out,$format,$($args),*).unwrap())
+    }
+    input! {
+        ab: [(i64, i64)],
+    }
+    for (a, b) in ab {
+        puts!("{}\n", calc(a, b));
+    }
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
