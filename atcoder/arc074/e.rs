b1be38e3369ff50faa27643dfeae8f4567902aa2
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-01-12 19:57:44 +0900

    Add atcoder/arc074/e.rs

---
+++ atcoder/arc074/e.rs
@@ -0,0 +1,233 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+/// Verified by https://atcoder.jp/contests/arc093/submissions/3968098
+mod mod_int {
+    use std::ops::*;
+    pub trait Mod: Copy { fn m() -> i64; }
+    #[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct ModInt<M> { pub x: i64, phantom: ::std::marker::PhantomData<M> }
+    impl<M: Mod> ModInt<M> {
+        fn check_integrity(self) {
+            debug_assert!(self.x >= 0);
+            debug_assert!(self.x < M::m());
+        }
+        // x >= 0
+        pub fn new(x: i64) -> Self { ModInt::new_internal(x % M::m()) }
+        fn new_internal(x: i64) -> Self {
+            ModInt { x: x, phantom: ::std::marker::PhantomData }
+        }
+        #[allow(dead_code)]
+        pub fn mul_fast(self, other: Self) -> Self {
+            self.check_integrity();
+            other.check_integrity();
+            ModInt::new_internal(self.x * other.x % M::m())
+        }
+        #[allow(dead_code)]
+        pub fn mul_slow(self, other: Self) -> Self {
+            // Naive multiplication in order to avoid overflow
+            self.check_integrity();
+            other.check_integrity();
+            let mut sum = ModInt::new_internal(0);
+            let mut cur = self;
+            let mut e = other.x;
+            if self.x < other.x {
+                cur = other;
+                e = self.x;
+            }
+            while e > 0 {
+                if e % 2 == 1 { sum += cur; }
+                cur += cur;
+                e /= 2;
+            }
+            sum
+        }
+        pub fn pow(self, mut e: i64) -> Self {
+            self.check_integrity();
+            debug_assert!(e >= 0);
+            let mut sum = ModInt::new_internal(1);
+            let mut cur = self;
+            while e > 0 {
+                if e % 2 != 0 { sum *= cur; }
+                cur *= cur;
+                e /= 2;
+            }
+            sum
+        }
+        #[allow(dead_code)]
+        pub fn inv(self) -> Self { self.pow(M::m() - 2) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Add<T> for ModInt<M> {
+        type Output = Self;
+        fn add(self, other: T) -> Self {
+            let other = other.into();
+            self.check_integrity();
+            other.check_integrity();
+            let mut sum = self.x + other.x;
+            if sum >= M::m() { sum -= M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Sub<T> for ModInt<M> {
+        type Output = Self;
+        fn sub(self, other: T) -> Self {
+            let other = other.into();
+            self.check_integrity();
+            other.check_integrity();
+            let mut sum = self.x - other.x;
+            if sum < 0 { sum += M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Mul<T> for ModInt<M> {
+        type Output = Self;
+        fn mul(self, other: T) -> Self { self.mul_fast(other.into()) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> AddAssign<T> for ModInt<M> {
+        fn add_assign(&mut self, other: T) { *self = *self + other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> SubAssign<T> for ModInt<M> {
+        fn sub_assign(&mut self, other: T) { *self = *self - other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> MulAssign<T> for ModInt<M> {
+        fn mul_assign(&mut self, other: T) { *self = *self * other; }
+    }
+    impl<M: Mod> Neg for ModInt<M> {
+        type Output = Self;
+        fn neg(self) -> Self { ModInt::new(0) - self }
+    }
+    impl<M> ::std::fmt::Display for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            self.x.fmt(f)
+        }
+    }
+    impl<M> ::std::fmt::Debug for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            self.x.fmt(f)
+        }
+    }
+    impl<M: Mod> From<i64> for ModInt<M> {
+        fn from(x: i64) -> Self { Self::new(x) }
+    }
+} // mod mod_int
+
+macro_rules! define_mod {
+    ($struct_name: ident, $modulo: expr) => {
+        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        struct $struct_name {}
+        impl mod_int::Mod for $struct_name { fn m() -> i64 { $modulo } }
+    }
+}
+const MOD: i64 = 1_000_000_007;
+define_mod!(P, MOD);
+type ModInt = mod_int::ModInt<P>;
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+    }
+    input! {
+        n: usize,
+        m: usize,
+        lrx: [(usize1, usize1, i32); m],
+    }
+    let mut cons = vec![Vec::new(); n + 3];
+    for (l, r, x) in lrx {
+        cons[r + 3].push((l + 3, x));
+    }
+    let mut dp = vec![vec![vec![ModInt::new(0); n + 3]; n + 3]; n + 3];
+    dp[2][1][0] = ModInt::new(1);
+    for i in 3 .. n + 3 {
+        for j in 0 .. i - 1 {
+            for k in 0 .. j {
+                dp[i][j][k] += dp[i - 1][j][k];
+                dp[i][i - 1][j] += dp[i - 1][j][k];
+                dp[i][i - 1][k] += dp[i - 1][j][k];
+            }
+        }
+        for &(l, x) in cons[i].iter() {
+            for j in 0 .. i {
+                let kind = 1 + (j >= l) as i32;
+                for k in 0 .. j {
+                    let kind = kind + (k >= l) as i32;
+                    if kind != x {
+                        // Doesn't fulfill the constraint. Reset.
+                        dp[i][j][k] = ModInt::new(0);
+                    }
+                }
+            }
+        }
+    }
+    let mut tot = ModInt::new(0);
+    for j in 0 .. n + 2 {
+        for k in 0 .. j {
+            tot += dp[n + 2][j][k];
+        }
+    }
+    puts!("{}\n", tot);
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
