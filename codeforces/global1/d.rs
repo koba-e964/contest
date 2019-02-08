5512893ecc0df97d1e1d7ab1339dccb2aadd78e3
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-02-09 00:47:59 +0900

    Add codeforces/global1/a.rs codeforces/global1/b.rs codeforces/global1/c.rs codeforces/global1/d.rs codeforces/global1/e.rs codeforces/global1/remain.txt

---
+++ codeforces/global1/d.rs
@@ -0,0 +1,99 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+    }
+    input! {
+        n: usize,
+        m: usize,
+        a: [usize1; n],
+    }
+    const INF: i32 = 1 << 20;
+    let mut freq = vec![0i32; m];
+    for a in a { freq[a] += 1; }
+    let mut dp = vec![[[-INF; 3]; 6]; m + 1];
+    dp[0][0][0] = 0;
+    for i in 0..m {
+        for j in 0..6 {
+            // How many i's are remaining?
+            let rem = freq[i] - j as i32;
+            if rem < 0 { continue; }
+            for k in 0..min(rem as usize + 1, 3) {
+                let tri = (rem - k as i32) / 3;
+                for l in 0..3 {
+                    dp[i + 1][l + k][k] = max(dp[i + 1][l + k][k],
+                                              dp[i][j][l] + tri + k as i32);
+                }
+            }
+        }
+    }
+    let ma = dp[m][0][0];
+    puts!("{}\n", ma);
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
