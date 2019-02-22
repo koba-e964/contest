68c7726ee230167d0d9ee08ae3eef654ba3591fe
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-02-22 23:36:15 +0900

    Add yukicoder/792.rs yukicoder/793.rs yukicoder/794.rs

---
+++ yukicoder/792.rs
@@ -0,0 +1,110 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+    }
+    input! {
+        n: usize,
+        qr: [([i32; n], i32); 1 << n],
+    }
+    let mut truth = vec![];
+    for (q, r) in qr {
+        if r == 1 {
+            truth.push(q);
+        }
+    }
+    let t = truth.len();
+    puts!("A=");
+    if t == 0 {
+        puts!("⊥\n");
+    } else if t == 1 << n {
+        puts!("⊤\n");
+    } else {
+        for i in 0..t {
+            let q = &truth[i];
+            puts!("(");
+            for j in 0..n {
+                if q[j] == 0 {
+                    puts!("¬");
+                }
+                puts!("P_{}", j + 1);
+                if j < n - 1 {
+                    puts!("∧");
+                }
+            }
+            puts!(")");
+            if i == t - 1 {
+                puts!("\n");
+            } else {
+                puts!("∨");
+            }
+        }
+    }
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
