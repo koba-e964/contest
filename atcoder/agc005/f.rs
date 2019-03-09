8edc6a2cc9b4e5ab77dc3b0806259c13df8c9496
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-03-09 15:39:22 +0900

    Add atcoder/agc005/f.rs

---
+++ atcoder/agc005/f.rs
@@ -0,0 +1,294 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+/// Verified by https://atcoder.jp/contests/arc093/submissions/3968098
+mod mod_int {
+    use std::ops::*;
+    pub trait Mod: Copy { fn m() -> i64; }
+    #[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct ModInt<M> { pub x: i64, phantom: ::std::marker::PhantomData<M> }
+    impl<M: Mod> ModInt<M> {
+        // x >= 0
+        pub fn new(x: i64) -> Self { ModInt::new_internal(x % M::m()) }
+        fn new_internal(x: i64) -> Self {
+            ModInt { x: x, phantom: ::std::marker::PhantomData }
+        }
+        pub fn pow(self, mut e: i64) -> Self {
+            debug_assert!(e >= 0);
+            let mut sum = ModInt::new_internal(1);
+            let mut cur = self;
+            while e > 0 {
+                if e % 2 != 0 { sum *= cur; }
+                cur *= cur;
+                e /= 2;
+            }
+            sum
+        }
+        #[allow(dead_code)]
+        pub fn inv(self) -> Self { self.pow(M::m() - 2) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Add<T> for ModInt<M> {
+        type Output = Self;
+        fn add(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x + other.x;
+            if sum >= M::m() { sum -= M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Sub<T> for ModInt<M> {
+        type Output = Self;
+        fn sub(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x - other.x;
+            if sum < 0 { sum += M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Mul<T> for ModInt<M> {
+        type Output = Self;
+        fn mul(self, other: T) -> Self { ModInt::new(self.x * other.into().x % M::m()) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> AddAssign<T> for ModInt<M> {
+        fn add_assign(&mut self, other: T) { *self = *self + other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> SubAssign<T> for ModInt<M> {
+        fn sub_assign(&mut self, other: T) { *self = *self - other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> MulAssign<T> for ModInt<M> {
+        fn mul_assign(&mut self, other: T) { *self = *self * other; }
+    }
+    impl<M: Mod> Neg for ModInt<M> {
+        type Output = Self;
+        fn neg(self) -> Self { ModInt::new(0) - self }
+    }
+    impl<M> ::std::fmt::Display for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            self.x.fmt(f)
+        }
+    }
+    impl<M> ::std::fmt::Debug for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            self.x.fmt(f)
+        }
+    }
+    impl<M: Mod> From<i64> for ModInt<M> {
+        fn from(x: i64) -> Self { Self::new(x) }
+    }
+} // mod mod_int
+
+macro_rules! define_mod {
+    ($struct_name: ident, $modulo: expr) => {
+        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        struct $struct_name {}
+        impl mod_int::Mod for $struct_name { fn m() -> i64 { $modulo } }
+    }
+}
+const MOD: i64 = 924844033;
+define_mod!(P, MOD);
+type ModInt = mod_int::ModInt<P>;
+
+// Depends on ModInt.rs
+fn fact_init(w: usize) -> (Vec<ModInt>, Vec<ModInt>) {
+    let mut fac = vec![ModInt::new(1); w];
+    let mut invfac = vec![0.into(); w];
+    for i in 1 .. w {
+        fac[i] = fac[i - 1] * i as i64;
+    }
+    invfac[w - 1] = fac[w - 1].inv();
+    for i in (0 .. w - 1).rev() {
+        invfac[i] = invfac[i + 1] * (i as i64 + 1);
+    }
+    (fac, invfac)
+}
+
+fn dfs(v: usize, par: usize, g: &[Vec<usize>], freq: &mut [usize])
+       -> usize /* size of subtree */ {
+    let mut sz = 1;
+    for &w in &g[v] {
+        if w == par { continue; }
+        sz += dfs(w, v, g, freq);
+    }
+    if par < g.len() { // v is not a root
+        freq[sz] += 1;
+        freq[g.len() - sz] += 1;
+    }
+    sz
+}
+
+/// FFT (in-place, verified as NTT only)
+/// R: Ring + Copy
+/// Verified by: https://codeforces.com/contest/1096/submission/47672373
+mod fft {
+    use std::ops::*;
+    /// n should be a power of 2. zeta is a primitive n-th root of unity.
+    /// one is unity
+    /// Note that the result should be multiplied by 1/sqrt(n).
+    pub fn transform<R>(f: &mut [R], zeta: R, one: R)
+        where R: Copy +
+        Add<Output = R> +
+        Sub<Output = R> +
+        Mul<Output = R> {
+        let n = f.len();
+        assert!(n.is_power_of_two());
+        {
+            let mut i = 0;
+            for j in 1 .. n - 1 {
+                let mut k = n >> 1;
+                loop {
+                    i ^= k;
+                    if k <= i { break; }
+                    k >>= 1;
+                }
+                if j < i { f.swap(i, j); }
+            }
+        }
+        let mut zetapow = Vec::new();
+        {
+            let mut m = 1;
+            let mut cur = zeta;
+            while m < n {
+                zetapow.push(cur);
+                cur = cur * cur;
+                m *= 2;
+            }
+        }
+        let mut m = 1;
+        while m < n {
+            let base = zetapow.pop().unwrap();
+            let mut r = 0;
+            while r < n {
+                let mut w = one;
+                for s in r .. r + m {
+                    let u = f[s];
+                    let d = f[s + m] * w;
+                    f[s] = u + d;
+                    f[s + m] = u - d;
+                    w = w * base;
+                }
+                r += 2 * m;
+            }
+            m *= 2;
+        }
+    }
+}
+
+use fft::*;
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+    }
+    input! {
+        n: usize,
+        ab: [(usize1, usize1); n - 1],
+    }
+    let (fac, invfac) = fact_init(n + 1);
+    let mut g = vec![vec![]; n];
+    for &(a, b) in &ab {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    let mut freq = vec![0; n + 1];
+    dfs(0, n, &g, &mut freq);
+    const N: usize = 1 << 19;
+    let mut f = vec![ModInt::new(0); N];
+    for i in 0..n + 1 {
+        f[i] = fac[i] * freq[i] as i64;
+    }
+    let mut g = vec![ModInt::new(0); N]; // base = N / 2
+    for i in 0..n + 1 {
+        g[N / 2 - i] = invfac[i];
+    }
+    //eprintln!("f = {:?}", f);
+    //eprintln!("g = {:?}", g);
+    // zeta transform
+    let one = ModInt::new(1);
+    let gen = ModInt::new(5);
+    let zeta = gen.pow((MOD - 1) / N as i64);
+    let invzeta = zeta.inv();
+
+    transform(&mut f, zeta, one);
+    transform(&mut g, zeta, one);
+    for i in 0..N { f[i] *= g[i]; }
+    transform(&mut f, invzeta, one);
+    transform(&mut g, invzeta, one);
+    for i in 0..N {
+        f[i] *= ModInt::new(N as i64).inv();
+    }
+
+    // whole
+    let mut whole = vec![ModInt::new(0); n + 1];
+    for k in 0..n + 1 {
+        whole[k] = fac[n] * invfac[k] * invfac[n - k] * n as i64;
+    }
+
+    for k in 1..n + 1 {
+        puts!("{}\n", whole[k] - f[N / 2 + k] * invfac[k]);
+    }
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
