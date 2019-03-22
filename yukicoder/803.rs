e575386d90aafb06e8be4dedf0ac78b47e012cf0
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-03-23 03:36:21 +0900

    Fix yukicoder/803.rs

--- yukicoder/803.rs
+++ yukicoder/803.rs
@@ -57,43 +57,6 @@ macro_rules! read_value {
     };
 }
 
-// returns (rank(a), dim ker(a))
-fn gauss_elim_gf2(a: &mut [Vec<i32>], b: &mut [i32]) -> Option<(usize, usize)> {
-    let n = a.len();
-    let m = b.len();
-    let mut row = 0;
-    for col in 0..m {
-        if row >= n { break; }
-        let mut row2 = None;
-        for i in row..n {
-            if a[i][col] == 1 {
-                row2 = Some(i);
-                break;
-            }
-        }
-        if row2.is_none() { continue; }
-        let row2 = row2.unwrap();
-        a.swap(row, row2);
-        for k in row + 1..n {
-            if a[k][col] == 1 {
-                for j in 0..m {
-                    a[k][j] ^= a[row][j];
-                }
-            }
-        }
-        if b[col] == 1 {
-            for j in 0..m {
-                b[j] ^= a[row][j];
-            }
-        }
-        row += 1;
-    }
-    for i in 0..m {
-        if b[i] != 0 { return None; }
-    }
-    Some((row, n - row))
-}
-
 /// Verified by https://atcoder.jp/contests/arc093/submissions/3968098
 mod mod_int {
     use std::ops::*;
@@ -180,6 +143,43 @@ macro_rules! define_mod {
 const MOD: i64 = 1_000_000_007;
 define_mod!(P, MOD);
 type ModInt = mod_int::ModInt<P>;
+
+// returns (rank(a), dim ker(a))
+fn gauss_elim_gf2(a: &mut [Vec<i32>], b: &mut [i32]) -> Option<(usize, usize)> {
+    let n = a.len();
+    let m = b.len();
+    let mut row = 0;
+    for col in 0..m {
+        if row >= n { break; }
+        let mut row2 = None;
+        for i in row..n {
+            if a[i][col] == 1 {
+                row2 = Some(i);
+                break;
+            }
+        }
+        if row2.is_none() { continue; }
+        let row2 = row2.unwrap();
+        a.swap(row, row2);
+        for k in row + 1..n {
+            if a[k][col] == 1 {
+                for j in 0..m {
+                    a[k][j] ^= a[row][j];
+                }
+            }
+        }
+        if b[col] == 1 {
+            for j in 0..m {
+                b[j] ^= a[row][j];
+            }
+        }
+        row += 1;
+    }
+    for i in 0..m {
+        if b[i] != 0 { return None; }
+    }
+    Some((row, n - row))
+}
 
 fn solve() {
     let out = std::io::stdout();
