02200151e79effcbb333c911ae2f1c4ba01f3e05
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-03-27 00:03:38 +0900

    Add atcoder/agc009/d.rs

---
+++ atcoder/agc009/d.rs
@@ -0,0 +1,113 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn dfs(v: usize, par: usize, g: &[Vec<usize>]) -> i64 {
+    let mut a = 0;
+    let mut cnt = vec![0; 60];
+    for &w in &g[v] {
+        if w == par { continue; }
+        let sub = dfs(w, v, g);
+        a |= sub;
+        for i in 0..60 {
+            if (sub & 1 << i) != 0 {
+                cnt[i] += 1;
+            }
+        }
+    }
+    let mut twomax = 0;
+    for i in 0..60 {
+        if cnt[i] >= 2 {
+            twomax = i;
+        }
+    }
+    (a + (1 << twomax)) & (-1) << twomax
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+    }
+    input! {
+        n: usize,
+        ab: [(usize1, usize1); n - 1],
+    }
+    let mut g = vec![vec![]; n];
+    for &(a, b) in &ab {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    let ret = dfs(0, n, &g);
+    //eprintln!("ret = {}", ret);
+    let mut ma = 0;
+    for i in 0..60 {
+        if (ret & 1 << i) != 0 {
+            ma = i;
+        }
+    }
+    puts!("{}\n", ma);
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
