1faa9c4dbe81dd41230bd2bd4de5011d4510c1fe
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-05-10 00:21:21 +0900

    Fix codeforces/270-1/d.cpp

--- codeforces/270-1/d.cpp
+++ codeforces/270-1/d.cpp
@@ -1,100 +1,58 @@
 #include <algorithm>
-#include <cassert>
-#include <cmath>
 #include <cstdio>
-#include <cstdlib>
-#include <iostream>
-#include <map>
-#include <set>
-#include <string>
 #include <vector>
 
 #define REP(i,s,n) for(int i=(int)(s);i<(int)(n);i++)
 
 using namespace std;
 typedef long long int ll;
-const double EPS=1e-9;
-
-const int DEBUG = 0;
+typedef pair<ll,int> PLI;
 
 const int N = 2014;
-int d[N][N];
+ll d[N][N];
+vector<pair<ll,int> > tr[N];
 
 void t(int v) {
-	cout << (v ? "YES" : "NO") << endl;
-	exit(0);
+  puts(v ? "YES" : "NO");
+  exit(0);
 }
 
-struct edge{
-	int x,y,dd;
-	edge(int x,int y, int dd): x(x),y(y),dd(dd){}
-	bool operator<(const edge& o)const { return dd < o.dd;}
-};
+void dfs(int r,int w,int par,ll dist){
+  if(d[r][w]!=dist)t(0);
+  for(PLI e:tr[w]){
+    int x=e.second;
+    if(par!=x)dfs(r,x,w,dist+e.first);
+  }
+}
 
 int n;
-int u[N] = {0};
-int rem;
-vector<edge> egs;
+int u[N]={};
+pair<ll,int> dist[N];
+const ll inf=1e15;
 int main(void){
-	cin >> n;
-	rem = n - 1;
-	REP(i,0,n) {
-		REP(j,0,n) {
-			cin>>d[i][j];
-		}
-	}
-	REP(i,0,n) {
-		if(d[i][i]) t(0);
-	}
-	REP(i,0,n) REP(j,0,n) if(d[i][j] != d[j][i]) t(0);
-	REP(i,0,n) REP(j,i+1,n) {
-		egs.push_back(edge(i,j,d[i][j]));
-	}
-	sort(egs.begin(), egs.end());
-	reverse(egs.begin(),egs.end());
-	int pos = 0;
-	while(rem > 0) { // T has >= 2 vertices
-		int maxi = -1,maxj = -1,maxd=0;
-		while(pos < egs.size()) {
-			edge e=egs[pos];
-			int i = e.x;
-			int j = e.y;
-			if(u[i] || u[j]) {
-				pos ++;
-				continue;
-			}
-			maxd = d[i][j];
-			maxi = i,maxj=j;
-			pos++;
-			break;
-		}
-		if(maxd == 0) t(0);
-		if(DEBUG) {
-			cout << "max path: " << maxi << "~" << maxj << " " << maxd << endl;
-		}
-		int xc = -1, xd = 0x3fffffff;
-		REP(i,0,n) {
-			if(u[i] || maxi == i) continue;
-			if(xd > d[maxi][i]) {
-				xc = i;
-				xd = d[maxi][i];
-			}
-		}
-		if(xd == 0) t(0);
-		if(DEBUG) {
-			cout << "xc=" << xc << ", xd=" << xd << endl; 
-		}
-		REP(y,0,n) {
-			if(u[y] || y == maxi) continue;
-			if(d[maxi][y] != d[maxi][xc] + d[xc][y]) {
-				t(0);
-			}
-		}
-		u[maxi] = 1;
-		rem --;
-		if(DEBUG) {
-			cout << "removed " << maxi << endl;
-		}
-	}
-	t(1);
+  scanf("%d",&n);
+  REP(i,0,n)REP(j,0,n) {
+    scanf("%lld",&d[i][j]);
+  }
+  // find an MST, Prim
+  u[0] = 1;
+  REP(i,0,n)dist[i]=make_pair(d[0][i],0);
+  REP(_,1,n){
+    pair<PLI,int> mi(PLI(inf,-1),-1);
+    REP(i,0,n){
+      if(u[i])continue;
+      mi=min(mi,make_pair(dist[i],i));
+    }
+    int w=mi.first.second;
+    int v=mi.second;
+    ll c=mi.first.first;
+    if(c==0)t(0);
+    u[v]=1;
+    tr[w].push_back(PLI(c,v));
+    tr[v].push_back(PLI(c,w));
+    REP(i,0,n)dist[i]=min(dist[i],PLI(d[v][i],v));
+  }
+  // distance for all pairs
+  REP(i,0,n)dfs(i,i,-1,0);
+  t(1);
 }
