675dc2b3410c52b4bf23caad2162756660afe483
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-05-19 02:34:49 +0900

    Fix comm/frac.cpp

--- comm/frac.cpp
+++ comm/frac.cpp
@@ -1,28 +1,35 @@
 typedef long long int ll;
+
 ll gcd(ll x,ll y){
-	while(y!=0){
-		ll r=x%y;
-		x=y;y=r;
-	}return x;
+  while(y!=0){
+    ll r=x%y;
+    x=y;y=r;
+  }
+  return x;
 }
 
-struct frac{ll x,y;frac(ll x,ll y):x(x),y(y){
-	reduce();
-}
-frac operator+(const frac& f)const{
-	ll g=gcd(y,f.y);
-	return frac(x*(f.y/g)+f.x*(y/g),y/g*f.y);
-}
-frac operator*(const frac& f)const{
-	return frac(x*f.x,y*f.y);
-}
-void reduce(){
-	ll g=gcd(x,y);
-	x/=g;
-	y/=g;
-	if(y<0){
-		x=-x;y=-y;
-	}
-}
+struct frac{
+  ll x,y;frac(ll x,ll y):x(x),y(y){
+    reduce();
+  }
+  frac operator+(const frac& f)const{
+    return frac(x*f.y+f.x*y, y*f.y);
+  }
+  frac operator*(const frac& f)const{
+    return frac(x*f.x,y*f.y);
+  }
+  bool operator<(const frac &f) const {
+    return x*f.y < y*f.x;
+  }
+  bool operator==(const frac &f) const {
+    return x*f.y == y*f.x;
+  }
+  void reduce(){
+    ll g=gcd(x,y);
+    x/=g;
+    y/=g;
+    if(y<0){
+      x=-x;y=-y;
+    }
+  }
 };
-
