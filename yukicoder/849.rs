9e390ee1968f3c152f6a14d287a0fe6a9924b44e
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-07-07 20:21:04 +0900

    Add yukicoder/849.rs

---
+++ yukicoder/849.rs
@@ -0,0 +1,117 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn gcd(mut x: i64, mut y: i64) -> i64 {
+    while y != 0 {
+        let r = x % y;
+        x = y;
+        y = r;
+    }
+    x
+}
+
+fn rem(mut a: i64, b: i64) -> i64 {
+    assert!(b > 0);
+    a %= b;
+    if a < 0 {
+        a += b;
+    }
+    a
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+    }
+    input! {
+        a: i64, b: i64, c: i64, d: i64,
+        xy: [(i64, i64)],
+    }
+    let det = (a * d - b * c).abs();
+    if det == 0 {
+        // degenerate
+        let e = gcd(a, c);
+        let f = gcd(b, d);
+        let mut hs = HashSet::new();
+        for &(x, y) in &xy {
+            let (x, y) = (x * f - y * e, x * e + y * f);
+            let y = rem(y, f * f + e * e);
+            hs.insert((x, y));
+        }
+        puts!("{}\n", hs.len());
+        return;
+    }
+    let mut hs = HashSet::new();
+    for &(x, y) in &xy {
+        let (x, y) = (x * d - y * c, -x * b + y * a);
+        let x = rem(x, det);
+        let y = rem(y, det);
+        hs.insert((x, y));
+    }
+    puts!("{}\n", hs.len());
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
