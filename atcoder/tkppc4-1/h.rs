71f63f97e43cb048d414bda81ff2e3f2939869b3
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-07-27 22:10:56 +0900

    Add atcoder/tkppc4-1/d.rs atcoder/tkppc4-1/e.rs atcoder/tkppc4-1/f.rs atcoder/tkppc4-1/g.rs atcoder/tkppc4-1/h.rs atcoder/tkppc4-1/remain.txt

---
+++ atcoder/tkppc4-1/h.rs
@@ -0,0 +1,102 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+    }
+    input! {
+        n: usize, m: usize, k: i64,
+        t_pre: [i64; n - 2],
+        abcd: [(usize1, usize1, i64, i64); m],
+    }
+    let mut t = vec![0; n];
+    for i in 0..n - 2 {
+        t[i + 1] = t_pre[i];
+    }
+    let mut g = vec![vec![]; n];
+    for &(a, b, c, d) in &abcd {
+        g[a].push((b, c, d));
+        g[b].push((a, c, d));
+    }
+    let mut que = BinaryHeap::new();
+    que.push((0, 0));
+    let mut dist = vec![1i64 << 50; n];
+    while let Some((d, v)) = que.pop() {
+        let d = -d;
+        if dist[v] <= d {
+            continue;
+        }
+        dist[v] = d;
+        for &(w, c, r) in &g[v] {
+            let nxt = (d + r - 1) / r * r + c + t[w];
+            que.push((-nxt, w));
+        }
+    }
+    puts!("{}\n", if dist[n - 1] > k { -1 } else { dist[n - 1] });
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
