b781db45a89ea708fbf88fecb098ff5e12001869
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2019-12-18 11:42:43 +0900

    Add codeforces/global6/a.rs codeforces/global6/b.rs codeforces/global6/c.rs codeforces/global6/d.rs codeforces/global6/remain.txt

---
+++ codeforces/global6/d.rs
@@ -0,0 +1,118 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+    }
+    input! {
+        n: usize,
+        x: [(usize1, usize1, i64)],
+    }
+    let mut delta = vec![0; n];
+    for (u, v, x) in x {
+        delta[u] += x;
+        delta[v] -= x;
+    }
+    let mut pos = vec![];
+    let mut neg = vec![];
+    for i in 0..n {
+        if delta[i] > 0 {
+            pos.push((delta[i], i));
+        }
+        if delta[i] < 0 {
+            neg.push((-delta[i], i));
+        }
+    }
+    let mut ans = vec![];
+    let mut cur = 0;
+    for (mut p, idx) in pos {
+        while p > 0 && cur < neg.len() {
+            let r = min(p, neg[cur].0);
+            ans.push((idx, neg[cur].1, r));
+            p -= r;
+            neg[cur].0 -= r;
+            if neg[cur].0 == 0 {
+                cur += 1;
+            }
+        }
+    }
+    puts!("{}\n", ans.len());
+    for (a, b, c) in ans {
+        puts!("{} {} {}\n", a + 1, b + 1, c);
+    }
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
