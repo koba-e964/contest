a3dd091365d21aeb5b00203b2ae449904647f694
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-01-11 01:20:37 +0900

    Add atcoder/abc150/a.rs atcoder/abc150/b.rs atcoder/abc150/c.rs atcoder/abc150/d.rs atcoder/abc150/e.rs atcoder/abc150/f.rs yukicoder/938.rs yukicoder/949.rs

---
+++ atcoder/abc150/c.rs
@@ -0,0 +1,143 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
+/**
+ * Returns the least index of elements that are modified, wrapped with Some.
+ * If the entire array is reversed, it returns None instead.
+ * v's elements must be pairwise distinct.
+ */
+fn next_permutation<T: Ord>(v: &mut [T]) -> Option<usize> {
+    let mut tail_dec: usize = 1;
+    let n = v.len();
+    while tail_dec < n {
+        if v[n - tail_dec - 1] > v[n - tail_dec] {
+            tail_dec += 1;
+        } else {
+            break;
+        }
+    }
+    // v[n - tail_dec .. n] is strictly decreasing
+    if tail_dec < n {
+        let x = n - tail_dec - 1;
+        let mut y = n;
+        {
+            let pivot = &v[x];
+            for i in (n - tail_dec .. n).rev() {
+                if v[i] > *pivot {
+                    y = i;
+                    break;
+                }
+            }
+            assert!(y < n);
+        }
+        v.swap(x, y);
+    }
+    v[n - tail_dec .. n].reverse();
+    if tail_dec < n {
+        Some(n - tail_dec - 1)
+    } else {
+        None
+    }
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+    }
+    input! {
+        n: usize,
+        p: [usize1; n],
+        q: [usize1; n],
+    }
+    let mut c: i32 = 0;
+    let mut pi = 0;
+    let mut qi = 0;
+    let mut a: Vec<usize> = (0..n).collect();
+    loop {
+        if p == a {
+            pi = c;
+        }
+        if q == a {
+            qi = c;
+        }
+        if let None = next_permutation(&mut a) {
+            break;
+        }
+        c += 1;
+    }
+    puts!("{}\n", (pi - qi).abs());
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
---
+++ yukicoder/949.rs
@@ -0,0 +1,129 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+    }
+    input! {
+        n: usize,
+        a: [i64; n + 1],
+        b: [i64; n + 1],
+        d: [i64; n],
+    }
+    let mut d = d;
+    d.sort(); d.reverse();
+    let mut dp = vec![vec![n + 1; n + 1]; n + 1];
+    dp[0][0] = 0;
+    for i in 0..n {
+        for j in 0..n - i {
+            let now = dp[i][j];
+            if now > n { continue; }
+            {
+                let nxt = a[i] + b[j + 1];
+                let mut cur = now;
+                while cur < n {
+                    if d[cur] <= nxt {
+                        break;
+                    }
+                    cur += 1;
+                }
+                dp[i][j + 1] = min(dp[i][j + 1], cur + 1);
+            }
+            {
+                let nxt = a[i + 1] + b[j];
+                let mut cur = now;
+                while cur < n {
+                    if d[cur] <= nxt {
+                        break;
+                    }
+                    cur += 1;
+                }
+                dp[i + 1][j] = min(dp[i + 1][j], cur + 1);
+            }
+        }
+    }
+    let mut ma = 0;
+    for i in 0..n + 1 {
+        for j in 0..n + 1 {
+            if dp[i][j] <= n {
+                ma = max(ma, i + j);
+            }
+        }
+    }
+    puts!("{}\n", ma);
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
