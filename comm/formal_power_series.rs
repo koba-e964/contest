6818fccce966bc28ef05183958740bac79e06f84
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-01-28 01:26:19 +0900

    Add judge.yosupo.jp/inv_of_formal_power_series.rs

--- comm/formal_power_series.rs
+++ comm/formal_power_series.rs
@@ -4,7 +4,7 @@
 ///
 /// Complexity: O(n log n)
 ///
-/// Verified by: https://yukicoder.me/submissions/415313
+/// Verified by: https://judge.yosupo.jp/submission/3219
 ///
 /// Depends on: ModInt.rs, fft.rs
 fn formal_power_series_inv<P: mod_int::Mod + PartialOrd>(
@@ -15,28 +15,27 @@ fn formal_power_series_inv<P: mod_int::Mod + PartialOrd>(
     assert!(n.is_power_of_two());
     assert_eq!(f[0], 1.into());
     let mut sz = 1;
-    let mut r = vec![mod_int::ModInt::new(0); n];
+    let mut r = vec![mod_int::ModInt::new(0); 2 * n];
+    let mut tmp_f = vec![mod_int::ModInt::new(0); 2 * n];
     r[0] = 1.into();
     while sz < n {
         sz *= 2;
         // r_{i + 1} = 2 * r_i - r_i^2 * f
         let zeta = gen.pow((P::m() - 1) / sz as i64 / 2);
-        let mut tmp_r = vec![mod_int::ModInt::new(0); 2 * sz];
-        let mut tmp_f = vec![mod_int::ModInt::new(0); 2 * sz];
         for i in 0..sz {
-            tmp_r[i] = r[i];
             tmp_f[i] = f[i];
         }
-        fft::transform(&mut tmp_r, zeta, 1.into());
-        fft::transform(&mut tmp_f, zeta, 1.into());
+        fft::transform(&mut r[..2 * sz], zeta, 1.into());
+        fft::transform(&mut tmp_f[..2 * sz], zeta, 1.into());
         let fac = mod_int::ModInt::new(2 * sz as i64).inv();
         for i in 0..2 * sz {
-            tmp_r[i] = tmp_r[i] * (-tmp_r[i] * tmp_f[i] + 2) * fac;
+            r[i] = r[i] * (-r[i] * tmp_f[i] + 2) * fac;
         }
-        fft::transform(&mut tmp_r, zeta.inv(), 1.into());
-        for i in 0..sz {
-            r[i] = tmp_r[i];
+        fft::transform(&mut r[..2 * sz], zeta.inv(), 1.into());
+        for i in sz..2 * sz {
+            r[i] = 0.into();
         }
     }
+    r.truncate(sz);
     r
 }
