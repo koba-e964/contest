5f8e93959de41f9304ba790e04b39e7db47f1a89
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-01-28 01:42:39 +0900

    Optimize comm/formal_power_series.rs (inv)

--- comm/formal_power_series.rs
+++ comm/formal_power_series.rs
@@ -4,7 +4,7 @@
 ///
 /// Complexity: O(n log n)
 ///
-/// Verified by: https://judge.yosupo.jp/submission/3219
+/// Verified by: https://judge.yosupo.jp/submission/3224
 ///
 /// Depends on: ModInt.rs, fft.rs
 fn formal_power_series_inv<P: mod_int::Mod + PartialOrd>(
@@ -15,27 +15,36 @@ fn formal_power_series_inv<P: mod_int::Mod + PartialOrd>(
     assert!(n.is_power_of_two());
     assert_eq!(f[0], 1.into());
     let mut sz = 1;
-    let mut r = vec![mod_int::ModInt::new(0); 2 * n];
-    let mut tmp_f = vec![mod_int::ModInt::new(0); 2 * n];
+    let mut r = vec![mod_int::ModInt::new(0); n];
+    let mut tmp_f = vec![mod_int::ModInt::new(0); n];
+    let mut tmp_r = vec![mod_int::ModInt::new(0); n];
     r[0] = 1.into();
+    // Adopts the technique used in https://judge.yosupo.jp/submission/3153
     while sz < n {
-        sz *= 2;
-        // r_{i + 1} = 2 * r_i - r_i^2 * f
         let zeta = gen.pow((P::m() - 1) / sz as i64 / 2);
-        for i in 0..sz {
-            tmp_f[i] = f[i];
-        }
-        fft::transform(&mut r[..2 * sz], zeta, 1.into());
-        fft::transform(&mut tmp_f[..2 * sz], zeta, 1.into());
-        let fac = mod_int::ModInt::new(2 * sz as i64).inv();
         for i in 0..2 * sz {
-            r[i] = r[i] * (-r[i] * tmp_f[i] + 2) * fac;
+            tmp_f[i] = f[i];
+            tmp_r[i] = r[i];
         }
-        fft::transform(&mut r[..2 * sz], zeta.inv(), 1.into());
+        fft::transform(&mut tmp_r[..2 * sz], zeta, 1.into());
+        fft::transform(&mut tmp_f[..2 * sz], zeta, 1.into());
+        let fac = mod_int::ModInt::new(2 * sz as i64).inv().pow(2);
+        for i in 0..2 * sz {
+            tmp_f[i] = tmp_f[i] * tmp_r[i];
+        }
+        fft::transform(&mut tmp_f[..2 * sz], zeta.inv(), 1.into());
+        for i in 0..sz {
+            tmp_f[i] = 0.into();
+        }
+        fft::transform(&mut tmp_f[..2 * sz], zeta, 1.into());
+        for i in 0..2 * sz {
+            tmp_f[i] = -tmp_f[i] * tmp_r[i] * fac;
+        }
+        fft::transform(&mut tmp_f[..2 * sz], zeta.inv(), 1.into());
         for i in sz..2 * sz {
-            r[i] = 0.into();
+            r[i] = tmp_f[i];
         }
+        sz *= 2;
     }
-    r.truncate(sz);
     r
 }
