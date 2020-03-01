1bdb59c9ac731d9ed35b639da9b10075bf6b204b
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-03-02 01:47:16 +0900

    Add atcoder/abc157/a.rs atcoder/abc157/b.rs atcoder/abc157/c.rs atcoder/abc157/d.rs atcoder/abc157/e.rs atcoder/abc157/f.rs

---
+++ atcoder/abc157/f.rs
@@ -0,0 +1,151 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
+const EPS: f64 = 1.0e-9;
+
+// http://is2011.2-d.jp/moin/moin.cgi/amylase/icpc#A.2BUYYwaHb0fdowbk6kcLk-
+// 円の交点
+// intersections of two circles
+fn cc_intersect((cx, cy, cr): (f64, f64, f64), (dx, dy, dr) : (f64, f64, f64))
+                -> Vec<(f64, f64)> {
+    let dist = ((cx - dx) * (cx - dx) + (cy - dy) * (cy - dy)).sqrt();
+    if dist > cr + dr + EPS {
+        return vec![];
+    }
+    if dist < (cr - dr).abs() - EPS {
+        return vec![];
+    }
+    let s = (cr + dr + dist) / 2.0;
+    let area = (s * (s - cr) * (s - dr) * (s - dist)).sqrt();
+    let h = 2.0 * area / dist;
+    let (vx, vy) = (dx - cx, dy - cy);
+    let vabs = (vx * vx + vy * vy).sqrt();
+    let (vx, vy) = (vx / vabs, vy / vabs);
+    let ss = (cr * cr - h * h).sqrt();
+    let (mx, my) = (cx + ss * vx, cy + ss * vy);
+    let (nx, ny) = (-vy, vx);
+    vec![(mx + nx * h, my + ny * h), (mx - nx * h, my - ny * h)]
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (let _ = write!(out,$($format)*););
+    }
+    input! {
+        n: usize, k: usize,
+        xyc: [(f64, f64, f64); n],
+    }
+    let mut pass = 1000000.0;
+    let mut fail = 0.0;
+    for _ in 0..100 {
+        let mid = (pass + fail) / 2.0;
+        let mut pts = vec![];
+        let mut xyr = xyc.clone();
+        for i in 0..n {
+            xyr[i].2 = mid / xyc[i].2;
+        }
+        for i in 0..n {
+            pts.push((xyr[i].0, xyr[i].1));
+        }
+        for i in 0..n {
+            for j in 0..i {
+                pts.append(&mut cc_intersect(xyr[i], xyr[j]));
+            }
+        }
+        let mut ok = false;
+        for &(x, y) in &pts {
+            let mut count = 0;
+            for i in 0..n {
+                let (cx, cy, cr) = xyr[i];
+                let dist = (x - cx) * (x - cx) + (y - cy) * (y - cy);
+                if dist < cr * cr + cr * EPS {
+                    count += 1;
+                }
+            }
+            if count >= k {
+                ok = true;
+                break;
+            }
+        }
+        if ok {
+            pass = mid;
+        } else {
+            fail = mid;
+        }
+    }
+    puts!("{}\n", pass);
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
