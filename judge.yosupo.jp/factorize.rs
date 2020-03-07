b2ad564bc829981ef99fed8de8d36be276640ed4
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-03-07 11:22:26 +0900

    Add judge.yosupo.jp/factorize.rs

--- judge.yosupo.jp/factorize.rs
+++ judge.yosupo.jp/factorize.rs
@@ -1,3 +1,71 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
 mod pollard_rho {
     use std::collections::HashMap;
     /// binary gcd
@@ -21,19 +89,9 @@ mod pollard_rho {
         if z >= n { z - n } else { z }
     }
 
-    fn mul_mod(x: i64, mut y: i64, n: i64) -> i64 {
-        assert!(x >= 0);
-        assert!(x < n);
-        let mut sum = 0;
-        let mut cur = x;
-        while y > 0 {
-            if (y & 1) == 1 {
-                sum = add_mod(sum, cur, n);
-            }
-            cur = add_mod(cur, cur, n);
-            y >>= 1;
-        }
-        sum
+    fn mul_mod(x: i64, y: i64, n: i64) -> i64 {
+        let a = x as i128 * y as i128 % n as i128;
+        a as i64
     }
 
     fn mod_pow(x: i64, mut e: i64, n: i64) -> i64 {
@@ -119,3 +177,26 @@ mod pollard_rho {
         v
     }
 } // mod pollard_rho
+
+fn main() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (let _ = write!(out,$($format)*););
+    }
+    input!(a: [i64]);
+    for a in a {
+        let fac = pollard_rho::factorize(a);
+        let mut ps = vec![];
+        for (p, e) in fac {
+            for _ in 0..e {
+                ps.push(p);
+            }
+        }
+        puts!("{}", ps.len());
+        for p in ps {
+            puts!(" {}", p);
+        }
+        puts!("\n");
+    }
+}
