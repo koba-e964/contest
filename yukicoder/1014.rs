6921510f5402c617913353194fd96d96acff4c5c
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-03-21 02:39:07 +0900

    Add yukicoder/1009.rs yukicoder/1010.rs yukicoder/1011.rs yukicoder/1012.rs yukicoder/1013.rs yukicoder/1014.rs

---
+++ yukicoder/1014.rs
@@ -0,0 +1,233 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
+trait Bisect<T> {
+    fn lower_bound(&self, val: &T) -> usize;
+    fn upper_bound(&self, val: &T) -> usize;
+}
+
+impl<T: Ord> Bisect<T> for [T] {
+    fn lower_bound(&self, val: &T) -> usize {
+        let mut pass = self.len() + 1;
+        let mut fail = 0;
+        while pass - fail > 1 {
+            let mid = (pass + fail) / 2;
+            if &self[mid - 1] >= val {
+                pass = mid;
+            } else {
+                fail = mid;
+            }
+        }
+        pass - 1
+    }
+    fn upper_bound(&self, val: &T) -> usize {
+        let mut pass = self.len() + 1;
+        let mut fail = 0;
+        while pass - fail > 1 {
+            let mid = (pass + fail) / 2;
+            if &self[mid - 1] > val {
+                pass = mid;
+            } else {
+                fail = mid;
+            }
+        }
+        pass - 1
+    }
+}
+
+/**
+ * Segment Tree. This data structure is useful for fast folding on intervals of an array
+ * whose elements are elements of monoid I. Note that constructing this tree requires the identity
+ * element of I and the operation of I.
+ * Verified by: yukicoder No. 259 (http://yukicoder.me/submissions/100581)
+ *              AGC015-E (http://agc015.contest.atcoder.jp/submissions/1461001)
+ */
+struct SegTree<I, BiOp> {
+    n: usize,
+    dat: Vec<I>,
+    op: BiOp,
+    e: I,
+}
+
+impl<I, BiOp> SegTree<I, BiOp>
+    where BiOp: Fn(I, I) -> I,
+          I: Copy {
+    pub fn new(n_: usize, op: BiOp, e: I) -> Self {
+        let mut n = 1;
+        while n < n_ { n *= 2; } // n is a power of 2
+        SegTree {n: n, dat: vec![e; 2 * n - 1], op: op, e: e}
+    }
+    /* ary[k] <- v */
+    pub fn update(&mut self, idx: usize, v: I) {
+        let mut k = idx + self.n - 1;
+        self.dat[k] = v;
+        while k > 0 {
+            k = (k - 1) / 2;
+            self.dat[k] = (self.op)(self.dat[2 * k + 1], self.dat[2 * k + 2]);
+        }
+    }
+    /* [a, b) (note: half-inclusive)
+     * http://proc-cpuinfo.fixstars.com/2017/07/optimize-segment-tree/ */
+    pub fn query(&self, mut a: usize, mut b: usize) -> I {
+        let mut left = self.e;
+        let mut right = self.e;
+        a += self.n - 1;
+        b += self.n - 1;
+        while a < b {
+            if (a & 1) == 0 {
+                left = (self.op)(left, self.dat[a]);
+            }
+            if (b & 1) == 0 {
+                right = (self.op)(self.dat[b - 1], right);
+            }
+            a = a / 2;
+            b = (b - 1) / 2;
+        }
+        (self.op)(left, right)
+    }
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (let _ = write!(out,$($format)*););
+    }
+    input! {
+        n: usize,
+        abc: [(i64, i64, i64); n],
+    }
+    let mut abc: Vec<_> = abc.into_iter().enumerate()
+        .map(|(i, (a, b, c))| (a, b, c, i)).collect();
+    abc.sort();
+    let mut to = vec![0; n];
+    for i in 0..n {
+        let (_, b, c, _) = abc[i];
+        let diff = b - c;
+        let idx = abc.upper_bound(&(diff, 1 << 40, 0, 0));
+        to[i] = idx;
+    }
+    let mut st = SegTree::new(n, max, 0);
+    for i in 0..n {
+        st.update(i, to[i]);
+    }
+    let mut back = vec![false; n];
+    let mut back_acc = vec![false; n];
+    for i in 0..n {
+        if to[i] > i {
+            let ma = st.query(i + 1, to[i]);
+            if i < ma {
+                back[i] = true;
+            }
+        }
+        if i >= 1 && !back[i] {
+            if to[i] > 0 && back_acc[min(to[i], i) - 1] {
+                back[i] = true;
+            }
+        }
+        back_acc[i] = back[i] || (i >= 1 && back_acc[i - 1]);
+    }
+    let mut dp = vec![0; n];
+    let mut dpma = vec![0; n + 1];
+    for i in 0..n {
+        if back[i] { continue; }
+        let lim = to[i];
+        let mut ma = dpma[min(lim, i)] + abc[i].1;
+        if lim >= i {
+            for j in i + 1..lim {
+                assert!(to[j] <= i);
+                ma = max(ma, dpma[to[j]] + abc[i].1 + abc[j].1);
+            }
+        }
+        dp[i] = ma;
+        dpma[i + 1] = max(dpma[i], dp[i]);
+    }
+    let mut ans = vec![0; n];
+    for i in 0..n {
+        let idx = abc[i].3;
+        if back[i] {
+            ans[idx] = -1;
+        } else {
+            ans[idx] = dp[i];
+        }
+    }
+    for i in 0..n {
+        if ans[i] == -1 {
+            puts!("BAN\n");
+        } else {
+            puts!("{}\n", ans[i]);
+        }
+    }
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
