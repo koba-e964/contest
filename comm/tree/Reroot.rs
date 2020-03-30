5931c2f31bd656d12836ec4448088458365a15fd
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-03-31 02:05:40 +0900

    Add atcoder/abc160/f-lib.rs comm/tree/Reroot.rs

---
+++ comm/tree/Reroot.rs
@@ -0,0 +1,91 @@
+/// Rerooting.
+/// Verified by: ABC160-F
+/// (https://atcoder.jp/contests/abc160/submissions/11378396)
+/// f: merge
+/// d: deepen (adds a node to a collection of child nodes)
+struct Reroot<T> {
+    g: Vec<Vec<usize>>,
+    zero: T,
+    dp1: Vec<T>,
+    dp2: Vec<T>,
+    ch: Vec<Vec<usize>>,
+    acc_l: Vec<Vec<T>>,
+    acc_r: Vec<Vec<T>>,
+}
+
+impl<T: Copy> Reroot<T> {
+    pub fn new(g: &[Vec<usize>], zero: T) -> Reroot<T> {
+        let n = g.len();
+        Reroot {
+            g: g.to_vec(),
+            zero: zero.clone(),
+            dp1: vec![zero.clone(); n],
+            dp2: vec![zero.clone(); n],
+            ch: vec![vec![]; n],
+            acc_l: vec![vec![]; n],
+            acc_r: vec![vec![]; n],
+        }
+    }
+    // TODO include f in struct
+    pub fn do_comp<F: FnMut(T, T) -> T, D: FnMut(T) -> T>(
+        &mut self,
+        f: &mut F,
+        d: &mut D,
+    ) {
+        let n = self.g.len();
+        Self::dfs1(0, n, &self.g, &mut self.dp1, &mut self.ch,
+                   &mut self.acc_l, &mut self.acc_r, self.zero, f, d);
+        Self::dfs2(0, &self.ch, &self.dp1, &mut self.dp2,
+                   self.zero,
+                   &self.acc_l, &self.acc_r, self.zero, f, d);
+    }
+    fn dfs1<F: FnMut(T, T) -> T, D: FnMut(T) -> T>(
+        v: usize, par: usize, g: &[Vec<usize>],
+        dp1: &mut [T],
+        ch: &mut [Vec<usize>],
+        acc_l: &mut [Vec<T>], acc_r: &mut [Vec<T>],
+        zero: T,
+        f: &mut F,
+        d: &mut D,
+    ) {
+        let mut ary = vec![];
+        let mut mych = vec![];
+        for &w in &g[v] {
+            if w == par { continue; }
+            mych.push(w);
+            Self::dfs1(w, v, g, dp1, ch, acc_l, acc_r, zero.clone(), f, d);
+            ary.push(dp1[w].clone());
+        }
+        let m = ary.len();
+        acc_l[v] = vec![zero.clone(); m + 1];
+        acc_r[v] = vec![zero.clone(); m + 1];
+        for i in 0..m {
+            let val = f(acc_l[v][i], ary[i]);
+            acc_l[v][i + 1] = val;
+        }
+        for i in (0..m).rev() {
+            let val = f(acc_r[v][i + 1], ary[i]);
+            acc_r[v][i] = val;
+        }
+        ch[v] = mych;
+        dp1[v] = d(acc_r[v][0]);
+    }
+    fn dfs2<F: FnMut(T, T) -> T, D: FnMut(T) -> T>(
+        v: usize, ch: &[Vec<usize>],
+        dp1: &[T],
+        dp2: &mut [T],
+        passed: T,
+        acc_l: &[Vec<T>], acc_r: &[Vec<T>],
+        zero: T,
+        f: &mut F,
+        d: &mut D,
+    ) {
+        dp2[v] = passed;
+        for i in 0..ch[v].len() {
+            let w = ch[v][i];
+            let leave_one = f(acc_l[v][i], acc_r[v][i + 1]);
+            Self::dfs2(w, ch, dp1, dp2, d(f(leave_one, passed)), acc_l, acc_r,
+                       zero, f, d);
+        }
+    }
+}
