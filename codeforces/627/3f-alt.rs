2f2fbb7d8d4314138f7091202062046db680058b
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-04-01 13:13:20 +0900

    Add codeforces/627/3f-alt.rs

--- codeforces/627/3f-alt.rs
+++ codeforces/627/3f-alt.rs
@@ -1,8 +1,74 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    }};
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
 /// Rerooting.
 /// Verified by: ABC160-F
 /// (https://atcoder.jp/contests/abc160/submissions/11378396)
-/// CF627-3F
-/// (https://codeforces.com/contest/1324/submission/75037092)
 /// f: merge
 /// d: deepen (adds a node to a collection of child nodes)
 struct Reroot<T> {
@@ -97,4 +163,39 @@ impl<T: Copy> Reroot<T> {
                        zero, f, d);
         }
     }
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (let _ = write!(out,$($format)*););
+    }
+    input! {
+        n: usize,
+        a: [i32; n],
+        uv: [(usize1, usize1); n - 1],
+    }
+    let mut g = vec![vec![]; n];
+    for &(u, v) in &uv {
+        g[u].push(v);
+        g[v].push(u);
+    }
+    let diff: Vec<i64> =
+        a.iter().map(|&v| if v == 0 { -1 } else { 1 }).collect();
+    let mut reroot = Reroot::new(&g, 0);
+    let mut f = |x, y| max(0, x) + max(0, y);
+    let mut d = |x, v| x + diff[v];
+    reroot.do_comp(&mut f, &mut d);
+    for i in 0..n {
+        puts!("{}{}", reroot.dp[i],
+              if i + 1 == n { "\n" } else {" " });
+    }
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
 }
