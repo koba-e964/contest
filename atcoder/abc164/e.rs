8f33a3afbccb3edc6c8f2c833912f3c8e6bdeae1
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-04-27 18:47:33 +0900

    Renovate comm/graph/Dijkstra.rs

--- atcoder/abc164/e.rs
+++ atcoder/abc164/e.rs
@@ -75,36 +75,6 @@ struct Dijkstra {
     edges: Vec<Vec<(usize, i64)>>, // adjacent list representation
 }
 
-/*
- * Code from https://doc.rust-lang.org/std/collections/binary_heap/
- */
-#[derive(Copy, Clone, Eq, PartialEq)]
-struct State {
-    cost: i64,
-    position: usize,
-}
-
-// The priority queue depends on `Ord`.
-// Explicitly implement the trait so the queue becomes a min-heap
-// instead of a max-heap.
-impl Ord for State {
-    fn cmp(&self, other: &State) -> Ordering {
-        // Notice that the we flip the ordering here
-        match other.cost.cmp(&self.cost) {
-            std::cmp::Ordering::Equal => other.position.cmp(&self.position),
-            x => x,
-        }
-    }
-}
-
-// `PartialOrd` needs to be implemented as well.
-impl PartialOrd for State {
-    fn partial_cmp(&self, other: &State) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-
 impl Dijkstra {
     fn new(n: usize) -> Self {
         Dijkstra { edges: vec![Vec::new(); n] }
@@ -118,15 +88,21 @@ impl Dijkstra {
     fn solve(&self, source: usize, inf: i64) -> Vec<i64> {
         let n = self.edges.len();
         let mut d = vec![inf; n];
+        // que holds (-distance, vertex), so that que.pop() returns the nearest element.
         let mut que = std::collections::BinaryHeap::new();
-        que.push(State {cost: 0, position: source});
-        while let Some(State {cost, position: pos}) = que.pop() {
+        que.push((0, source));
+        while let Some((cost, pos)) = que.pop() {
+            let cost = -cost;
             if d[pos] <= cost {
                 continue;
             }
             d[pos] = cost;
-            for adj in &self.edges[pos] {
-                que.push(State {cost: cost + adj.1, position: adj.0});
+            for &(w, c) in &self.edges[pos] {
+                let newcost = cost + c;
+                if d[w] > newcost {
+                    d[w] = newcost + 1;
+                    que.push((-newcost, w));
+                }
             }
         }
         return d;
---
+++ comm/graph/Dijkstra.rs
@@ -0,0 +1,42 @@
+/*
+ * Dijkstra's algorithm.
+ * Verified by: AtCoder ABC164 (https://atcoder.jp/contests/abc164/submissions/12423853)
+ */
+
+struct Dijkstra {
+    edges: Vec<Vec<(usize, i64)>>, // adjacent list representation
+}
+
+impl Dijkstra {
+    fn new(n: usize) -> Self {
+        Dijkstra { edges: vec![Vec::new(); n] }
+    }
+    fn add_edge(&mut self, from: usize, to: usize, cost: i64) {
+        self.edges[from].push((to, cost));
+    }
+    /*
+     * This function returns a Vec consisting of the distances from vertex source.
+     */
+    fn solve(&self, source: usize, inf: i64) -> Vec<i64> {
+        let n = self.edges.len();
+        let mut d = vec![inf; n];
+        // que holds (-distance, vertex), so that que.pop() returns the nearest element.
+        let mut que = std::collections::BinaryHeap::new();
+        que.push((0, source));
+        while let Some((cost, pos)) = que.pop() {
+            let cost = -cost;
+            if d[pos] <= cost {
+                continue;
+            }
+            d[pos] = cost;
+            for &(w, c) in &self.edges[pos] {
+                let newcost = cost + c;
+                if d[w] > newcost {
+                    d[w] = newcost + 1;
+                    que.push((-newcost, w));
+                }
+            }
+        }
+        return d;
+    }
+}
