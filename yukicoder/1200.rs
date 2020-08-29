a6f4cb361cae16fca53cb34d30f6877e130d9203
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-08-29 15:53:04 +0900

    Fix yukicoder/1200.rs

--- yukicoder/1200.rs
+++ yukicoder/1200.rs
@@ -65,22 +65,159 @@ macro_rules! debugln {
     ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
 }
 
-fn divs(x: i64) -> Vec<i64> {
-    let mut i = 1;
-    let mut ans = vec![];
-    while i * i <= x {
-        if x % i == 0 {
-            ans.push(i);
-            if i * i != x {
-                ans.push(x / i);
+/// https://judge.yosupo.jp/submission/5155
+mod pollard_rho {
+    use std::collections::HashMap;
+    /// binary gcd
+    pub fn gcd(mut x: i64, mut y: i64) -> i64 {
+        if y == 0 { return x; }
+        if x == 0 { return y; }
+        let k = (x | y).trailing_zeros();
+        y >>= k;
+        x >>= x.trailing_zeros();
+        while y != 0 {
+            y >>= y.trailing_zeros();
+            if x > y { let t = x; x = y; y = t; }
+            y -= x;
+        }
+        x << k
+    }
+
+    fn add_mod(x: i64, y: i64, n: i64) -> i64 {
+        let z = x + y;
+        if z >= n { z - n } else { z }
+    }
+
+    fn mul_mod(x: i64, mut y: i64, n: i64) -> i64 {
+        assert!(x >= 0);
+        assert!(x < n);
+        let mut sum = 0;
+        let mut cur = x;
+        while y > 0 {
+            if (y & 1) == 1 {
+                sum = add_mod(sum, cur, n);
+            }
+            cur = add_mod(cur, cur, n);
+            y >>= 1;
+        }
+        sum
+    }
+
+    fn mod_pow(x: i64, mut e: i64, n: i64) -> i64 {
+        let mut prod = if n == 1 { 0 } else { 1 };
+        let mut cur = x % n;
+        while e > 0 {
+            if (e & 1) == 1 {
+                prod = mul_mod(prod, cur, n);
+            }
+            e >>= 1;
+            if e > 0 {
+                cur = mul_mod(cur, cur, n);
             }
         }
-        i += 1;
+        prod
     }
-    ans
+
+    pub fn is_prime(n: i64) -> bool {
+        if n <= 1 { return false; }
+        let small = [2, 3, 5, 7, 11, 13];
+        if small.iter().any(|&u| u == n) { return true; }
+        if small.iter().any(|&u| n % u == 0) { return false; }
+        let mut d = n - 1;
+        let e = d.trailing_zeros();
+        d >>= e;
+        // https://miller-rabin.appspot.com/
+        let a = [2, 325, 9375, 28178, 450775, 9780504, 1795265022];
+        a.iter().all(|&a| {
+            if a % n == 0 { return true; }
+            let mut x = mod_pow(a, d, n);
+            if x == 1 { return true; }
+            for _ in 0 .. e {
+                if x == n - 1 {
+                    return true;
+                }
+                x = mul_mod(x, x, n);
+                if x == 1 { return false; }
+            }
+            x == 1
+        })
+    }
+
+    fn pollard_rho(n: i64, c: &mut i64) -> i64 {
+        // An improvement with Brent's cycle detection algorithm is performed.
+        // https://maths-people.anu.edu.au/~brent/pub/pub051.html
+        if n % 2 == 0 { return 2; }
+        loop {
+            let mut x: i64; // tortoise
+            let mut y = 2; // hare
+            let mut d = 1;
+            let cc = *c;
+            let f = |i| add_mod(mul_mod(i, i, n), cc, n);
+            let mut r = 1;
+            // We don't perform the gcd-once-in-a-while optimization
+            // because the plain gcd-every-time algorithm appears to
+            // outperform, at least on judge.yosupo.jp :)
+            while d == 1 {
+                x = y;
+                for _ in 0..r {
+                    y = f(y);
+                    d = gcd((x - y).abs(), n);
+                    if d != 1 {
+                        break;
+                    }
+                }
+                r *= 2;
+            }
+            if d == n {
+                *c += 1;
+                continue;
+            }
+            return d;
+        }
+    }
+
+    /// Outputs (p, e) in p's ascending order.
+    pub fn factorize(x: i64) -> Vec<(i64, usize)> {
+        if x <= 1 {
+            return Vec::new();
+        }
+        let mut hm = HashMap::new();
+        let mut pool = vec![x];
+        let mut c = 1;
+        while let Some(u) = pool.pop() {
+            if is_prime(u) {
+                *hm.entry(u).or_insert(0) += 1;
+                continue;
+            }
+            let p = pollard_rho(u, &mut c);
+            pool.push(p);
+            pool.push(u / p);
+        }
+        let mut v: Vec<_> = hm.into_iter().collect();
+        v.sort();
+        v
+    }
+} // mod pollard_rho
+
+fn divs_dfs<F>(pe: &[(i64, usize)], x: i64, f: &mut F) where F: FnMut(i64) {
+    if pe.is_empty() {
+        f(x);
+        return;
+    }
+    let (p, e) = pe[0];
+    let mut x = x;
+    for _ in 0..e + 1 {
+        divs_dfs(&pe[1..], x, f);
+        x *= p;
+    }
 }
 
-fn solve() {
+fn divs<F>(x: i64, f: &mut F) where F: FnMut(i64) {
+    let pe = pollard_rho::factorize(x);
+    divs_dfs(&pe, 1, f);
+}
+
+fn main() {
     let out = std::io::stdout();
     let mut out = BufWriter::new(out.lock());
     macro_rules! puts {
@@ -93,37 +230,28 @@ fn solve() {
         let dif = (x - y).abs();
         if dif == 0 {
             let mut ans = x - 1;
-            let ds = divs(x);
-            for d in ds {
+            divs(x, &mut |d|{
                 if d > 2 {
                     ans += 1;
                 }
-            }
+            });
             puts!("{}\n", ans);
             continue;
         }
-        let divs = divs(dif);
         let mut ans = 0;
-        for d in divs {
+        divs(dif, &mut |d| {
             let a = d + 1;
             let bc = (x - y) / d;
             let xbc = x + bc;
             if xbc % (a + 1) != 0 {
-                continue;
+                return;
             }
             let b = xbc / (a + 1);
             let c = b - bc;
             if b > 0 && c > 0 {
                 ans += 1;
             }
-        }
+        });
         puts!("{}\n", ans);
     }
-}
-
-fn main() {
-    // In order to avoid potential stack overflow, spawn a new thread.
-    let stack_size = 104_857_600; // 100 MB
-    let thd = std::thread::Builder::new().stack_size(stack_size);
-    thd.spawn(|| solve()).unwrap().join().unwrap();
 }
