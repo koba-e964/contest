535d06ebc7c9bfae13526bd332a6844ff3814f9f
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-08-31 07:16:58 +0900

    Add yukicoder/1205.rs yukicoder/1206.rs yukicoder/1207.rs yukicoder/1208.rs yukicoder/1209.rs yukicoder/1210.rs yukicoder/1211.rs

---
+++ yukicoder/1211.rs
@@ -0,0 +1,165 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, [graph1; $len:expr]) => {{
+        let mut g = vec![vec![]; $len];
+        let ab = read_value!($next, [(usize1, usize1)]);
+        for (a, b) in ab {
+            g[a].push(b);
+            g[b].push(a);
+        }
+        g
+    }};
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
+trait Bisect<T> {
+    fn lower_bound(&self, val: &T) -> usize;
+    fn upper_bound(&self, val: &T) -> usize;
+}
+
+impl<T: Ord> Bisect<T> for [T] {
+    fn lower_bound(&self, val: &T) -> usize {
+        let mut pass = self.len() + 1;
+        let mut fail = 0;
+        while pass - fail > 1 {
+            let mid = (pass + fail) / 2;
+            if &self[mid - 1] >= val {
+                pass = mid;
+            } else {
+                fail = mid;
+            }
+        }
+        pass - 1
+    }
+    fn upper_bound(&self, val: &T) -> usize {
+        let mut pass = self.len() + 1;
+        let mut fail = 0;
+        while pass - fail > 1 {
+            let mid = (pass + fail) / 2;
+            if &self[mid - 1] > val {
+                pass = mid;
+            } else {
+                fail = mid;
+            }
+        }
+        pass - 1
+    }
+}
+
+const B: usize = 17;
+
+fn check(x: i64, a: &[i64], acc: &[i64], k: usize) -> bool {
+    let n = a.len();
+    let mut dp = vec![vec![0; 2 * n + 1]; B];
+    for i in 0..n {
+        let idx = acc.lower_bound(&(acc[i] + x));
+        dp[0][i] = idx;
+        dp[0][i + n] = min(idx + n, 2 * n);
+    }
+    dp[0][2 * n] = 2 * n;
+    for i in 0..B - 1 {
+        for j in 0..2 * n + 1 {
+            dp[i + 1][j] = dp[i][dp[i][j]];
+        }
+    }
+    for i in 0..n {
+        let mut to = i;
+        for j in 0..B {
+            if (k & 1 << j) != 0 {
+                to = dp[j][to];
+            }
+        }
+        if to <= i + n {
+            return true;
+        }
+    }
+    false
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (let _ = write!(out,$($format)*););
+    }
+    input! {
+        n: usize, k: usize,
+        a: [i64; n],
+    }
+    let sum: i64 = a.iter().sum();
+    let mut pass = 0;
+    let mut fail = sum / k as i64 + 1;
+    let mut acc = vec![0; 2 * n + 1];
+    for i in 0..2 * n {
+        acc[i + 1] = acc[i] + a[i % n];
+    }
+    while fail - pass > 1 {
+        let mid = (fail + pass) / 2;
+        if check(mid, &a, &acc, k) {
+            pass = mid;
+        } else {
+            fail = mid;
+        }
+    }
+    puts!("{}\n", pass);
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
