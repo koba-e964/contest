b8cf4477c53cbebbc2567555e1c572653e42c0bd
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-09-21 19:21:58 +0900

    Add comm/rng.rs

--- codeforces/524/2e.rs
+++ codeforces/524/2e.rs
@@ -57,6 +57,29 @@ macro_rules! read_value {
     };
 }
 
+struct Rng {
+    x: u64,
+}
+
+impl Rng {
+    fn new() -> Self {
+        use std::hash::{Hasher, BuildHasher};
+        let hm: HashMap<i32, i32> = HashMap::new();
+        let mut hash = hm.hasher().build_hasher();
+        hash.write_u32(8128);
+        Rng {
+            x: hash.finish(),
+        }
+    }
+    fn next(&mut self) -> u64 {
+        let a = 0xdead_c0de_0013_3331u64;
+        let b = 2457;
+        self.x = self.x.wrapping_mul(a).wrapping_add(b);
+        let x = self.x;
+        x ^ x << 10
+    }
+}
+
 fn solve() {
     let out = std::io::stdout();
     let mut out = BufWriter::new(out.lock());
@@ -73,18 +96,9 @@ fn solve() {
         x.into_iter().map(|y| ((y as u8) - b'a') as usize).collect()
     }).collect();
     let mut rnd = vec![0u64; 26];
-    {
-        use std::hash::{Hasher, BuildHasher};
-        let a = 0xdead_c0de_0013_3331;
-        let b = 2457;
-        let hm: HashMap<i32, i32> = HashMap::new();
-        let mut hash = hm.hasher().build_hasher();
-        hash.write_u32(8128);
-        let mut x: u64 = hash.finish();
-        for i in 0 .. 26 {
-            x = x.wrapping_mul(a).wrapping_add(b);
-            rnd[i] = x ^ x << 10;
-        }
+    let mut rng = Rng::new();
+    for i in 0..26 {
+        rnd[i] = rng.next();
     }
     let mut tot = 0i64;
     for j in 0 .. m {
---
+++ comm/rng.rs
@@ -0,0 +1,23 @@
+struct Rng {
+    x: u64,
+}
+
+impl Rng {
+    fn new() -> Self {
+        use std::collections::HashMap;
+        use std::hash::{Hasher, BuildHasher};
+        let hm: HashMap<i32, i32> = HashMap::new();
+        let mut hash = hm.hasher().build_hasher();
+        hash.write_u32(8128);
+        Rng {
+            x: hash.finish(),
+        }
+    }
+    fn next(&mut self) -> u64 {
+        let a = 0xdead_c0de_0013_3331u64;
+        let b = 2457;
+        self.x = self.x.wrapping_mul(a).wrapping_add(b);
+        let x = self.x;
+        x ^ x << 10
+    }
+}
