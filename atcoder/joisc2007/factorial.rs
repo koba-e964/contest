d4a6c32680f51cedfd4e34e304e2fe9962d838dc
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-12-12 03:33:57 +0900

    Add atcoder/joisc2007/factorial.rs atcoder/joisc2007/mall.rs atcoder/joisc2007/score.rs

---
+++ atcoder/joisc2007/factorial.rs
@@ -0,0 +1,249 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, [graph1; $len:expr]) => {{
+        let mut g = vec![vec![]; $len];
+        let ab = read_value!($next, [(usize1, usize1)]);
+        for (a, b) in ab {
+            g[a].push(b);
+            g[b].push(a);
+        }
+        g
+    }};
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
+/// https://judge.yosupo.jp/submission/5155
+mod pollard_rho {
+    use std::collections::HashMap;
+    /// binary gcd
+    pub fn gcd(mut x: i64, mut y: i64) -> i64 {
+        if y == 0 { return x; }
+        if x == 0 { return y; }
+        let k = (x | y).trailing_zeros();
+        y >>= k;
+        x >>= x.trailing_zeros();
+        while y != 0 {
+            y >>= y.trailing_zeros();
+            if x > y { let t = x; x = y; y = t; }
+            y -= x;
+        }
+        x << k
+    }
+
+    fn add_mod(x: i64, y: i64, n: i64) -> i64 {
+        let z = x + y;
+        if z >= n { z - n } else { z }
+    }
+
+    fn mul_mod(x: i64, mut y: i64, n: i64) -> i64 {
+        assert!(x >= 0);
+        assert!(x < n);
+        let mut sum = 0;
+        let mut cur = x;
+        while y > 0 {
+            if (y & 1) == 1 {
+                sum = add_mod(sum, cur, n);
+            }
+            cur = add_mod(cur, cur, n);
+            y >>= 1;
+        }
+        sum
+    }
+
+    fn mod_pow(x: i64, mut e: i64, n: i64) -> i64 {
+        let mut prod = if n == 1 { 0 } else { 1 };
+        let mut cur = x % n;
+        while e > 0 {
+            if (e & 1) == 1 {
+                prod = mul_mod(prod, cur, n);
+            }
+            e >>= 1;
+            if e > 0 {
+                cur = mul_mod(cur, cur, n);
+            }
+        }
+        prod
+    }
+
+    pub fn is_prime(n: i64) -> bool {
+        if n <= 1 { return false; }
+        let small = [2, 3, 5, 7, 11, 13];
+        if small.iter().any(|&u| u == n) { return true; }
+        if small.iter().any(|&u| n % u == 0) { return false; }
+        let mut d = n - 1;
+        let e = d.trailing_zeros();
+        d >>= e;
+        // https://miller-rabin.appspot.com/
+        let a = [2, 325, 9375, 28178, 450775, 9780504, 1795265022];
+        a.iter().all(|&a| {
+            if a % n == 0 { return true; }
+            let mut x = mod_pow(a, d, n);
+            if x == 1 { return true; }
+            for _ in 0 .. e {
+                if x == n - 1 {
+                    return true;
+                }
+                x = mul_mod(x, x, n);
+                if x == 1 { return false; }
+            }
+            x == 1
+        })
+    }
+
+    fn pollard_rho(n: i64, c: &mut i64) -> i64 {
+        // An improvement with Brent's cycle detection algorithm is performed.
+        // https://maths-people.anu.edu.au/~brent/pub/pub051.html
+        if n % 2 == 0 { return 2; }
+        loop {
+            let mut x: i64; // tortoise
+            let mut y = 2; // hare
+            let mut d = 1;
+            let cc = *c;
+            let f = |i| add_mod(mul_mod(i, i, n), cc, n);
+            let mut r = 1;
+            // We don't perform the gcd-once-in-a-while optimization
+            // because the plain gcd-every-time algorithm appears to
+            // outperform, at least on judge.yosupo.jp :)
+            while d == 1 {
+                x = y;
+                for _ in 0..r {
+                    y = f(y);
+                    d = gcd((x - y).abs(), n);
+                    if d != 1 {
+                        break;
+                    }
+                }
+                r *= 2;
+            }
+            if d == n {
+                *c += 1;
+                continue;
+            }
+            return d;
+        }
+    }
+
+    /// Outputs (p, e) in p's ascending order.
+    pub fn factorize(x: i64) -> Vec<(i64, usize)> {
+        if x <= 1 {
+            return Vec::new();
+        }
+        let mut hm = HashMap::new();
+        let mut pool = vec![x];
+        let mut c = 1;
+        while let Some(u) = pool.pop() {
+            if is_prime(u) {
+                *hm.entry(u).or_insert(0) += 1;
+                continue;
+            }
+            let p = pollard_rho(u, &mut c);
+            pool.push(p);
+            pool.push(u / p);
+        }
+        let mut v: Vec<_> = hm.into_iter().collect();
+        v.sort();
+        v
+    }
+} // mod pollard_rho
+
+fn ord(p: i64, mut n: i64) -> i64 {
+    let mut s = 0;
+    while n > 0 {
+        n /= p;
+        s += n;
+    }
+    s
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (let _ = write!(out,$($format)*););
+    }
+    #[allow(unused)]
+    macro_rules! putvec {
+        ($v:expr) => {
+            for i in 0..$v.len() {
+                puts!("{}{}", $v[i], if i + 1 == $v.len() {"\n"} else {" "});
+            }
+        }
+    }
+    input! {
+        n: i64,
+    }
+    let pe = pollard_rho::factorize(n);
+    let mut ma = 1;
+    for (p, e) in pe {
+        let mut lo = 0;
+        let mut hi = e as i64;
+        while hi - lo > 1 {
+            let mid = (hi + lo) / 2;
+            if ord(p, p * mid) >= e as i64 {
+                hi = mid;
+            } else {
+                lo = mid;
+            }
+        }
+        ma = max(ma, hi * p);
+    }
+    puts!("{}\n", ma);
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
