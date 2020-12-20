49eca92f61fe4e1753510039fb71541ae87e6f3d
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-12-20 10:54:06 +0900

    Add atcoder/abc186/a.rs atcoder/abc186/b.rs atcoder/abc186/c.rs atcoder/abc186/d.rs atcoder/abc186/e.rs atcoder/abc186/f.rs

---
+++ atcoder/abc186/f.rs
@@ -0,0 +1,202 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, [graph1; $len:expr]) => {{
+        let mut g = vec![vec![]; $len];
+        let ab = read_value!($next, [(usize1, usize1)]);
+        for (a, b) in ab {
+            g[a].push(b);
+            g[b].push(a);
+        }
+        g
+    }};
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+trait Change {
+    fn chmax(&mut self, x: Self);
+    fn chmin(&mut self, x: Self);
+}
+impl<T: PartialOrd> Change for T {
+    fn chmax(&mut self, x: T) {
+        if *self < x { *self = x; }
+    }
+    fn chmin(&mut self, x: T) {
+        if *self > x { *self = x; }
+    }
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
+/**
+ * Segment Tree. This data structure is useful for fast folding on intervals of an array
+ * whose elements are elements of monoid I. Note that constructing this tree requires the identity
+ * element of I and the operation of I.
+ * Verified by: yukicoder No. 259 (http://yukicoder.me/submissions/100581)
+ *              AGC015-E (http://agc015.contest.atcoder.jp/submissions/1461001)
+ */
+struct SegTree<I, BiOp> {
+    n: usize,
+    dat: Vec<I>,
+    op: BiOp,
+    e: I,
+}
+
+impl<I, BiOp> SegTree<I, BiOp>
+    where BiOp: Fn(I, I) -> I,
+          I: Copy {
+    pub fn new(n_: usize, op: BiOp, e: I) -> Self {
+        let mut n = 1;
+        while n < n_ { n *= 2; } // n is a power of 2
+        SegTree {n: n, dat: vec![e; 2 * n - 1], op: op, e: e}
+    }
+    /* ary[k] <- v */
+    pub fn update(&mut self, idx: usize, v: I) {
+        let mut k = idx + self.n - 1;
+        self.dat[k] = v;
+        while k > 0 {
+            k = (k - 1) / 2;
+            self.dat[k] = (self.op)(self.dat[2 * k + 1], self.dat[2 * k + 2]);
+        }
+    }
+    /* [a, b) (note: half-inclusive)
+     * http://proc-cpuinfo.fixstars.com/2017/07/optimize-segment-tree/ */
+    pub fn query(&self, mut a: usize, mut b: usize) -> I {
+        let mut left = self.e;
+        let mut right = self.e;
+        a += self.n - 1;
+        b += self.n - 1;
+        while a < b {
+            if (a & 1) == 0 {
+                left = (self.op)(left, self.dat[a]);
+            }
+            if (b & 1) == 0 {
+                right = (self.op)(self.dat[b - 1], right);
+            }
+            a = a / 2;
+            b = (b - 1) / 2;
+        }
+        (self.op)(left, right)
+    }
+}
+
+fn calc(h: usize, w: usize, xy: &[(usize, usize)]) -> Vec<usize> {
+    let mut buc = vec![h; w];
+    let mut mi = w;
+    for &(x, y) in xy {
+        if x == 0 {
+            mi = min(mi, y);
+        }
+    }
+    for &(x, y) in xy {
+        buc[y] = min(buc[y], x);
+    }
+    for i in mi..w {
+        buc[i] = 0;
+    }
+    buc
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (let _ = write!(out,$($format)*););
+    }
+    #[allow(unused)]
+    macro_rules! putvec {
+        ($v:expr) => {
+            for i in 0..$v.len() {
+                puts!("{}{}", $v[i], if i + 1 == $v.len() {"\n"} else {" "});
+            }
+        }
+    }
+    input! {
+        h: usize, w: usize, m: usize,
+        xy: [(usize1, usize1); m],
+    }
+    let col = calc(h, w, &xy);
+    let yx: Vec<_> = xy.iter().map(|&(x, y)| (y, x)).collect();
+    let row = calc(w, h, &yx);/*
+    eprintln!("row = {:?}", row);
+    eprintln!("col = {:?}", col);*/
+    let mut tot: i64 = 0;
+    for i in 0..h {
+        tot += row[i] as i64;
+    }
+    for i in 0..w {
+        tot += col[i] as i64;
+    }
+    let mut st = SegTree::new(w, |x, y| x + y, 0);
+    let mut cs = vec![vec![]; h + 1];
+    for i in 0..w {
+        cs[col[i]].push(i);
+    }
+    for &x in &cs[h] {
+        let val = st.query(x, x + 1);
+        st.update(x, val + 1);
+    }
+    for i in (0..h).rev() {
+        tot -= st.query(0, row[i]);
+        for &x in &cs[i] {
+            let val = st.query(x, x + 1);
+            st.update(x, val + 1);
+        }
+    }
+    puts!("{}\n", tot);
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
