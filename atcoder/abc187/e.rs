03f87ad1f40e5d864d1f11731e8e6c0ac5572ba9
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-01-03 16:57:49 +0900

    Add atcoder/abc187/a.rs atcoder/abc187/b.rs atcoder/abc187/c.rs atcoder/abc187/d.rs atcoder/abc187/e.rs atcoder/abc187/f.rs

---
+++ atcoder/abc187/e.rs
@@ -0,0 +1,155 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, [graph1; $len:expr]) => {{
+        let mut g = vec![vec![]; $len];
+        let ab = read_value!($next, [(usize1, usize1)]);
+        for (a, b) in ab {
+            g[a].push(b);
+            g[b].push(a);
+        }
+        g
+    }};
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+trait Change {
+    fn chmax(&mut self, x: Self);
+    fn chmin(&mut self, x: Self);
+}
+impl<T: PartialOrd> Change for T {
+    fn chmax(&mut self, x: T) {
+        if *self < x { *self = x; }
+    }
+    fn chmin(&mut self, x: T) {
+        if *self > x { *self = x; }
+    }
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
+fn dfs1(g: &[Vec<usize>], v: usize, p: usize, par: &mut [usize]) {
+    par[v] = p;
+    for &w in &g[v] {
+        if w == p {
+            continue;
+        }
+        dfs1(g, w, v, par);
+    }
+}
+
+fn dfs2(g: &[Vec<usize>], v: usize, p: usize, desc: &[i64], ans: &mut [i64],
+        mut x: i64) {
+    x += desc[v];
+    ans[v] += x;
+    for &w in &g[v] {
+        if w == p {
+            continue;
+        }
+        dfs2(g, w, v, desc, ans, x);
+    }
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (let _ = write!(out,$($format)*););
+    }
+    #[allow(unused)]
+    macro_rules! putvec {
+        ($v:expr) => {
+            for i in 0..$v.len() {
+                puts!("{}{}", $v[i], if i + 1 == $v.len() {"\n"} else {" "});
+            }
+        }
+    }
+    input! {
+        n: usize,
+        ab: [(usize1, usize1); n - 1],
+        tex: [(i32, usize1, i64)],
+    }
+    let mut g = vec![vec![]; n];
+    for &(a, b) in &ab {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    let mut par = vec![0; n];
+    let mut gl = 0;
+    let mut desc = vec![0; n];
+    dfs1(&g, 0, n, &mut par);
+    for (t, e, x) in tex {
+        let (a, b) = ab[e];
+        let (a, b) = if t == 1 {
+            (a, b)
+        } else {
+            (b, a)
+        };
+        if par[a] == b {
+            desc[a] += x;
+        } else {
+            desc[b] -= x;
+            gl += x;
+        }
+    }
+    let mut ans = vec![gl; n];
+    dfs2(&g, 0, n, &desc, &mut ans, 0);
+    for i in 0..n {
+        puts!("{}\n", ans[i]);
+    }
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
---
+++ atcoder/abc187/f.rs
@@ -0,0 +1,159 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, [graph1; $len:expr]) => {{
+        let mut g = vec![vec![]; $len];
+        let ab = read_value!($next, [(usize1, usize1)]);
+        for (a, b) in ab {
+            g[a].push(b);
+            g[b].push(a);
+        }
+        g
+    }};
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+trait Change {
+    fn chmax(&mut self, x: Self);
+    fn chmin(&mut self, x: Self);
+}
+impl<T: PartialOrd> Change for T {
+    fn chmax(&mut self, x: T) {
+        if *self < x { *self = x; }
+    }
+    fn chmin(&mut self, x: T) {
+        if *self > x { *self = x; }
+    }
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
+/// Generates an Iterator over subsets of univ, in the descending order. 
+/// Verified by: http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3050308
+struct SubsetIter { bits: Option<usize>, univ: usize }
+impl Iterator for SubsetIter {
+    type Item = usize;
+    fn next(&mut self) -> Option<usize> {
+        match self.bits {
+            None => None,
+            Some(bits) => {
+                let ans = bits;
+                self.bits =
+                    if bits == 0 { None }
+                else { Some((bits - 1) & self.univ) };
+                Some(ans)
+            }
+        }
+    }
+}
+fn subsets(univ: usize) -> SubsetIter {
+    SubsetIter { bits: Some(univ), univ: univ }
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (let _ = write!(out,$($format)*););
+    }
+    #[allow(unused)]
+    macro_rules! putvec {
+        ($v:expr) => {
+            for i in 0..$v.len() {
+                puts!("{}{}", $v[i], if i + 1 == $v.len() {"\n"} else {" "});
+            }
+        }
+    }
+    input! {
+        n: usize,
+        ab: [(usize1, usize1)],
+    }
+    let mut g = vec![0; n];
+    for &(a, b) in &ab {
+        g[a] |= 1 << b;
+        g[b] |= 1 << a;
+    }
+    let mut cli = vec![false; 1 << n];
+    for bits in 1..1 << n {
+        let mut ok = true;
+        for i in 0..n {
+            if (bits & 1 << i) == 0 {
+                continue;
+            }
+            if ((g[i] | 1 << i) & bits) != bits {
+                ok = false;
+            }
+        }
+        cli[bits] = ok;
+    }
+    const INF: usize = 1 << 30;
+    let mut dp = vec![INF; 1 << n];
+    dp[0] = 0;
+    for bits in 1..1 << n {
+        for sub in subsets(bits) {
+            if sub == 0 {
+                continue;
+            }
+            if dp[bits - sub] < INF && cli[sub] {
+                let val = dp[bits - sub] + 1;
+                dp[bits].chmin(val);
+            }
+        }
+    }
+    puts!("{}\n", dp[(1 << n) - 1]);
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
