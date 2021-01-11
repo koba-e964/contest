509777fd0b1702a4ddcd21861c26133c090fc9eb
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-01-11 12:57:55 +0900

    Add atcoder/abc188/a.rs atcoder/abc188/b.rs atcoder/abc188/c.rs atcoder/abc188/d.rs atcoder/abc188/e.rs atcoder/abc188/f.rs

---
+++ atcoder/abc188/f.rs
@@ -0,0 +1,136 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, [graph1; $len:expr]) => {{
+        let mut g = vec![vec![]; $len];
+        let ab = read_value!($next, [(usize1, usize1)]);
+        for (a, b) in ab {
+            g[a].push(b);
+            g[b].push(a);
+        }
+        g
+    }};
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+trait Change {
+    fn chmax(&mut self, x: Self);
+    fn chmin(&mut self, x: Self);
+}
+impl<T: PartialOrd> Change for T {
+    fn chmax(&mut self, x: T) {
+        if *self < x { *self = x; }
+    }
+    fn chmin(&mut self, x: T) {
+        if *self > x { *self = x; }
+    }
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
+// \pm 1, ..., \pm 2^n
+fn calc(del: i64, n: i64, memo: &mut HashMap<(i64, i64), i64>) -> i64 {
+    if n == 0 {
+        return del.abs();
+    }
+    if let Some(&val) = memo.get(&(del, n)) {
+        return val;
+    }
+    if del % 2 == 0 {
+        let val = calc(del / 2, n - 1, memo);
+        memo.insert((del, n), val);
+        return val;
+    }
+    let mut val = del.abs();
+    val.chmin(calc((del - 1) / 2, n - 1, memo) + 1);
+    val.chmin(calc((del + 1) / 2, n - 1, memo) + 1);
+    memo.insert((del, n), val);
+    val
+}
+
+// Tags: limited-state-space
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (let _ = write!(out,$($format)*););
+    }
+    #[allow(unused)]
+    macro_rules! putvec {
+        ($v:expr) => {
+            for i in 0..$v.len() {
+                puts!("{}{}", $v[i], if i + 1 == $v.len() {"\n"} else {" "});
+            }
+        }
+    }
+    input! {
+        x: i64, y: i64,
+    }
+    let mut mi = (x - y).abs();
+    let mut nx = x;
+    let mut memo = HashMap::new();
+    for i in 0..61 {
+        let del = y - nx;
+        mi.chmin(i + calc(del, i, &mut memo));
+        if nx >= y {
+            break;
+        }
+        nx *= 2;
+    }
+    puts!("{}\n", mi);
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
