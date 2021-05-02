b32ab692d2ac671b2c2c9976621de106469032ff
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-05-02 22:00:55 +0900

    Add atcoder/abc199/a.rs atcoder/abc199/b.rs atcoder/abc199/c.rs atcoder/abc199/d.rs atcoder/abc199/e.rs atcoder/abc199/f.rs comm/matrix-mint.rs

---
+++ atcoder/abc199/c.rs
@@ -0,0 +1,125 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, [graph1; $len:expr]) => {{
+        let mut g = vec![vec![]; $len];
+        let ab = read_value!($next, [(usize1, usize1)]);
+        for (a, b) in ab {
+            g[a].push(b);
+            g[b].push(a);
+        }
+        g
+    }};
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+trait Change {
+    fn chmax(&mut self, x: Self);
+    fn chmin(&mut self, x: Self);
+}
+impl<T: PartialOrd> Change for T {
+    fn chmax(&mut self, x: T) {
+        if *self < x { *self = x; }
+    }
+    fn chmin(&mut self, x: T) {
+        if *self > x { *self = x; }
+    }
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (let _ = write!(out,$($format)*););
+    }
+    #[allow(unused)]
+    macro_rules! putvec {
+        ($v:expr) => {
+            for i in 0..$v.len() {
+                puts!("{}{}", $v[i], if i + 1 == $v.len() {"\n"} else {" "});
+            }
+        }
+    }
+    input! {
+        n: usize,
+        s: chars,
+        tab: [(i32, usize, usize)],
+    }
+    let mut s = s;
+    let mut sh = false;
+    for (t, a, b) in tab {
+        if t == 2 {
+            sh = !sh;
+        } else {
+            let (a, b) = if sh {
+                ((a + n - 1) % (2 * n), (b + n - 1) % (2 * n))
+            } else {
+                (a - 1, b - 1)
+            };
+            s.swap(a, b);
+        }
+    }
+    if sh {
+        let mut t = s[n..].to_vec();
+        t.extend_from_slice(&s[..n]);
+        s = t;
+    }
+    puts!("{}\n", s.into_iter().collect::<String>());
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
---
+++ comm/matrix-mint.rs
@@ -0,0 +1,29 @@
+// Depends on MInt.rs
+// Verified by: https://atcoder.jp/contests/abc199/submissions/22259436
+fn squmul(a: &[Vec<MInt>], b: &[Vec<MInt>]) -> Vec<Vec<MInt>> {
+    let n = a.len();
+    let mut ret = vec![vec![MInt::new(0); n]; n];
+    for i in 0..n {
+        for j in 0..n {
+            for k in 0..n {
+                ret[i][k] += a[i][j] * b[j][k];
+            }
+        }
+    }
+    ret
+}
+
+fn squpow(a: &[Vec<MInt>], mut e: i64) -> Vec<Vec<MInt>> {
+    let n = a.len();
+    let mut sum = vec![vec![MInt::new(0); n]; n];
+    for i in 0..n { sum[i][i] = 1.into(); }
+    let mut cur = a.to_vec();
+    while e > 0 {
+        if e % 2 == 1 {
+            sum = squmul(&sum, &cur);
+        }
+        cur = squmul(&cur, &cur);
+        e /= 2;
+    }
+    sum
+}
