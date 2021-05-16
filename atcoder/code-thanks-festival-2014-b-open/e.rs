e0b8ed608a0d51b57154e51e44d3b688ca4ecc3c
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-05-16 14:22:39 +0900

    Add atcoder/code-thanks-festival-2014-b-open/a.rs atcoder/code-thanks-festival-2014-b-open/b.rs atcoder/code-thanks-festival-2014-b-open/c.rs atcoder/code-thanks-festival-2014-b-open/d.rs atcoder/code-thanks-festival-2014-b-open/e.rs atcoder/code-thanks-festival-2014-b-open/f.rs atcoder/code-thanks-festival-2014-b-open/g.rs atcoder/code-thanks-festival-2014-b-open/h.rs

---
+++ atcoder/code-thanks-festival-2014-b-open/e.rs
@@ -0,0 +1,142 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+trait Change { fn chmax(&mut self, x: Self); fn chmin(&mut self, x: Self); }
+impl<T: PartialOrd> Change for T {
+    fn chmax(&mut self, x: T) { if *self < x { *self = x; } }
+    fn chmin(&mut self, x: T) { if *self > x { *self = x; } }
+}
+
+/**
+ * Union-Find tree.
+ * Verified by https://atcoder.jp/contests/pakencamp-2019-day3/submissions/9253305
+ */
+struct UnionFind { disj: Vec<usize>, rank: Vec<usize> }
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        let disj = (0..n).collect();
+        UnionFind { disj: disj, rank: vec![1; n] }
+    }
+    fn root(&mut self, x: usize) -> usize {
+        if x != self.disj[x] {
+            let par = self.disj[x];
+            let r = self.root(par);
+            self.disj[x] = r;
+        }
+        self.disj[x]
+    }
+    fn unite(&mut self, x: usize, y: usize) {
+        let mut x = self.root(x);
+        let mut y = self.root(y);
+        if x == y { return }
+        if self.rank[x] > self.rank[y] {
+            std::mem::swap(&mut x, &mut y);
+        }
+        self.disj[x] = y;
+        self.rank[y] += self.rank[x];
+    }
+    #[allow(unused)]
+    fn is_same_set(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+    #[allow(unused)]
+    fn size(&mut self, x: usize) -> usize {
+        let x = self.root(x);
+        self.rank[x]
+    }
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {($($format:tt)*) => (let _ = write!(out,$($format)*););}
+    #[allow(unused)]
+    macro_rules! putvec {
+        ($v:expr) => {
+            for i in 0..$v.len() {
+                puts!("{}{}", $v[i], if i + 1 == $v.len() {"\n"} else {" "});
+            }
+        }
+    }
+    input! {
+        r: usize, c: usize,
+        rs: usize1, cs: usize1,
+        rg: usize1, cg: usize1,
+        rchw: [(usize1, usize1, usize, usize)],
+    }
+    let mut tbl = vec![vec![false; c]; r];
+    for &(r, c, h, w) in &rchw {
+        for i in r..r + h {
+            for j in c..c + w {
+                tbl[i][j] = true;
+            }
+        }
+    }
+    let mut uf = UnionFind::new(r * c);
+    for i in 0..r {
+        for j in 0..c {
+            let v = i * c + j;
+            if j > 0 && tbl[i][j] && tbl[i][j - 1] {
+                uf.unite(v, v - 1);
+            }
+            if i > 0 && tbl[i][j] && tbl[i - 1][j] {
+                uf.unite(v, v - c);
+            }
+        }
+    }
+    puts!("{}\n", if uf.is_same_set(rs * c + cs, rg * c + cg) {
+        "YES"
+    } else {
+        "NO"
+    });
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
