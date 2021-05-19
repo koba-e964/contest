ccfb6495af37ed1a8b07ac92033cca2f3cf09ab8
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-05-19 23:30:28 +0900

    Add atcoder/maximum-cup-2013/a.rs atcoder/maximum-cup-2013/b.rs atcoder/maximum-cup-2013/j.rs atcoder/maximum-cup-2013/remain.txt

---
+++ atcoder/maximum-cup-2013/j.rs
@@ -0,0 +1,130 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Read, BufWriter, Write};
+
+#[allow(dead_code)]
+fn getline() -> String {
+    let mut ret = String::new();
+    std::io::stdin().read_line(&mut ret).ok().unwrap();
+    ret
+}
+
+fn get_word() -> String {
+    let stdin = std::io::stdin();
+    let mut stdin=stdin.lock();
+    let mut u8b: [u8; 1] = [0];
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(16);
+        loop {
+            let res = stdin.read(&mut u8b);
+            if res.unwrap_or(0) == 0 || u8b[0] <= b' ' {
+                break;
+            } else {
+                buf.push(u8b[0]);
+            }
+        }
+        if buf.len() >= 1 {
+            let ret = String::from_utf8(buf).unwrap();
+            return ret;
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn get<T: std::str::FromStr>() -> T { get_word().parse().ok().unwrap() }
+
+/**
+ * Segment Tree. This data structure is useful for fast folding on intervals of an array
+ * whose elements are elements of monoid I. Note that constructing this tree requires the identity
+ * element of I and the operation of I.
+ * Verified by: yukicoder No. 259 (http://yukicoder.me/submissions/100581)
+ *              AGC015-E (http://agc015.contest.atcoder.jp/submissions/1461001)
+ */
+struct SegTree<I, BiOp> {
+    n: usize,
+    dat: Vec<I>,
+    op: BiOp,
+    e: I,
+}
+
+impl<I, BiOp> SegTree<I, BiOp>
+    where BiOp: Fn(I, I) -> I,
+          I: Copy {
+    pub fn new(n_: usize, op: BiOp, e: I) -> Self {
+        let mut n = 1;
+        while n < n_ { n *= 2; } // n is a power of 2
+        SegTree {n: n, dat: vec![e; 2 * n - 1], op: op, e: e}
+    }
+    /* ary[k] <- v */
+    pub fn update(&mut self, idx: usize, v: I) {
+        let mut k = idx + self.n - 1;
+        self.dat[k] = v;
+        while k > 0 {
+            k = (k - 1) / 2;
+            self.dat[k] = (self.op)(self.dat[2 * k + 1], self.dat[2 * k + 2]);
+        }
+    }
+    /* [a, b) (note: half-inclusive)
+     * http://proc-cpuinfo.fixstars.com/2017/07/optimize-segment-tree/ */
+    pub fn query(&self, mut a: usize, mut b: usize) -> I {
+        let mut left = self.e;
+        let mut right = self.e;
+        a += self.n - 1;
+        b += self.n - 1;
+        while a < b {
+            if (a & 1) == 0 {
+                left = (self.op)(left, self.dat[a]);
+            }
+            if (b & 1) == 0 {
+                right = (self.op)(self.dat[b - 1], right);
+            }
+            a = a / 2;
+            b = (b - 1) / 2;
+        }
+        (self.op)(left, right)
+    }
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {($($format:tt)*) => (let _ = write!(out,$($format)*););}
+    #[allow(unused)]
+    macro_rules! putvec {
+        ($v:expr) => {
+            for i in 0..$v.len() {
+                puts!("{}{}", $v[i], if i + 1 == $v.len() {"\n"} else {" "});
+            }
+        }
+    }
+    loop {
+        let n: i64 = get();
+        let k: i64 = get();
+        if n == -1 || k == -1 {
+            break;
+        }
+        let n = n as usize;
+        let k = k as usize;
+        let a: Vec<u64> = (0..n).map(|_| get()).collect();
+        let mut st = SegTree::new(n, max, 0);
+        for i in 0..n {
+            st.update(i, a[i]);
+        }
+        let mut ok = true;
+        for i in k..n {
+            if st.query(0, i - k) > a[i] {
+                ok = false;
+            }
+        }
+        puts!("{}\n", if ok { "Yes" } else { "No" });
+    }
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
