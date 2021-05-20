57cc76501e957370bc8e88081add0c6d2a8ef93c
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-05-20 12:03:55 +0900

    Add atcoder/kupc2012/e.rs atcoder/kupc2012/j.rs atcoder/kupc2012/k.rs atcoder/kupc2012/remain.txt

---
+++ atcoder/kupc2012/e.rs
@@ -0,0 +1,80 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::Read;
+
+#[allow(dead_code)]
+fn getline() -> String {
+    let mut ret = String::new();
+    std::io::stdin().read_line(&mut ret).ok().unwrap();
+    ret
+}
+
+fn get_word() -> String {
+    let stdin = std::io::stdin();
+    let mut stdin=stdin.lock();
+    let mut u8b: [u8; 1] = [0];
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(16);
+        loop {
+            let res = stdin.read(&mut u8b);
+            if res.unwrap_or(0) == 0 || u8b[0] <= b' ' {
+                break;
+            } else {
+                buf.push(u8b[0]);
+            }
+        }
+        if buf.len() >= 1 {
+            let ret = String::from_utf8(buf).unwrap();
+            return ret;
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn get<T: std::str::FromStr>() -> T { get_word().parse().ok().unwrap() }
+
+struct Rng {
+    x: u64,
+}
+
+impl Rng {
+    fn new() -> Self {
+        use std::collections::HashMap;
+        use std::hash::{Hasher, BuildHasher};
+        let hm: HashMap<i32, i32> = HashMap::new();
+        let mut hash = hm.hasher().build_hasher();
+        hash.write_u32(8128);
+        Rng {
+            x: hash.finish(),
+        }
+    }
+    fn next(&mut self) -> u64 {
+        let a = 0xdead_c0de_0013_3331u64;
+        let b = 2457;
+        self.x = self.x.wrapping_mul(a).wrapping_add(b);
+        let x = self.x;
+        x ^ x << 10
+    }
+}
+
+fn solve() {
+    let n: usize = get();
+    for _ in 0..n {
+        get_word();
+    }
+    let mut rng = Rng::new();
+    for _ in 0..1000 {
+        let x = rng.next() as usize % n;
+        println!("{}", x);
+        let _y: i32 = get();
+    }
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
