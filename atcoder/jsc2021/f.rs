4764a26b607720ac5dec176ce460e60e165c2767
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-05-22 16:38:53 +0900

    Add atcoder/jsc2021/f.rs

---
+++ atcoder/jsc2021/f.rs
@@ -0,0 +1,202 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, [graph1; $len:expr]) => {{
+        let mut g = vec![vec![]; $len];
+        let ab = read_value!($next, [(usize1, usize1)]);
+        for (a, b) in ab {
+            g[a].push(b);
+            g[b].push(a);
+        }
+        g
+    }};
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+trait Change {
+    fn chmax(&mut self, x: Self);
+    fn chmin(&mut self, x: Self);
+}
+impl<T: PartialOrd> Change for T {
+    fn chmax(&mut self, x: T) {
+        if *self < x { *self = x; }
+    }
+    fn chmin(&mut self, x: T) {
+        if *self > x { *self = x; }
+    }
+}
+
+#[allow(unused)]
+macro_rules! debug {
+    ($($format:tt)*) => (write!(std::io::stderr(), $($format)*).unwrap());
+}
+#[allow(unused)]
+macro_rules! debugln {
+    ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
+}
+
+/**
+ * Segment Tree. This data structure is useful for fast folding on intervals of an array
+ * whose elements are elements of monoid I. Note that constructing this tree requires the identity
+ * element of I and the operation of I.
+ * Verified by: yukicoder No. 259 (http://yukicoder.me/submissions/100581)
+ *              AGC015-E (http://agc015.contest.atcoder.jp/submissions/1461001)
+ */
+struct SegTree<I, BiOp> {
+    n: usize,
+    dat: Vec<I>,
+    op: BiOp,
+    e: I,
+}
+
+impl<I, BiOp> SegTree<I, BiOp>
+    where BiOp: Fn(I, I) -> I,
+          I: Copy {
+    pub fn new(n_: usize, op: BiOp, e: I) -> Self {
+        let mut n = 1;
+        while n < n_ { n *= 2; } // n is a power of 2
+        SegTree {n: n, dat: vec![e; 2 * n - 1], op: op, e: e}
+    }
+    /* ary[k] <- v */
+    pub fn update(&mut self, idx: usize, v: I) {
+        let mut k = idx + self.n - 1;
+        self.dat[k] = v;
+        while k > 0 {
+            k = (k - 1) / 2;
+            self.dat[k] = (self.op)(self.dat[2 * k + 1], self.dat[2 * k + 2]);
+        }
+    }
+    /* [a, b) (note: half-inclusive)
+     * http://proc-cpuinfo.fixstars.com/2017/07/optimize-segment-tree/ */
+    pub fn query(&self, mut a: usize, mut b: usize) -> I {
+        let mut left = self.e;
+        let mut right = self.e;
+        a += self.n - 1;
+        b += self.n - 1;
+        while a < b {
+            if (a & 1) == 0 {
+                left = (self.op)(left, self.dat[a]);
+            }
+            if (b & 1) == 0 {
+                right = (self.op)(self.dat[b - 1], right);
+            }
+            a = a / 2;
+            b = (b - 1) / 2;
+        }
+        (self.op)(left, right)
+    }
+}
+
+// The author read the editorial before implementing this.
+// Tags: balanced-binary-tree, segment-tree
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (let _ = write!(out,$($format)*););
+    }
+    #[allow(unused)]
+    macro_rules! putvec {
+        ($v:expr) => {
+            for i in 0..$v.len() {
+                puts!("{}{}", $v[i], if i + 1 == $v.len() {"\n"} else {" "});
+            }
+        }
+    }
+    input! {
+        n: usize, m: usize, q: usize,
+        txy: [(i32, usize1, i64); q],
+    }
+    let mut coord = vec![0];
+    for &(_, _, y) in &txy {
+        coord.push(y);
+    }
+    coord.sort(); coord.dedup();
+    let k = coord.len();
+    let adder = |x, y| x + y;
+    let mut r_cnt = SegTree::new(k, adder, 0);
+    let mut r_sum = SegTree::new(k, adder, 0);
+    let mut c_cnt = SegTree::new(k, adder, 0);
+    let mut c_sum = SegTree::new(k, adder, 0);
+    let mut sum = 0i64;
+    let mut a = vec![0; n];
+    let mut b = vec![0; m];
+    r_cnt.update(0, n as i64);
+    c_cnt.update(0, m as i64);
+    let get = |cnt: &SegTree<_, _>, sum: &SegTree<_, _>, idx: usize, val: i64| -> i64 {
+        sum.query(idx, k) + val * cnt.query(0, idx)
+    };
+    let upd = |cnt: &mut SegTree<_, _>, sum: &mut SegTree<_, _>, idx: usize, val: i64, del: i64| {
+        sum.update(idx, sum.query(idx, idx + 1) + val * del);
+        cnt.update(idx, cnt.query(idx, idx + 1) + del);
+    };
+    for (t, x, y) in txy {
+        let idx = coord.binary_search(&y).unwrap();
+        if t == 1 {
+            let old = coord.binary_search(&a[x]).unwrap();
+            let del = get(&c_cnt, &c_sum, old, a[x]);
+            let add = get(&c_cnt, &c_sum, idx, y);
+            sum += add - del;
+            upd(&mut r_cnt, &mut r_sum, old, a[x], -1);
+            upd(&mut r_cnt, &mut r_sum, idx, y, 1);
+            a[x] = y;
+        } else {
+            let old = coord.binary_search(&b[x]).unwrap();
+            let del = get(&r_cnt, &r_sum, old, b[x]);
+            let add = get(&r_cnt, &r_sum, idx, y);
+            sum += add - del;
+            upd(&mut c_cnt, &mut c_sum, old, b[x], -1);
+            upd(&mut c_cnt, &mut c_sum, idx, y, 1);
+            b[x] = y;
+        }
+        puts!("{}\n", sum);
+    }
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
