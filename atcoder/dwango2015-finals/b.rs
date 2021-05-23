d7112b5eecbfff19ca5aa2488b235252d78840fa
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-05-24 00:09:38 +0900

    Add atcoder/dwango2015-finals/a.rs atcoder/dwango2015-finals/b.rs atcoder/dwango2015-finals/c.rs atcoder/dwango2015-finals/remain.txt

---
+++ atcoder/dwango2015-finals/b.rs
@@ -0,0 +1,209 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+trait Change { fn chmax(&mut self, x: Self); fn chmin(&mut self, x: Self); }
+impl<T: PartialOrd> Change for T {
+    fn chmax(&mut self, x: T) { if *self < x { *self = x; } }
+    fn chmin(&mut self, x: T) { if *self > x { *self = x; } }
+}
+
+/**
+ * Dinic's algorithm for maximum flow problem.
+ * Verified by: yukicoder No.177 (http://yukicoder.me/submissions/148371)
+ * Min-cut (the second element of max_flow's returned values) is not verified.
+ */
+
+#[derive(Clone)]
+struct Edge<T> {
+    to: usize,
+    cap: T,
+    rev: usize, // rev is the position of the reverse edge in graph[to]
+}
+
+struct Dinic<T> {
+    graph: Vec<Vec<Edge<T>>>,
+    iter: Vec<usize>,
+    zero: T,
+}
+
+impl<T> Dinic<T>
+    where T: Clone,
+          T: Copy,
+          T: Ord,
+          T: std::ops::AddAssign,
+          T: std::ops::SubAssign,
+{
+    fn bfs(&self, s: usize, level: &mut [Option<usize>]) {
+        let n = level.len();
+        for i in 0 .. n {
+            level[i] = None;
+        }
+        let mut que = std::collections::VecDeque::new();
+        level[s] = Some(0);
+        que.push_back(s);
+        while let Some(v) = que.pop_front() {
+            for e in self.graph[v].iter() {
+	        if e.cap > self.zero && level[e.to] == None {
+	            level[e.to] = Some(level[v].unwrap() + 1);
+	            que.push_back(e.to);
+                }
+            }
+	}
+    }
+    /* search augment path by dfs.
+     * if f == None, f is treated as infinity.
+     */
+    fn dfs(&mut self, v: usize, t: usize, f: Option<T>, level: &mut [Option<usize>]) -> T {
+        if v == t {
+            return f.unwrap();
+        }
+        while self.iter[v] < self.graph[v].len() {
+            let i = self.iter[v];
+            let e = self.graph[v][i].clone();
+            if e.cap > self.zero && level[v] < level[e.to] {
+                let newf = std::cmp::min(f.unwrap_or(e.cap), e.cap);
+                let d = self.dfs(e.to, t, Some(newf), level);
+                if d > self.zero {
+                    self.graph[v][i].cap -= d;
+                    self.graph[e.to][e.rev].cap += d;
+                    return d;
+                }
+            }
+            self.iter[v] += 1;
+        }
+        self.zero
+    }
+    pub fn new(n: usize, zero: T) -> Self {
+        Dinic {
+            graph: vec![Vec::new(); n],
+            iter: vec![0; n],
+            zero: zero,
+        }
+    }
+    pub fn add_edge(&mut self, from: usize, to: usize, cap: T) {
+        let added_from = Edge { to: to, cap: cap,
+                            rev: self.graph[to].len() };
+        let added_to = Edge { to: from, cap: self.zero,
+                            rev: self.graph[from].len() };
+        self.graph[from].push(added_from);
+        self.graph[to].push(added_to);
+    }
+    pub fn max_flow(&mut self, s: usize, t: usize) -> T {
+        let mut flow = self.zero;
+        let n = self.graph.len();
+        let mut level = vec![None; n];
+        loop {
+            self.bfs(s, &mut level);
+            if level[t] == None {
+                return flow;
+            }
+            self.iter.clear();
+            self.iter.resize(n, 0);
+            loop {
+                let f = self.dfs(s, t, None, &mut level);
+                if f <= self.zero { break; }
+                flow += f;
+            }
+        }
+    }
+}
+
+// Tags: perfect-matching, bipartite-matching-with-delta, flow-with-delta
+fn main() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {($($format:tt)*) => (let _ = write!(out,$($format)*););}
+    #[allow(unused)]
+    macro_rules! putvec {
+        ($v:expr) => {
+            for i in 0..$v.len() {
+                puts!("{}{}", $v[i], if i + 1 == $v.len() {"\n"} else {" "});
+            }
+        }
+    }
+    input! {
+        n: usize,
+        s: [chars; n],
+    }
+    let minl = s.iter().map(|x| x.len()).min().unwrap();
+    let mut occ = vec![vec![Vec::with_capacity(n); n]; 26];
+    for offset in 0..minl {
+        for i in 0..26 {
+            for j in 0..n {
+                occ[i][j].truncate(0);
+            }
+        }
+        for j in 0..n {
+            for k in 0..min(n, s[j].len() - offset) {
+                let idx = (s[j][offset + k] as u8 - b'a') as usize;
+                occ[idx][j].push(k);
+            }
+        }
+        'outer:
+        for letter in 0..26 {
+            for j in 0..n {
+                if occ[letter][j].is_empty() {
+                    continue 'outer;
+                }
+            }
+            let mut din = Dinic::new(2 + 2 * n, 0i32);
+            for i in 0..n {
+                din.add_edge(0, 2 + i, 1);
+                din.add_edge(2 + n + i, 1, 1);
+            }
+            for j in 0..n {
+                for &k in &occ[letter][j] {
+                    din.add_edge(2 + j, 2 + n + k, 1);
+                }
+            }
+            let max = din.max_flow(0, 1);
+            if max as usize == n {
+                puts!("YES\n");
+                return;
+            }
+        }
+    }
+    puts!("NO\n");
+}
