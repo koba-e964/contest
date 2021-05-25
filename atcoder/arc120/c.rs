ad30f5f5a102e07a5b424b3a54d143b31bbd812c
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-05-25 10:19:57 +0900

    Add atcoder/arc120/a.rs atcoder/arc120/b.rs atcoder/arc120/c.rs atcoder/arc120/d.rs atcoder/arc120/remain.txt

---
+++ atcoder/arc120/c.rs
@@ -0,0 +1,173 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+trait Change { fn chmax(&mut self, x: Self); fn chmin(&mut self, x: Self); }
+impl<T: PartialOrd> Change for T {
+    fn chmax(&mut self, x: T) { if *self < x { *self = x; } }
+    fn chmin(&mut self, x: T) { if *self > x { *self = x; } }
+}
+
+/**
+ * Segment Tree. This data structure is useful for fast folding on intervals of an array
+ * whose elements are elements of monoid I. Note that constructing this tree requires the identity
+ * element of I and the operation of I.
+ * Verified by: yukicoder No. 259 (http://yukicoder.me/submissions/100581)
+ *              AGC015-E (http://agc015.contest.atcoder.jp/submissions/1461001)
+ */
+struct SegTree<I, BiOp> {
+    n: usize,
+    dat: Vec<I>,
+    op: BiOp,
+    e: I,
+}
+
+impl<I, BiOp> SegTree<I, BiOp>
+    where BiOp: Fn(I, I) -> I,
+          I: Copy {
+    pub fn new(n_: usize, op: BiOp, e: I) -> Self {
+        let mut n = 1;
+        while n < n_ { n *= 2; } // n is a power of 2
+        SegTree {n: n, dat: vec![e; 2 * n - 1], op: op, e: e}
+    }
+    /* ary[k] <- v */
+    pub fn update(&mut self, idx: usize, v: I) {
+        let mut k = idx + self.n - 1;
+        self.dat[k] = v;
+        while k > 0 {
+            k = (k - 1) / 2;
+            self.dat[k] = (self.op)(self.dat[2 * k + 1], self.dat[2 * k + 2]);
+        }
+    }
+    /* [a, b) (note: half-inclusive)
+     * http://proc-cpuinfo.fixstars.com/2017/07/optimize-segment-tree/ */
+    pub fn query(&self, mut a: usize, mut b: usize) -> I {
+        let mut left = self.e;
+        let mut right = self.e;
+        a += self.n - 1;
+        b += self.n - 1;
+        while a < b {
+            if (a & 1) == 0 {
+                left = (self.op)(left, self.dat[a]);
+            }
+            if (b & 1) == 0 {
+                right = (self.op)(self.dat[b - 1], right);
+            }
+            a = a / 2;
+            b = (b - 1) / 2;
+        }
+        (self.op)(left, right)
+    }
+}
+
+fn inv(a: &[usize]) -> i64 {
+    let n = a.len();
+    let mut st = SegTree::new(n, |x, y| x + y, 0i64);
+    let mut ans = 0;
+    for i in 0..n {
+        ans += st.query(a[i] + 1, n);
+        st.update(a[i], 1);
+    }
+    ans
+}
+
+// Tags: multiset-inversion-count, inversion
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {($($format:tt)*) => (let _ = write!(out,$($format)*););}
+    #[allow(unused)]
+    macro_rules! putvec {
+        ($v:expr) => {
+            for i in 0..$v.len() {
+                puts!("{}{}", $v[i], if i + 1 == $v.len() {"\n"} else {" "});
+            }
+        }
+    }
+    input! {
+        n: usize,
+        a: [i64; n],
+        b: [i64; n],
+    }
+    let mut a = a;
+    let mut b = b;
+    for i in 0..n {
+        a[i] += i as i64;
+        b[i] += i as i64;
+    }
+    {
+        let mut a = a.clone();
+        let mut b = b.clone();
+        a.sort();
+        b.sort();
+        if a != b {
+            puts!("-1\n");
+            return;
+        }
+    }
+    let mut u = a.clone();
+    u.sort(); u.dedup();
+    let m = u.len();
+    let mut occ = vec![vec![]; m];
+    let mut pos = vec![0; m];
+    for i in 0..n {
+        let idx = u.binary_search(&a[i]).unwrap();
+        occ[idx].push(i);
+    }
+    let mut arr = vec![];
+    for i in 0..n {
+        let idx = u.binary_search(&b[i]).unwrap();
+        let to = occ[idx][pos[idx]];
+        arr.push(to);
+        pos[idx] += 1;
+    }
+    puts!("{}\n", inv(&arr));
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
---
+++ atcoder/arc120/d.rs
@@ -0,0 +1,150 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+trait Change { fn chmax(&mut self, x: Self); fn chmin(&mut self, x: Self); }
+impl<T: PartialOrd> Change for T {
+    fn chmax(&mut self, x: T) { if *self < x { *self = x; } }
+    fn chmin(&mut self, x: T) { if *self > x { *self = x; } }
+}
+
+trait Bisect<T> {
+    fn lower_bound(&self, val: &T) -> usize;
+    fn upper_bound(&self, val: &T) -> usize;
+}
+
+impl<T: Ord> Bisect<T> for [T] {
+    fn lower_bound(&self, val: &T) -> usize {
+        let mut pass = self.len() + 1;
+        let mut fail = 0;
+        while pass - fail > 1 {
+            let mid = (pass + fail) / 2;
+            if &self[mid - 1] >= val {
+                pass = mid;
+            } else {
+                fail = mid;
+            }
+        }
+        pass - 1
+    }
+    fn upper_bound(&self, val: &T) -> usize {
+        let mut pass = self.len() + 1;
+        let mut fail = 0;
+        while pass - fail > 1 {
+            let mid = (pass + fail) / 2;
+            if &self[mid - 1] > val {
+                pass = mid;
+            } else {
+                fail = mid;
+            }
+        }
+        pass - 1
+    }
+}
+
+fn dfs(l: usize, r: usize, acc: &[usize], occ: &[Vec<usize>], ans: &mut [char]) {
+    if l >= r {
+        return;
+    }
+    // l <-> r-1 ?
+    assert_eq!(acc[l], acc[r]);
+    if acc[l + 1] == acc[r - 1] {
+        ans[l] = '(';
+        ans[r - 1] = ')';
+        dfs(l + 1, r - 1, acc, occ, ans);
+        return;
+    }
+    let delta = acc[l];
+    let idx1 = occ[delta].upper_bound(&l);
+    let idx2 = occ[delta].lower_bound(&r);
+    for i in idx1..idx2 + 1 {
+        dfs(occ[delta][i - 1], occ[delta][i], acc, occ, ans);
+    }
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {($($format:tt)*) => (let _ = write!(out,$($format)*););}
+    #[allow(unused)]
+    macro_rules! putvec {
+        ($v:expr) => {
+            for i in 0..$v.len() {
+                puts!("{}{}", $v[i], if i + 1 == $v.len() {"\n"} else {" "});
+            }
+        }
+    }
+    input! {
+        n: usize,
+        a: [i64; 2 * n],
+    }
+    let mut b = vec![];
+    for i in 0..2 * n {
+        b.push((a[i], i));
+    }
+    b.sort();
+    let mut c = vec![-1; 2 * n];
+    for i in n..2 * n {
+        c[b[i].1] = 1;
+    }
+    let mut delta = n;
+    let mut occ = vec![vec![]; 2 * n + 1];
+    let mut acc = vec![n];
+    occ[n].push(0);
+    for i in 0..2 * n {
+        delta = (delta as i32 + c[i]) as usize;
+        acc.push(delta);
+        occ[delta].push(i + 1);
+    }
+    let mut res = vec!['+'; 2 * n];
+    dfs(0, 2 * n, &acc, &occ, &mut res);
+    puts!("{}\n", res.into_iter().collect::<String>());
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
