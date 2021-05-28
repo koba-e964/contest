055a9687021c34264ac5db5eb210d5156de97d61
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-05-28 11:12:37 +0900

    Add atcoder/autumn_fest/a.rs atcoder/autumn_fest/b.rs

---
+++ atcoder/autumn_fest/a.rs
@@ -0,0 +1,75 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::Read;
+
+#[allow(dead_code)]
+fn getline() -> String {
+    let mut ret = String::new();
+    std::io::stdin().read_line(&mut ret).ok().unwrap();
+    ret
+}
+
+fn get_word() -> String {
+    let stdin = std::io::stdin();
+    let mut stdin=stdin.lock();
+    let mut u8b: [u8; 1] = [0];
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(16);
+        loop {
+            let res = stdin.read(&mut u8b);
+            if res.unwrap_or(0) == 0 || u8b[0] <= b' ' {
+                break;
+            } else {
+                buf.push(u8b[0]);
+            }
+        }
+        if buf.len() >= 1 {
+            let ret = String::from_utf8(buf).unwrap();
+            return ret;
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn get<T: std::str::FromStr>() -> T { get_word().parse().ok().unwrap() }
+
+fn solve() {
+    let n: usize = get();
+    let tlim: usize = get();
+    let p: Vec<usize> = (0..n).map(|_| get()).collect();
+    let s: Vec<Vec<usize>> = (0..n).map(|i| {
+        (0..p[i]).map(|_| get()).collect()
+    }).collect();
+    let t: Vec<Vec<usize>> = (0..n).map(|i| {
+        (0..p[i]).map(|_| get()).collect()
+    }).collect();
+    let mut que = BinaryHeap::new();
+    let mut pos = vec![0; n];
+    for i in 0..n {
+        que.push(Reverse((s[i][0], i)));
+    }
+    let mut el = 0;
+    let mut tot = 0;
+    while let Some(Reverse((sc, idx))) = que.pop() {
+        let tij = t[idx][pos[idx]];
+        let diff = sc - if pos[idx] == 0 { 0 } else { s[idx][pos[idx] - 1] };
+        el += tij;
+        if el <= tlim {
+            tot += diff;
+        }
+        pos[idx] += 1;
+        if pos[idx] < p[idx] {
+            que.push(Reverse((s[idx][pos[idx]], idx)));
+        }
+    }
+    println!("{}", tot);
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
---
+++ atcoder/autumn_fest/b.rs
@@ -0,0 +1,151 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+trait Change { fn chmax(&mut self, x: Self); fn chmin(&mut self, x: Self); }
+impl<T: PartialOrd> Change for T {
+    fn chmax(&mut self, x: T) { if *self < x { *self = x; } }
+    fn chmin(&mut self, x: T) { if *self > x { *self = x; } }
+}
+
+/**
+ * Union-Find tree.
+ * Verified by https://atcoder.jp/contests/pakencamp-2019-day3/submissions/9253305
+ */
+struct UnionFind { disj: Vec<usize>, rank: Vec<usize> }
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        let disj = (0..n).collect();
+        UnionFind { disj: disj, rank: vec![1; n] }
+    }
+    fn root(&mut self, x: usize) -> usize {
+        if x != self.disj[x] {
+            let par = self.disj[x];
+            let r = self.root(par);
+            self.disj[x] = r;
+        }
+        self.disj[x]
+    }
+    fn unite(&mut self, x: usize, y: usize) {
+        let mut x = self.root(x);
+        let mut y = self.root(y);
+        if x == y { return }
+        if self.rank[x] > self.rank[y] {
+            std::mem::swap(&mut x, &mut y);
+        }
+        self.disj[x] = y;
+        self.rank[y] += self.rank[x];
+    }
+    #[allow(unused)]
+    fn is_same_set(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+    #[allow(unused)]
+    fn size(&mut self, x: usize) -> usize {
+        let x = self.root(x);
+        self.rank[x]
+    }
+}
+
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {($($format:tt)*) => (let _ = write!(out,$($format)*););}
+    input! {
+        h: usize, w: usize,
+        c: [chars; h],
+    }
+    let mut uf = UnionFind::new(h * w);
+    let mut active = vec![false; h * w];
+    for i in 0..h {
+        for j in 0..w - 2 {
+            let v = i * w + j;
+            if c[i][j] == c[i][j + 1] && c[i][j] == c[i][j + 2] {
+                uf.unite(v, v + 1);
+                uf.unite(v, v + 2);
+                for k in 0..3 {
+                    active[v + k] = true;
+                }
+            }
+        }
+    }
+    for i in 0..h - 2 {
+        for j in 0..w {
+            let v = i * w + j;
+            if c[i][j] == c[i + 1][j] && c[i][j] == c[i + 2][j] {
+                uf.unite(v, v + w);
+                uf.unite(v, v + 2 * w);
+                for k in 0..3 {
+                    active[v + k * w] = true;
+                }
+            }
+        }
+    }
+    for i in 0..h {
+        for j in 0..w {
+            let v = i * w + j;
+            if i > 0 && c[i][j] == c[i - 1][j] && active[v] && active[v - w] {
+                uf.unite(v, v - w);
+            }
+            if j > 0 && c[i][j] == c[i][j - 1] && active[v] && active[v - 1] {
+                uf.unite(v, v - 1);
+            }
+        }
+    }
+    let mut ans = 0;
+    for i in 0..h * w {
+        if active[i] && uf.root(i) == i {
+            ans += 1;
+        }
+    }
+    puts!("{}\n", ans);
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
