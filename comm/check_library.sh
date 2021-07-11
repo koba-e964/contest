5f673fc9033c977ee0135f1cb5c134feaa3f7f60
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-07-11 22:37:57 +0900

    Fix comm/check_library.sh

--- comm/check_library.sh
+++ comm/check_library.sh
@@ -2,7 +2,7 @@ set -eux
 
 cd `dirname $0`
 
-RUST_TESTERS="test_pollard_rho"
+RUST_TESTERS="test_pollard_rho test_berlekamp_massey"
 for file in ${RUST_TESTERS}; do
     rustc ${file}.rs -O
     ./${file}
