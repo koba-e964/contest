669036ee9008decdb5b82156ff152ff5c1db0a6c
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-08-01 19:36:28 +0900

    Add codeforces/735/2e.rs codeforces/735/remain.txt codeforces/735/url.txt

---
+++ codeforces/735/2e.rs
@@ -0,0 +1,183 @@
+use std::cmp::*;
+use std::io::{Read, BufWriter, Write};
+
+fn get_word() -> String {
+    let stdin = std::io::stdin();
+    let mut stdin=stdin.lock();
+    let mut u8b: [u8; 1] = [0];
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(16);
+        loop {
+            let res = stdin.read(&mut u8b);
+            if res.unwrap_or(0) == 0 || u8b[0] <= b' ' {
+                break;
+            } else {
+                buf.push(u8b[0]);
+            }
+        }
+        if buf.len() >= 1 {
+            let ret = String::from_utf8(buf).unwrap();
+            return ret;
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn get<T: std::str::FromStr>() -> T { get_word().parse().ok().unwrap() }
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
+
+/// Verified by https://atcoder.jp/contests/abc198/submissions/21774342
+mod mod_int {
+    use std::ops::*;
+    pub trait Mod: Copy { fn m() -> i64; }
+    #[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct ModInt<M> { pub x: i32, phantom: ::std::marker::PhantomData<M> }
+    impl<M: Mod> ModInt<M> {
+        // x >= 0
+        pub fn new(x: i64) -> Self { ModInt::new_internal((x % M::m()) as _) }
+        fn new_internal(x: i32) -> Self {
+            ModInt { x: x, phantom: ::std::marker::PhantomData }
+        }
+        pub fn pow(self, mut e: i64) -> Self {
+            debug_assert!(e >= 0);
+            let mut sum = ModInt::new_internal(1);
+            let mut cur = self;
+            while e > 0 {
+                if e % 2 != 0 { sum *= cur; }
+                cur *= cur;
+                e /= 2;
+            }
+            sum
+        }
+        #[allow(dead_code)]
+        pub fn inv(self) -> Self { self.pow(M::m() - 2) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Add<T> for ModInt<M> {
+        type Output = Self;
+        fn add(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x + other.x;
+            if sum >= M::m() as _ { sum -= M::m() as i32; }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Sub<T> for ModInt<M> {
+        type Output = Self;
+        fn sub(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x - other.x;
+            if sum < 0 { sum += M::m() as i32; }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Mul<T> for ModInt<M> {
+        type Output = Self;
+        fn mul(self, other: T) -> Self { ModInt::new(self.x as i64 * other.into().x as i64) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> AddAssign<T> for ModInt<M> {
+        fn add_assign(&mut self, other: T) { *self = *self + other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> SubAssign<T> for ModInt<M> {
+        fn sub_assign(&mut self, other: T) { *self = *self - other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> MulAssign<T> for ModInt<M> {
+        fn mul_assign(&mut self, other: T) { *self = *self * other; }
+    }
+    impl<M: Mod> Neg for ModInt<M> {
+        type Output = Self;
+        fn neg(self) -> Self { ModInt::new(0) - self }
+    }
+    impl<M> ::std::fmt::Display for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            self.x.fmt(f)
+        }
+    }
+    impl<M: Mod> From<i64> for ModInt<M> {
+        fn from(x: i64) -> Self { Self::new(x) }
+    }
+} // mod mod_int
+
+macro_rules! define_mod {
+    ($struct_name: ident, $modulo: expr) => {
+        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        struct $struct_name {}
+        impl mod_int::Mod for $struct_name { fn m() -> i64 { $modulo } }
+    }
+}
+const MOD: i64 = 998_244_353;
+define_mod!(P, MOD);
+type MInt = mod_int::ModInt<P>;
+
+// If d >= 2, our life is very easy.
+fn dfs(v: usize, par: usize, g: &[Vec<usize>], d: usize) -> (usize, bool) {
+    assert!(d >= 2);
+    let mut sz = 1;
+    for &w in &g[v] {
+        if w == par {
+            continue;
+        }
+        let (subsz, sub) = dfs(w, v, g, d);
+        if !sub {
+            return (0, false);
+        }
+        sz += subsz;
+    }
+    if sz % d == 0 || (sz + d - 1) % d == 0 {
+        (sz, true)
+    } else {
+        (0, false)
+    }
+}
+
+fn calc(g: &[Vec<usize>]) -> Vec<MInt> {
+    let n = g.len();
+    let mut dp = vec![MInt::new(0); n + 1];
+    dp[1] = MInt::new(2).pow(n as i64 - 1);
+    for d in 2..n + 1 {
+        if (n - 1) % d == 0 {
+            let ans = dfs(0, n, &g, d).1;
+            dp[d] = if ans { 1.into() } else { 0.into() };
+        }
+    }
+    for i in (1..n + 1).rev() {
+        for j in 2..n / i + 1 {
+            dp[i] = dp[i] - dp[i * j];
+        }
+    }
+    dp
+}
+
+// Tags: essentially-unique-enumeration, edge-orientation
+fn solve() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {($($format:tt)*) => (let _ = write!(out,$($format)*););}
+    #[allow(unused)]
+    macro_rules! putvec {
+        ($v:expr) => {
+            for i in 0..$v.len() {
+                puts!("{}{}", $v[i], if i + 1 == $v.len() {"\n"} else {" "});
+            }
+        }
+    }
+    let t: usize = get();
+    for _ in 0..t {
+        let n: usize = get();
+        let mut g = vec![vec![]; n];
+        for _ in 0..n - 1 {
+            let a = get::<usize>() - 1;
+            let b = get::<usize>() - 1;
+            g[a].push(b);
+            g[b].push(a);
+        }
+        let ans = calc(&g);
+        putvec!(ans[1..]);
+    }
+}
+
