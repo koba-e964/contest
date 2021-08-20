6e3e5db5b1b7b799203a1d0000136581b086a057
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-08-21 08:13:35 +0900

    Add yukicoder/1621.rs

---
+++ yukicoder/1621.rs
@@ -0,0 +1,146 @@
+use std::cmp::*;
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+/// Verified by https://atcoder.jp/contests/abc198/submissions/21774342
+mod mod_int {
+    use std::ops::*;
+    pub trait Mod: Copy { fn m() -> i64; }
+    #[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct ModInt<M> { pub x: i64, phantom: ::std::marker::PhantomData<M> }
+    impl<M: Mod> ModInt<M> {
+        // x >= 0
+        pub fn new(x: i64) -> Self { ModInt::new_internal(x % M::m()) }
+        fn new_internal(x: i64) -> Self {
+            ModInt { x: x, phantom: ::std::marker::PhantomData }
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Add<T> for ModInt<M> {
+        type Output = Self;
+        fn add(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x + other.x;
+            if sum >= M::m() { sum -= M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Sub<T> for ModInt<M> {
+        type Output = Self;
+        fn sub(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x - other.x;
+            if sum < 0 { sum += M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Mul<T> for ModInt<M> {
+        type Output = Self;
+        fn mul(self, other: T) -> Self { ModInt::new(self.x * other.into().x % M::m()) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> AddAssign<T> for ModInt<M> {
+        fn add_assign(&mut self, other: T) { *self = *self + other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> SubAssign<T> for ModInt<M> {
+        fn sub_assign(&mut self, other: T) { *self = *self - other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> MulAssign<T> for ModInt<M> {
+        fn mul_assign(&mut self, other: T) { *self = *self * other; }
+    }
+    impl<M: Mod> Neg for ModInt<M> {
+        type Output = Self;
+        fn neg(self) -> Self { ModInt::new(0) - self }
+    }
+    impl<M> ::std::fmt::Display for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            self.x.fmt(f)
+        }
+    }
+    impl<M: Mod> From<i64> for ModInt<M> {
+        fn from(x: i64) -> Self { Self::new(x) }
+    }
+} // mod mod_int
+
+macro_rules! define_mod {
+    ($struct_name: ident, $modulo: expr) => {
+        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        struct $struct_name {}
+        impl mod_int::Mod for $struct_name { fn m() -> i64 { $modulo } }
+    }
+}
+const MOD: i64 = 998_244_353;
+define_mod!(P, MOD);
+type MInt = mod_int::ModInt<P>;
+
+// Tags: partition-number, dp, inversions
+fn main() {
+    input! {
+        n: usize, k: usize,
+        a: [i64; n],
+    }
+    let mut coo = a.clone();
+    coo.sort(); coo.dedup();
+    let m = coo.len();
+    let mut f = vec![0; m];
+    for a in a {
+        f[coo.binary_search(&a).unwrap()] += 1;
+    }
+    let lim = n * (n - 1) / 2 + 1;
+    let mut help = vec![vec![vec![MInt::new(0); lim]; n + 1]; n + 1];
+    for x in 0..n + 1 {
+        help[x][0][0] += 1;
+        for y in 1..n - x + 1 {
+            for i in 0..x * y + 1 {
+                let mut me = help[x][y - 1][i];
+                if x >= 1 && i >= y {
+                    me += help[x - 1][y][i - y];
+                }
+                help[x][y][i] = me;
+            }
+        }
+    }
+    let mut dp = vec![MInt::new(0); lim];
+    dp[0] = 1.into();
+    let mut x = 0;
+    for y in f {
+        let mut tmp = vec![MInt::new(0); x * y + 1];
+        for i in 0..x * y + 1 {
+            tmp[i] = help[x][y][i];
+        }
+        assert!(tmp[0] == 1.into());
+        for j in (0..lim).rev() {
+            for u in 1..min(j + 1, tmp.len()) {
+                dp[j] = dp[j] + dp[j - u] * tmp[u];
+            }
+        }
+        x += y;
+    }
+    println!("{}", dp[k]);
+}
