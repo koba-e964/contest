1f01aa8541f35a9897d088d65716e1c76ca25c96
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-08-27 11:14:01 +0900

    Add luogu/P3711.rs luogu/P4091.rs luogu/P4464.rs

---
+++ luogu/P3711.rs
@@ -0,0 +1,345 @@
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+/// Verified by https://atcoder.jp/contests/abc198/submissions/21774342
+mod mod_int {
+    use std::ops::*;
+    pub trait Mod: Copy { fn m() -> i64; }
+    #[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct ModInt<M> { pub x: i64, phantom: ::std::marker::PhantomData<M> }
+    impl<M: Mod> ModInt<M> {
+        // x >= 0
+        pub fn new(x: i64) -> Self { ModInt::new_internal(x % M::m()) }
+        fn new_internal(x: i64) -> Self {
+            ModInt { x: x, phantom: ::std::marker::PhantomData }
+        }
+        pub fn pow(self, mut e: i64) -> Self {
+            debug_assert!(e >= 0);
+            let mut sum = ModInt::new_internal(1);
+            let mut cur = self;
+            while e > 0 {
+                if e % 2 != 0 { sum *= cur; }
+                cur *= cur;
+                e /= 2;
+            }
+            sum
+        }
+        #[allow(dead_code)]
+        pub fn inv(self) -> Self { self.pow(M::m() - 2) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Add<T> for ModInt<M> {
+        type Output = Self;
+        fn add(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x + other.x;
+            if sum >= M::m() { sum -= M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Sub<T> for ModInt<M> {
+        type Output = Self;
+        fn sub(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x - other.x;
+            if sum < 0 { sum += M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Mul<T> for ModInt<M> {
+        type Output = Self;
+        fn mul(self, other: T) -> Self { ModInt::new(self.x * other.into().x % M::m()) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> AddAssign<T> for ModInt<M> {
+        fn add_assign(&mut self, other: T) { *self = *self + other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> SubAssign<T> for ModInt<M> {
+        fn sub_assign(&mut self, other: T) { *self = *self - other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> MulAssign<T> for ModInt<M> {
+        fn mul_assign(&mut self, other: T) { *self = *self * other; }
+    }
+    impl<M: Mod> Neg for ModInt<M> {
+        type Output = Self;
+        fn neg(self) -> Self { ModInt::new(0) - self }
+    }
+    impl<M> ::std::fmt::Display for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            self.x.fmt(f)
+        }
+    }
+    impl<M: Mod> ::std::fmt::Debug for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            let (mut a, mut b, _) = red(self.x, M::m());
+            if b < 0 {
+                a = -a;
+                b = -b;
+            }
+            write!(f, "{}/{}", a, b)
+        }
+    }
+    impl<M: Mod> From<i64> for ModInt<M> {
+        fn from(x: i64) -> Self { Self::new(x) }
+    }
+    // Finds the simplest fraction x/y congruent to r mod p.
+    // The return value (x, y, z) satisfies x = y * r + z * p.
+    fn red(r: i64, p: i64) -> (i64, i64, i64) {
+        if r.abs() <= 10000 {
+            return (r, 1, 0);
+        }
+        let mut nxt_r = p % r;
+        let mut q = p / r;
+        if 2 * nxt_r >= r {
+            nxt_r -= r;
+            q += 1;
+        }
+        if 2 * nxt_r <= -r {
+            nxt_r += r;
+            q -= 1;
+        }
+        let (x, z, y) = red(nxt_r, r);
+        (x, y - q * z, z)
+    }
+} // mod mod_int
+
+macro_rules! define_mod {
+    ($struct_name: ident, $modulo: expr) => {
+        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        struct $struct_name {}
+        impl mod_int::Mod for $struct_name { fn m() -> i64 { $modulo } }
+    }
+}
+const MOD: i64 = 998_244_353;
+define_mod!(P, MOD);
+type MInt = mod_int::ModInt<P>;
+
+// Depends on MInt.rs
+fn fact_init(w: usize) -> (Vec<MInt>, Vec<MInt>) {
+    let mut fac = vec![MInt::new(1); w];
+    let mut invfac = vec![0.into(); w];
+    for i in 1..w {
+        fac[i] = fac[i - 1] * i as i64;
+    }
+    invfac[w - 1] = fac[w - 1].inv();
+    for i in (0..w - 1).rev() {
+        invfac[i] = invfac[i + 1] * (i as i64 + 1);
+    }
+    (fac, invfac)
+}
+
+// FFT (in-place, verified as NTT only)
+// R: Ring + Copy
+// Verified by: https://judge.yosupo.jp/submission/53831
+// Adopts the technique used in https://judge.yosupo.jp/submission/3153.
+mod fft {
+    use std::ops::*;
+    // n should be a power of 2. zeta is a primitive n-th root of unity.
+    // one is unity
+    // Note that the result is bit-reversed.
+    pub fn fft<R>(f: &mut [R], zeta: R, one: R)
+        where R: Copy +
+        Add<Output = R> +
+        Sub<Output = R> +
+        Mul<Output = R> {
+        let n = f.len();
+        assert!(n.is_power_of_two());
+        let mut m = n;
+        let mut base = zeta;
+        unsafe {
+            while m > 2 {
+                m >>= 1;
+                let mut r = 0;
+                while r < n {
+                    let mut w = one;
+                    for s in r..r + m {
+                        let &u = f.get_unchecked(s);
+                        let d = *f.get_unchecked(s + m);
+                        *f.get_unchecked_mut(s) = u + d;
+                        *f.get_unchecked_mut(s + m) = w * (u - d);
+                        w = w * base;
+                    }
+                    r += 2 * m;
+                }
+                base = base * base;
+            }
+            if m > 1 {
+                // m = 1
+                let mut r = 0;
+                while r < n {
+                    let &u = f.get_unchecked(r);
+                    let d = *f.get_unchecked(r + 1);
+                    *f.get_unchecked_mut(r) = u + d;
+                    *f.get_unchecked_mut(r + 1) = u - d;
+                    r += 2;
+                }
+            }
+        }
+    }
+    pub fn inv_fft<R>(f: &mut [R], zeta_inv: R, one: R)
+        where R: Copy +
+        Add<Output = R> +
+        Sub<Output = R> +
+        Mul<Output = R> {
+        let n = f.len();
+        assert!(n.is_power_of_two());
+        let zeta = zeta_inv; // inverse FFT
+        let mut zetapow = Vec::with_capacity(20);
+        {
+            let mut m = 1;
+            let mut cur = zeta;
+            while m < n {
+                zetapow.push(cur);
+                cur = cur * cur;
+                m *= 2;
+            }
+        }
+        let mut m = 1;
+        unsafe {
+            if m < n {
+                zetapow.pop();
+                let mut r = 0;
+                while r < n {
+                    let &u = f.get_unchecked(r);
+                    let d = *f.get_unchecked(r + 1);
+                    *f.get_unchecked_mut(r) = u + d;
+                    *f.get_unchecked_mut(r + 1) = u - d;
+                    r += 2;
+                }
+                m = 2;
+            }
+            while m < n {
+                let base = zetapow.pop().unwrap();
+                let mut r = 0;
+                while r < n {
+                    let mut w = one;
+                    for s in r..r + m {
+                        let &u = f.get_unchecked(s);
+                        let d = *f.get_unchecked(s + m) * w;
+                        *f.get_unchecked_mut(s) = u + d;
+                        *f.get_unchecked_mut(s + m) = u - d;
+                        w = w * base;
+                    }
+                    r += 2 * m;
+                }
+                m *= 2;
+            }
+        }
+    }
+}
+
+// Computes f^{-1} mod x^{f.len()}.
+// Reference: https://codeforces.com/blog/entry/56422
+// Complexity: O(n log n)
+// Verified by: https://judge.yosupo.jp/submission/3219
+// Depends on: MInt.rs, fft.rs
+fn fps_inv<P: mod_int::Mod + PartialEq>(
+    f: &[mod_int::ModInt<P>],
+    gen: mod_int::ModInt<P>
+) -> Vec<mod_int::ModInt<P>> {
+    let n = f.len();
+    assert!(n.is_power_of_two());
+    assert_eq!(f[0], 1.into());
+    let mut sz = 1;
+    let mut r = vec![mod_int::ModInt::new(0); n];
+    let mut tmp_f = vec![mod_int::ModInt::new(0); n];
+    let mut tmp_r = vec![mod_int::ModInt::new(0); n];
+    r[0] = 1.into();
+    // Adopts the technique used in https://judge.yosupo.jp/submission/3153
+    while sz < n {
+        let zeta = gen.pow((P::m() - 1) / sz as i64 / 2);
+        tmp_f[..2 * sz].copy_from_slice(&f[..2 * sz]);
+        tmp_r[..2 * sz].copy_from_slice(&r[..2 * sz]);
+        fft::fft(&mut tmp_r[..2 * sz], zeta, 1.into());
+        fft::fft(&mut tmp_f[..2 * sz], zeta, 1.into());
+        let fac = mod_int::ModInt::new(2 * sz as i64).inv().pow(2);
+        for i in 0..2 * sz {
+            tmp_f[i] *= tmp_r[i];
+        }
+        fft::inv_fft(&mut tmp_f[..2 * sz], zeta.inv(), 1.into());
+        for v in &mut tmp_f[..sz] {
+            *v = 0.into();
+        }
+        fft::fft(&mut tmp_f[..2 * sz], zeta, 1.into());
+        for i in 0..2 * sz {
+            tmp_f[i] = -tmp_f[i] * tmp_r[i] * fac;
+        }
+        fft::inv_fft(&mut tmp_f[..2 * sz], zeta.inv(), 1.into());
+        r[sz..2 * sz].copy_from_slice(&tmp_f[sz..2 * sz]);
+        sz *= 2;
+    }
+    r
+}
+
+fn bernoulli(n: usize, gen: MInt, fac: &[MInt], invfac: &[MInt]) -> Vec<MInt> {
+    assert!(n.is_power_of_two());
+    let mut dp = vec![MInt::new(0); n];
+    for i in 0..n {
+        dp[i] = invfac[i + 1];
+    }
+    let mut dp = fps_inv(&dp, gen);
+    dp[1] += 1;
+    for i in 2..n {
+        dp[i] *= fac[i];
+    }
+    dp
+}
+
+// Tags: bernoulli-number
+fn main() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {($($format:tt)*) => (let _ = write!(out,$($format)*););}
+    input! {
+        n: usize,
+        a: [i64; n + 1],
+    }
+    const W: usize = 1 << 18;
+    let (fac, invfac) = fact_init(W + 1);
+    let ber = bernoulli(W, 3.into(), &fac, &invfac);
+    let mut ans = vec![MInt::new(0); 2 * W];
+    let mut rev = vec![MInt::new(0); 2 * W];
+    for i in 0..n + 1 {
+        ans[i + 1] = fac[i] * a[i];
+        rev[(2 * W - i) % (2 * W)] = ber[i] * invfac[i];
+    }
+    let factor = MInt::new(2 * W as i64).inv();
+    let zeta = MInt::new(3).pow((MOD - 1) / 2 / W as i64);
+    fft::fft(&mut ans, zeta, 1.into());
+    fft::fft(&mut rev, zeta, 1.into());
+    for i in 0..2 * W {
+        ans[i] *= rev[i] * factor;
+    }
+    fft::inv_fft(&mut ans, zeta.inv(), 1.into());
+    ans[0] = a[0].into();
+    for i in 0..n + 2 {
+        puts!("{}{}", ans[i] * invfac[i], if i == n + 1 { "\n" } else { " " });
+    }
+}
---
+++ luogu/P4464.rs
@@ -0,0 +1,342 @@
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+// https://judge.yosupo.jp/submission/5155
+mod pollard_rho {
+    /// binary gcd
+    pub fn gcd(mut x: i64, mut y: i64) -> i64 {
+        if y == 0 { return x; }
+        if x == 0 { return y; }
+        let k = (x | y).trailing_zeros();
+        y >>= k;
+        x >>= x.trailing_zeros();
+        while y != 0 {
+            y >>= y.trailing_zeros();
+            if x > y { let t = x; x = y; y = t; }
+            y -= x;
+        }
+        x << k
+    }
+
+    fn add_mod(x: i64, y: i64, n: i64) -> i64 {
+        let z = x + y;
+        if z >= n { z - n } else { z }
+    }
+
+    fn mul_mod(x: i64, mut y: i64, n: i64) -> i64 {
+        assert!(x >= 0);
+        assert!(x < n);
+        let mut sum = 0;
+        let mut cur = x;
+        while y > 0 {
+            if (y & 1) == 1 { sum = add_mod(sum, cur, n); }
+            cur = add_mod(cur, cur, n);
+            y >>= 1;
+        }
+        sum
+    }
+
+    fn mod_pow(x: i64, mut e: i64, n: i64) -> i64 {
+        let mut prod = if n == 1 { 0 } else { 1 };
+        let mut cur = x % n;
+        while e > 0 {
+            if (e & 1) == 1 { prod = mul_mod(prod, cur, n); }
+            e >>= 1;
+            if e > 0 { cur = mul_mod(cur, cur, n); }
+        }
+        prod
+    }
+
+    pub fn is_prime(n: i64) -> bool {
+        if n <= 1 { return false; }
+        let small = [2, 3, 5, 7, 11, 13];
+        if small.iter().any(|&u| u == n) { return true; }
+        if small.iter().any(|&u| n % u == 0) { return false; }
+        let mut d = n - 1;
+        let e = d.trailing_zeros();
+        d >>= e;
+        // https://miller-rabin.appspot.com/
+        let a = [2, 325, 9375, 28178, 450775, 9780504, 1795265022];
+        a.iter().all(|&a| {
+            if a % n == 0 { return true; }
+            let mut x = mod_pow(a, d, n);
+            if x == 1 { return true; }
+            for _ in 0..e {
+                if x == n - 1 {
+                    return true;
+                }
+                x = mul_mod(x, x, n);
+                if x == 1 { return false; }
+            }
+            x == 1
+        })
+    }
+
+    fn pollard_rho(n: i64, c: &mut i64) -> i64 {
+        // An improvement with Brent's cycle detection algorithm is performed.
+        // https://maths-people.anu.edu.au/~brent/pub/pub051.html
+        if n % 2 == 0 { return 2; }
+        loop {
+            let mut x: i64; // tortoise
+            let mut y = 2; // hare
+            let mut d = 1;
+            let cc = *c;
+            let f = |i| add_mod(mul_mod(i, i, n), cc, n);
+            let mut r = 1;
+            // We don't perform the gcd-once-in-a-while optimization
+            // because the plain gcd-every-time algorithm appears to
+            // outperform, at least on judge.yosupo.jp :)
+            while d == 1 {
+                x = y;
+                for _ in 0..r {
+                    y = f(y);
+                    d = gcd((x - y).abs(), n);
+                    if d != 1 { break; }
+                }
+                r *= 2;
+            }
+            if d == n {
+                *c += 1;
+                continue;
+            }
+            return d;
+        }
+    }
+
+    /// Outputs (p, e) in p's ascending order.
+    pub fn factorize(x: i64) -> Vec<(i64, usize)> {
+        if x <= 1 { return vec![]; }
+        let mut hm = std::collections::HashMap::new();
+        let mut pool = vec![x];
+        let mut c = 1;
+        while let Some(u) = pool.pop() {
+            if is_prime(u) {
+                *hm.entry(u).or_insert(0) += 1;
+                continue;
+            }
+            let p = pollard_rho(u, &mut c);
+            pool.push(p);
+            pool.push(u / p);
+        }
+        let mut v: Vec<_> = hm.into_iter().collect();
+        v.sort();
+        v
+    }
+} // mod pollard_rho
+
+/// Verified by https://atcoder.jp/contests/abc198/submissions/21774342
+mod mod_int {
+    use std::ops::*;
+    pub trait Mod: Copy { fn m() -> i64; }
+    #[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct ModInt<M> { pub x: i64, phantom: ::std::marker::PhantomData<M> }
+    impl<M: Mod> ModInt<M> {
+        // x >= 0
+        pub fn new(x: i64) -> Self { ModInt::new_internal(x % M::m()) }
+        fn new_internal(x: i64) -> Self {
+            ModInt { x: x, phantom: ::std::marker::PhantomData }
+        }
+        pub fn pow(self, mut e: i64) -> Self {
+            debug_assert!(e >= 0);
+            let mut sum = ModInt::new_internal(1);
+            let mut cur = self;
+            while e > 0 {
+                if e % 2 != 0 { sum *= cur; }
+                cur *= cur;
+                e /= 2;
+            }
+            sum
+        }
+        #[allow(dead_code)]
+        pub fn inv(self) -> Self { self.pow(M::m() - 2) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Add<T> for ModInt<M> {
+        type Output = Self;
+        fn add(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x + other.x;
+            if sum >= M::m() { sum -= M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Sub<T> for ModInt<M> {
+        type Output = Self;
+        fn sub(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x - other.x;
+            if sum < 0 { sum += M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Mul<T> for ModInt<M> {
+        type Output = Self;
+        fn mul(self, other: T) -> Self { ModInt::new(self.x * other.into().x % M::m()) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> AddAssign<T> for ModInt<M> {
+        fn add_assign(&mut self, other: T) { *self = *self + other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> SubAssign<T> for ModInt<M> {
+        fn sub_assign(&mut self, other: T) { *self = *self - other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> MulAssign<T> for ModInt<M> {
+        fn mul_assign(&mut self, other: T) { *self = *self * other; }
+    }
+    impl<M: Mod> Neg for ModInt<M> {
+        type Output = Self;
+        fn neg(self) -> Self { ModInt::new(0) - self }
+    }
+    impl<M> ::std::fmt::Display for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            self.x.fmt(f)
+        }
+    }
+    impl<M: Mod> ::std::fmt::Debug for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            let (mut a, mut b, _) = red(self.x, M::m());
+            if b < 0 {
+                a = -a;
+                b = -b;
+            }
+            write!(f, "{}/{}", a, b)
+        }
+    }
+    impl<M: Mod> From<i64> for ModInt<M> {
+        fn from(x: i64) -> Self { Self::new(x) }
+    }
+    // Finds the simplest fraction x/y congruent to r mod p.
+    // The return value (x, y, z) satisfies x = y * r + z * p.
+    fn red(r: i64, p: i64) -> (i64, i64, i64) {
+        if r.abs() <= 10000 {
+            return (r, 1, 0);
+        }
+        let mut nxt_r = p % r;
+        let mut q = p / r;
+        if 2 * nxt_r >= r {
+            nxt_r -= r;
+            q += 1;
+        }
+        if 2 * nxt_r <= -r {
+            nxt_r += r;
+            q -= 1;
+        }
+        let (x, z, y) = red(nxt_r, r);
+        (x, y - q * z, z)
+    }
+} // mod mod_int
+
+macro_rules! define_mod {
+    ($struct_name: ident, $modulo: expr) => {
+        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        struct $struct_name {}
+        impl mod_int::Mod for $struct_name { fn m() -> i64 { $modulo } }
+    }
+}
+const MOD: i64 = 1_000_000_007;
+define_mod!(P, MOD);
+type MInt = mod_int::ModInt<P>;
+
+// Depends on MInt.rs
+fn fact_init(w: usize) -> (Vec<MInt>, Vec<MInt>) {
+    let mut fac = vec![MInt::new(1); w];
+    let mut invfac = vec![0.into(); w];
+    for i in 1..w {
+        fac[i] = fac[i - 1] * i as i64;
+    }
+    invfac[w - 1] = fac[w - 1].inv();
+    for i in (0..w - 1).rev() {
+        invfac[i] = invfac[i + 1] * (i as i64 + 1);
+    }
+    (fac, invfac)
+}
+
+fn bernoulli_naive(n: usize, fac: &[MInt], invfac: &[MInt]) -> Vec<MInt> {
+    let mut dp = vec![MInt::new(0); n];
+    dp[0] = 1.into();
+    for i in 1..n {
+        let mut tmp = MInt::new(0);
+        for j in 0..i {
+            tmp -= fac[i + 1] * invfac[i + 1 - j] * invfac[j] * dp[j];
+        }
+        dp[i] = tmp * invfac[i + 1] * fac[i];
+    }
+    dp[1] += 1;
+    dp
+}
+
+// Tags: bernoulli-number, inclusion-exclusion-on-divisors, dirichlet-convolution, multiplicative-function
+// The author read the solution before implementing this.
+fn main() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {($($format:tt)*) => (let _ = write!(out,$($format)*););}
+    input! {
+        t: usize,
+        nxy: [(i64, usize, usize); t],
+    }
+    const W: usize = 3010;
+    let (fac, invfac) = fact_init(W + 1);
+    let ber = bernoulli_naive(W, &fac, &invfac);
+    use std::time::*;
+    for (n, x, y) in nxy {
+        let mut now = Instant::now();
+        let pe = pollard_rho::factorize(n);
+        eprintln!("divs: {:?}", now.elapsed());
+        let f = |i: usize, p: i64, u: usize| -> MInt {
+            let val = MInt::new(p).pow(i as i64 * u as i64);
+            let mut sum = val;
+            let step = MInt::new(p).pow(MOD - 1 + x as i64 - i as i64);
+            for _ in 0..u {
+                sum = sum * step + val;
+            }
+            sum
+        };
+        now = Instant::now();
+        // inclusion-exclusion-principle on divisors
+        let mut ans = MInt::new(0);
+        for i in 1..y + 2 {
+            let mut tmp = MInt::new(1);
+            for &(p, e) in &pe {
+                let a = f(i, p, e) - f(i, p, e - 1) * MInt::new(p).pow(y as i64);
+                tmp *= a;
+            }
+            if i == 0 {
+                ans += tmp;
+            } else {
+                ans += ber[y + 1 - i] * fac[y] * invfac[i] * invfac[y + 1 - i]
+                    * tmp;
+            }
+        }
+        puts!("{}\n", ans * MInt::new(n).pow(y as i64));
+        eprintln!("output: {:?}", now.elapsed());
+    }
+}
