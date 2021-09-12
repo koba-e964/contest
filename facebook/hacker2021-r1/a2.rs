648e125c5b5304a36a3ae3af2f7553d6a1d30f6b
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-09-13 08:04:32 +0900

    Add facebook/hacker2021-r1/a1.rs facebook/hacker2021-r1/a2.rs facebook/hacker2021-r1/a3.rs facebook/hacker2021-r1/b.rs facebook/hacker2021-r1/remain.txt facebook/hacker2021-r1/url.txt

---
+++ facebook/hacker2021-r1/a2.rs
@@ -0,0 +1,73 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+const MOD: i64 = 1_000_000_007;
+
+fn main() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {($($format:tt)*) => (let _ = write!(out,$($format)*););}
+    input! {
+        t: usize,
+        ns: [(usize, chars); t],
+    }
+    for case_nr in 0..t {
+        let (n, ref s) = ns[case_nr];
+        let mut last = '+';
+        let mut lastpos = 0;
+        let mut sum = 0i64;
+        for i in 0..n {
+            let c = s[i];
+            if c == 'F' { continue; }
+            let idx = i as i64;
+            if c == last {
+                lastpos = idx;
+                continue;
+            }
+            if last != '+' {
+                sum += (n as i64 - idx) * (lastpos + 1);
+                sum %= MOD;
+            }
+            last = c;
+            lastpos = idx;
+        }
+        puts!("Case #{}: {}\n", case_nr + 1, sum);
+    }
+}
