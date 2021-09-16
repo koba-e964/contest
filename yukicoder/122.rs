d2b3de3671b3515152eab561aab33398f8dd09eb
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-09-16 19:27:47 +0900

    Add yukicoder/122.rs

---
+++ yukicoder/122.rs
@@ -0,0 +1,255 @@
+use std::io::*;
+fn get_word() -> String {
+    let mut stdin = std::io::stdin();
+    let mut u8b: [u8; 1] = [0];
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(16);
+        loop {
+            let res = stdin.read(&mut u8b);
+            if res.is_err() || res.ok().unwrap() == 0 || u8b[0] <= ' ' as u8 {
+                break;
+            } else {
+                buf.push(u8b[0]);
+            }
+        }
+        if buf.len() >= 1 {
+            let ret = std::string::String::from_utf8(buf).unwrap();
+            return ret;
+        }
+    }
+}
+fn parse<T: std::str::FromStr>(s: &str) -> T { s.parse::<T>().ok().unwrap() }
+
+#[allow(dead_code)]
+fn get<T: std::str::FromStr>() -> T { parse(&get_word()) }
+
+/// Verified by https://atcoder.jp/contests/abc198/submissions/21774342
+mod mod_int {
+    use std::ops::*;
+    pub trait Mod: Copy { fn m() -> i64; }
+    #[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct ModInt<M> { pub x: i64, phantom: ::std::marker::PhantomData<M> }
+    impl<M: Mod> ModInt<M> {
+        // x >= 0
+        pub fn new(x: i64) -> Self { ModInt::new_internal(x % M::m()) }
+        fn new_internal(x: i64) -> Self {
+            ModInt { x: x, phantom: ::std::marker::PhantomData }
+        }
+        pub fn pow(self, mut e: i64) -> Self {
+            debug_assert!(e >= 0);
+            let mut sum = ModInt::new_internal(1);
+            let mut cur = self;
+            while e > 0 {
+                if e % 2 != 0 { sum *= cur; }
+                cur *= cur;
+                e /= 2;
+            }
+            sum
+        }
+        #[allow(dead_code)]
+        pub fn inv(self) -> Self { self.pow(M::m() - 2) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Add<T> for ModInt<M> {
+        type Output = Self;
+        fn add(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x + other.x;
+            if sum >= M::m() { sum -= M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Sub<T> for ModInt<M> {
+        type Output = Self;
+        fn sub(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x - other.x;
+            if sum < 0 { sum += M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Mul<T> for ModInt<M> {
+        type Output = Self;
+        fn mul(self, other: T) -> Self { ModInt::new(self.x * other.into().x % M::m()) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> AddAssign<T> for ModInt<M> {
+        fn add_assign(&mut self, other: T) { *self = *self + other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> SubAssign<T> for ModInt<M> {
+        fn sub_assign(&mut self, other: T) { *self = *self - other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> MulAssign<T> for ModInt<M> {
+        fn mul_assign(&mut self, other: T) { *self = *self * other; }
+    }
+    impl<M: Mod> Neg for ModInt<M> {
+        type Output = Self;
+        fn neg(self) -> Self { ModInt::new(0) - self }
+    }
+    impl<M> ::std::fmt::Display for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            self.x.fmt(f)
+        }
+    }
+    impl<M: Mod> ::std::fmt::Debug for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            let (mut a, mut b, _) = red(self.x, M::m());
+            if b < 0 {
+                a = -a;
+                b = -b;
+            }
+            write!(f, "{}/{}", a, b)
+        }
+    }
+    impl<M: Mod> From<i64> for ModInt<M> {
+        fn from(x: i64) -> Self { Self::new(x) }
+    }
+    // Finds the simplest fraction x/y congruent to r mod p.
+    // The return value (x, y, z) satisfies x = y * r + z * p.
+    fn red(r: i64, p: i64) -> (i64, i64, i64) {
+        if r.abs() <= 10000 {
+            return (r, 1, 0);
+        }
+        let mut nxt_r = p % r;
+        let mut q = p / r;
+        if 2 * nxt_r >= r {
+            nxt_r -= r;
+            q += 1;
+        }
+        if 2 * nxt_r <= -r {
+            nxt_r += r;
+            q -= 1;
+        }
+        let (x, z, y) = red(nxt_r, r);
+        (x, y - q * z, z)
+    }
+} // mod mod_int
+
+macro_rules! define_mod {
+    ($struct_name: ident, $modulo: expr) => {
+        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        struct $struct_name {}
+        impl mod_int::Mod for $struct_name { fn m() -> i64 { $modulo } }
+    }
+}
+const MOD: i64 = 1_000_000_007;
+define_mod!(P, MOD);
+type MInt = mod_int::ModInt<P>;
+
+/**
+ * Returns the least index of elements that are modified, wrapped with Some.
+ * If the entire array is reversed, it returns None instead.
+ * v's elements must be pairwise distinct.
+ */
+fn next_permutation<T: Ord>(v: &mut [T]) -> Option<usize> {
+    let mut tail_dec: usize = 1;
+    let n = v.len();
+    while tail_dec < n {
+        if v[n - tail_dec - 1] > v[n - tail_dec] {
+            tail_dec += 1;
+        } else {
+            break;
+        }
+    }
+    // v[n - tail_dec .. n] is strictly decreasing
+    if tail_dec < n {
+        let x = n - tail_dec - 1;
+        let mut y = n;
+        {
+            let pivot = &v[x];
+            for i in (n - tail_dec .. n).rev() {
+                if v[i] > *pivot {
+                    y = i;
+                    break;
+                }
+            }
+            assert!(y < n);
+        }
+        v.swap(x, y);
+    }
+    v[n - tail_dec .. n].reverse();
+    if tail_dec < n {
+        Some(n - tail_dec - 1)
+    } else {
+        None
+    }
+}
+
+const W: usize = 20010;
+
+// ret[v]: #ways to pick elements from a with max v
+// picked elements should form an increasing sequence 
+fn calc2(a: &[(usize, usize)]) -> Vec<MInt> {
+    let mut dp = vec![MInt::new(1); W];
+    for &(l, r) in a {
+        let mut ep = vec![MInt::new(0); W];
+        for i in l..r + 1 {
+            ep[i] = ep[i - 1] + dp[i - 1];
+        }
+        for i in r + 1..W {
+            ep[i] = ep[i - 1];
+        }
+        dp = ep;
+    }
+    for i in (0..W - 1).rev() {
+        dp[i + 1] = dp[i + 1] - dp[i];
+    }
+    dp
+}
+
+// ret[v]: #ways to pick different elements from a with max v
+fn calc1(a: &[(usize, usize)]) -> Vec<MInt> {
+    let n = a.len();
+    let mut p: Vec<_> = (0..n).collect();
+    let mut ans = vec![MInt::new(0); W];
+    loop {
+        let mut b = vec![(0, 0); n];
+        for i in 0..n {
+            b[i] = a[p[i]];
+        }
+        let sub = calc2(&b);
+        for i in 0..W {
+            ans[i] += sub[i];
+        }
+        if let None = next_permutation(&mut p) {
+            break;
+        }
+    }
+    ans
+}
+
+fn solve(a: &[usize; 7], b: &[usize; 7]) -> MInt {
+    let mut lo = [(0, 0); 4];
+    let mut hi = [(0, 0); 3];
+    for i in 0..4 {
+        lo[i] = (a[2 * i], b[2 * i]);
+    }
+    for i in 0..3 {
+        hi[i] = (W - b[2 * i + 1], W - a[2 * i + 1]);
+    }
+    let a = calc1(&lo);
+    let b = calc1(&hi);
+    let mut tot = MInt::new(0);
+    let mut acc = MInt::new(0);
+    for i in 0..W {
+        acc += a[i];
+        tot += acc * b[W - 1 - i];
+    }
+    tot
+}
+
+fn main() {
+    let mut mi = [0usize; 7];
+    let mut ma = [0usize; 7];
+    for i in 0..7 {
+        mi[i] = get();
+        ma[i] = get();
+    }
+    let tot = solve(&mi, &ma);
+    for i in 0..7 {
+        let t1 = W - ma[i];
+        let t2 = W - mi[i];
+        mi[i] = t1;
+        ma[i] = t2;
+    }
+    let tot = tot + solve(&mi, &ma);
+    println!("{}", tot);
+}
