365047aba13624a4860e69267a0394037305392b
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-09-18 16:36:12 +0900

    Add yukicoder/821.rs yukicoder/822.rs yukicoder/823.rs

---
+++ yukicoder/822.rs
@@ -0,0 +1,47 @@
+use std::io::Read;
+
+fn get_word() -> String {
+    let stdin = std::io::stdin();
+    let mut stdin=stdin.lock();
+    let mut u8b: [u8; 1] = [0];
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(16);
+        loop {
+            let res = stdin.read(&mut u8b);
+            if res.unwrap_or(0) == 0 || u8b[0] <= b' ' {
+                break;
+            } else {
+                buf.push(u8b[0]);
+            }
+        }
+        if buf.len() >= 1 {
+            let ret = String::from_utf8(buf).unwrap();
+            return ret;
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn get<T: std::str::FromStr>() -> T { get_word().parse().ok().unwrap() }
+
+fn main() {
+    let n: i32 = get();
+    let k: i32 = get();
+    let mut p = 1;
+    while p <= n {
+        p *= 2;
+    }
+    let mut ans = 0;
+    for j in 0..k + 1 {
+        for i in n..p {
+            if (i & (i + j)) == n {
+                ans += 1;
+                if i + j >= p {
+                    println!("INF");
+                    return;
+                }
+            }
+        }
+    }
+    println!("{}", ans);
+}
