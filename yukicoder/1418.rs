ae5b95711ff1839ed1bc0dfd5722cca9ee88cf5b
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-10-15 10:11:41 +0900

    Add yukicoder/1418.rs yukicoder/1421.rs

---
+++ yukicoder/1418.rs
@@ -0,0 +1,68 @@
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
+
+fn dfs(v: usize, par: usize, g: &[Vec<usize>]) -> (i64, i64) {
+    let n = g.len();
+    let mut sz = 1;
+    let mut tot = 0;
+    for &w in &g[v] {
+        if w == par { continue; }
+        let (subsz, subtot) = dfs(w, v, g);
+        tot += subsz * (n as i64 - subsz);
+        sz += subsz;
+        tot += subtot;
+    }
+    (sz, tot)
+}
+
+// Tags: sum-of-distances-on-trees
+fn solve() {
+    input! {
+        n: usize,
+        ab: [(usize1, usize1); n - 1],
+    }
+    let mut g = vec![vec![]; n];
+    for &(a, b) in &ab {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    let (_sz, tot) = dfs(0, n, &g);
+    println!("{}", 2 * tot + n as i64 * n as i64);
+}
---
+++ yukicoder/1421.rs
@@ -0,0 +1,83 @@
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+// Tags: linear-algebra, gaussian-elimination
+fn main() {
+    input! {
+        n: usize, m: usize,
+        by: [([usize1], i64); m],
+    }
+    let mut basis = vec![];
+    for (b, mut y) in by {
+        let mut c = 0i64;
+        for &b in &b {
+            c |= 1 << b;
+        }
+        for &(p, q) in &basis {
+            if (c ^ p) < c {
+                c ^= p;
+                y ^= q;
+            }
+        }
+        if c == 0 && y != 0 {
+            println!("-1");
+            return;
+        }
+        if c != 0 {
+            basis.push((c, y));
+        }
+    }
+    basis.sort();
+    let mut x = vec![0; n];
+    for (p, mut q) in basis {
+        let mut hi = 0;
+        for i in 0..n {
+            if (p & 1 << i) != 0 {
+                hi = i;
+            }
+        }
+        for i in 0..hi {
+            if (p & 1 << i) != 0 {
+                q ^= x[i];
+            }
+        }
+        x[hi] = q;
+    }
+    for i in 0..n {
+        println!("{}", x[i]);
+    }
+}
