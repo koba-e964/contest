316b15b70da13e930c1a9604d672e530715c742d
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-10-19 11:54:44 +0900

    Add yukicoder/1340.rs yukicoder/1554.rs

---
+++ yukicoder/1340.rs
@@ -0,0 +1,72 @@
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+fn squmul(a: &[Vec<i64>], b: &[Vec<i64>]) -> Vec<Vec<i64>> {
+    let n = a.len();
+    let mut ret = vec![vec![0; n]; n];
+    for i in 0..n {
+        for j in 0..n {
+            for k in 0..n {
+                ret[i][k] |= a[i][j] & b[j][k];
+            }
+        }
+    }
+    ret
+}
+
+fn squpow(a: &[Vec<i64>], mut e: i64) -> Vec<Vec<i64>> {
+    let n = a.len();
+    let mut sum = vec![vec![0; n]; n];
+    for i in 0..n { sum[i][i] = 1; }
+    let mut cur = a.to_vec();
+    while e > 0 {
+        if e % 2 == 1 {
+            sum = squmul(&sum, &cur);
+        }
+        cur = squmul(&cur, &cur);
+        e /= 2;
+    }
+    sum
+}
+
+fn main() {
+    input! {
+        n: usize, m: usize, t: i64,
+        ab: [(usize, usize); m],
+    }
+    let mut mat = vec![vec![0; n]; n];
+    for &(a, b) in &ab {
+        mat[a][b] = 1;
+    }
+    let ans = squpow(&mat, t);
+    println!("{}", ans[0].iter().filter(|&&x| x == 1).count());
+}
---
+++ yukicoder/1554.rs
@@ -0,0 +1,142 @@
+use std::cmp::*;
+use std::collections::*;
+use std::io::Read;
+
+fn get_word() -> String {
+    let stdin = std::io::stdin();
+    let mut stdin=stdin.lock();
+    let mut u8b: [u8; 1] = [0];
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(16);
+        loop {
+            let res = stdin.read(&mut u8b);
+            if res.unwrap_or(0) == 0 || u8b[0] <= b' ' {
+                break;
+            } else {
+                buf.push(u8b[0]);
+            }
+        }
+        if buf.len() >= 1 {
+            let ret = String::from_utf8(buf).unwrap();
+            return ret;
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn get<T: std::str::FromStr>() -> T { get_word().parse().ok().unwrap() }
+
+/// Verified by https://atcoder.jp/contests/abc198/submissions/21774342
+mod mod_int {
+    use std::ops::*;
+    pub trait Mod: Copy { fn m() -> i64; }
+    #[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct ModInt<M> { pub x: i64, phantom: ::std::marker::PhantomData<M> }
+    impl<M: Mod> ModInt<M> {
+        // x >= 0
+        pub fn new(x: i64) -> Self { ModInt::new_internal(x % M::m()) }
+        fn new_internal(x: i64) -> Self {
+            ModInt { x: x, phantom: ::std::marker::PhantomData }
+        }
+        pub fn pow(self, mut e: i64) -> Self {
+            debug_assert!(e >= 0);
+            let mut sum = ModInt::new_internal(1);
+            let mut cur = self;
+            while e > 0 {
+                if e % 2 != 0 { sum *= cur; }
+                cur *= cur;
+                e /= 2;
+            }
+            sum
+        }
+        #[allow(dead_code)]
+        pub fn inv(self) -> Self { self.pow(M::m() - 2) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Add<T> for ModInt<M> {
+        type Output = Self;
+        fn add(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x + other.x;
+            if sum >= M::m() { sum -= M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Sub<T> for ModInt<M> {
+        type Output = Self;
+        fn sub(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x - other.x;
+            if sum < 0 { sum += M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Mul<T> for ModInt<M> {
+        type Output = Self;
+        fn mul(self, other: T) -> Self { ModInt::new(self.x * other.into().x % M::m()) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> AddAssign<T> for ModInt<M> {
+        fn add_assign(&mut self, other: T) { *self = *self + other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> SubAssign<T> for ModInt<M> {
+        fn sub_assign(&mut self, other: T) { *self = *self - other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> MulAssign<T> for ModInt<M> {
+        fn mul_assign(&mut self, other: T) { *self = *self * other; }
+    }
+    impl<M: Mod> Neg for ModInt<M> {
+        type Output = Self;
+        fn neg(self) -> Self { ModInt::new(0) - self }
+    }
+    impl<M> ::std::fmt::Display for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            self.x.fmt(f)
+        }
+    }
+    impl<M: Mod> From<i64> for ModInt<M> {
+        fn from(x: i64) -> Self { Self::new(x) }
+    }
+} // mod mod_int
+
+macro_rules! define_mod {
+    ($struct_name: ident, $modulo: expr) => {
+        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        struct $struct_name {}
+        impl mod_int::Mod for $struct_name { fn m() -> i64 { $modulo } }
+    }
+}
+const MOD: i64 = 998_244_353;
+define_mod!(P, MOD);
+type MInt = mod_int::ModInt<P>;
+
+const INF: i64 = 1_000_000_000_000_000;
+
+fn solve(a: &[i64], k: i64) -> MInt {
+    let n = a.len();
+    let s: i64 = a.iter().sum();
+    let sinv = MInt::new(s).inv();
+    let mut que = BinaryHeap::new();
+    let mut pmod = vec![MInt::new(0); n];
+    for i in 0..n {
+        let pr = INF * a[i];
+        pmod[i] = MInt::new(a[i]) * sinv;
+        que.push((pr, i, 1));
+    }
+    let mut ans = MInt::new(1);
+    for i in 0..k {
+        ans *= i + 1;
+        let (_pr, idx, c) = que.pop().unwrap();
+        ans *= pmod[idx] * MInt::new(c).inv();
+        que.push((INF * a[idx] / (c + 1), idx, c + 1));
+    }
+    ans
+}
+
+fn main() {
+    let t: usize = get();
+    for _ in 0..t {
+        let n: usize = get();
+        let k = get();
+        let a: Vec<i64> = (0..n).map(|_| get()).collect();
+        println!("{}", solve(&a, k));
+    }
+}
