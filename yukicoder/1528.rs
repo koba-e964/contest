c660d6b15cb9c91f38bff6143f8d3d6b7d8ff221
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2021-11-02 11:23:14 +0900

    Add yukicoder/1527.py yukicoder/1528.rs yukicoder/681.rs

---
+++ yukicoder/1528.rs
@@ -0,0 +1,61 @@
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+fn main() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {($($format:tt)*) => (let _ = write!(out,$($format)*););}
+    #[allow(unused)]
+    macro_rules! putvec {
+        ($v:expr) => {
+            for i in 0..$v.len() {
+                puts!("{}{}", $v[i], if i + 1 == $v.len() {"\n"} else {" "});
+            }
+        }
+    }
+    input!(n: usize);
+    if n % 2 == 0 {
+        let v: Vec<_> = (1..n / 2 + 1).map(|x| x * 2).collect();
+        putvec!(v);
+        return;
+    }
+    if n == 1 {
+        puts!("1\n");
+        return;
+    }
+    if n <= 5 {
+        puts!("-1\n");
+        return;
+    }
+    let mut v = vec![3, 6, 2, 4];
+    for i in 4..n / 2 + 1 {
+        v.push(2 * i);
+    }
+    putvec!(v);
+}
