20db8d3d44f98c310ae24d1b1c16ebcc827f45ed
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2022-01-17 22:50:34 +0900

    Add cafecoder/release_test/a.rs cafecoder/szpp_scratch_2021/a.rs cafecoder/szpp_scratch_2021/b.rs cafecoder/szpp_scratch_2021/c.rs cafecoder/szpp_scratch_2021/d.rs cafecoder/szpp_scratch_2021/e.rs cafecoder/szpp_scratch_2021_pra/b.rs cafecoder/szpp_scratch_2021_pra/c.rs cafecoder/szpp_scratch_2021_pra/contest.yml cafecoder/szpp_scratch_2021_pra/d.rs cafecoder/tea001/a.rs cafecoder/tea001/contest.yml

---
+++ cafecoder/szpp_scratch_2021/d.rs
@@ -0,0 +1,39 @@
+use std::io::Read;
+
+fn get_word() -> String {
+    let stdin = std::io::stdin();
+    let mut stdin=stdin.lock();
+    let mut u8b: [u8; 1] = [0];
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(16);
+        loop {
+            let res = stdin.read(&mut u8b);
+            if res.unwrap_or(0) == 0 || u8b[0] <= b' ' {
+                break;
+            } else {
+                buf.push(u8b[0]);
+            }
+        }
+        if buf.len() >= 1 {
+            let ret = String::from_utf8(buf).unwrap();
+            return ret;
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn get<T: std::str::FromStr>() -> T { get_word().parse().ok().unwrap() }
+
+fn main() {
+    let n: usize = get();
+    let mut f = vec![0; 101];
+    for _ in 0..n {
+        let a: usize = get();
+        f[a] += 1;
+    }
+    let q: usize = get();
+    for _ in 0..q {
+        let x: usize = get();
+        println!("{}", f[x]);
+    }
+}
