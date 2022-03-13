d6b572dcb41bc596cd04581650a8d539133fd11f
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2022-03-14 08:29:05 +0900

    Add atcoder/abc241/ex.rs

---
+++ atcoder/abc241/ex.rs
@@ -0,0 +1,170 @@
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+/// Verified by https://atcoder.jp/contests/abc198/submissions/21774342
+mod mod_int {
+    use std::ops::*;
+    pub trait Mod: Copy { fn m() -> i64; }
+    #[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct ModInt<M> { pub x: i64, phantom: ::std::marker::PhantomData<M> }
+    impl<M: Mod> ModInt<M> {
+        // x >= 0
+        pub fn new(x: i64) -> Self { ModInt::new_internal(x % M::m()) }
+        fn new_internal(x: i64) -> Self {
+            ModInt { x: x, phantom: ::std::marker::PhantomData }
+        }
+        pub fn pow(self, mut e: i64) -> Self {
+            debug_assert!(e >= 0);
+            let mut sum = ModInt::new_internal(1);
+            let mut cur = self;
+            while e > 0 {
+                if e % 2 != 0 { sum *= cur; }
+                cur *= cur;
+                e /= 2;
+            }
+            sum
+        }
+        #[allow(dead_code)]
+        pub fn inv(self) -> Self { self.pow(M::m() - 2) }
+    }
+    impl<M: Mod> Default for ModInt<M> {
+        fn default() -> Self { Self::new_internal(0) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Add<T> for ModInt<M> {
+        type Output = Self;
+        fn add(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x + other.x;
+            if sum >= M::m() { sum -= M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Sub<T> for ModInt<M> {
+        type Output = Self;
+        fn sub(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x - other.x;
+            if sum < 0 { sum += M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Mul<T> for ModInt<M> {
+        type Output = Self;
+        fn mul(self, other: T) -> Self { ModInt::new(self.x * other.into().x % M::m()) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> AddAssign<T> for ModInt<M> {
+        fn add_assign(&mut self, other: T) { *self = *self + other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> SubAssign<T> for ModInt<M> {
+        fn sub_assign(&mut self, other: T) { *self = *self - other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> MulAssign<T> for ModInt<M> {
+        fn mul_assign(&mut self, other: T) { *self = *self * other; }
+    }
+    impl<M: Mod> Neg for ModInt<M> {
+        type Output = Self;
+        fn neg(self) -> Self { ModInt::new(0) - self }
+    }
+    impl<M> ::std::fmt::Display for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            self.x.fmt(f)
+        }
+    }
+    impl<M: Mod> From<i64> for ModInt<M> {
+        fn from(x: i64) -> Self { Self::new(x) }
+    }
+} // mod mod_int
+
+macro_rules! define_mod {
+    ($struct_name: ident, $modulo: expr) => {
+        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        struct $struct_name {}
+        impl mod_int::Mod for $struct_name { fn m() -> i64 { $modulo } }
+    }
+}
+const MOD: i64 = 998_244_353;
+define_mod!(P, MOD);
+type MInt = mod_int::ModInt<P>;
+
+// https://atcoder.jp/contests/abc241/tasks/abc241_h
+// \sum_{\sum a = m} x_1^{a_1} ... x_k^{a_k} は、x_i^m * coef[i] の和で表すことができ、
+// coef[i] は m によらず定まるので前計算できる。
+// Tags: inclusion-exclusion-principle, sum-of-products-of-powers
+fn main() {
+    input! {
+        n: usize, m: i64,
+        ab: [(i64, i64); n],
+    }
+    let mut tot = MInt::new(0);
+    let mut apb = vec![MInt::new(0); n];
+    let mut coef = vec![MInt::new(0); n];
+    for i in 0..n {
+        let (a, b) = ab[i];
+        apb[i] = MInt::new(a).pow(b + 1);
+        let mut tmp = MInt::new(0);
+        if i == 0 {
+            tmp += 1;
+        } else {
+            for j in 0..i {
+                let inv = MInt::new(ab[j].0 + MOD - a).inv();
+                tmp -= coef[j] * inv * a;
+                coef[j] *= inv * ab[j].0;
+            }
+        }
+        coef[i] = tmp;
+    }
+    for bits in 0i32..1 << n {
+        let bc = bits.count_ones();
+        let mut tmp = MInt::new(1);
+        let mut rem = m;
+        for i in 0..n {
+            if (bits & 1 << i) != 0 {
+                tmp *= apb[i];
+                rem -= ab[i].1 + 1;
+            }
+        }
+        if rem < 0 {
+            continue;
+        }
+        let mut sum = MInt::new(0);
+        for i in 0..n {
+            sum += coef[i] * MInt::new(ab[i].0).pow(rem);
+        }
+        tmp *= sum;
+        if bc % 2 == 0 {
+            tot += tmp;
+        } else {
+            tot -= tmp;
+        }
+    }
+    println!("{}", tot);
+}
