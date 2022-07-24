3d9e492203169b9340273b86c7fb997f518b0528
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2022-07-24 15:34:59 +0900

    Add atcoder/abc258/ex.rs

---
+++ atcoder/abc258/ex.rs
@@ -0,0 +1,197 @@
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+/// Verified by https://atcoder.jp/contests/abc198/submissions/21774342
+mod mod_int {
+    use std::ops::*;
+    pub trait Mod: Copy { fn m() -> i64; }
+    #[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct ModInt<M> { pub x: i64, phantom: ::std::marker::PhantomData<M> }
+    impl<M: Mod> ModInt<M> {
+        // x >= 0
+        pub fn new(x: i64) -> Self { ModInt::new_internal(x % M::m()) }
+        fn new_internal(x: i64) -> Self {
+            ModInt { x: x, phantom: ::std::marker::PhantomData }
+        }
+        pub fn pow(self, mut e: i64) -> Self {
+            debug_assert!(e >= 0);
+            let mut sum = ModInt::new_internal(1);
+            let mut cur = self;
+            while e > 0 {
+                if e % 2 != 0 { sum *= cur; }
+                cur *= cur;
+                e /= 2;
+            }
+            sum
+        }
+        #[allow(dead_code)]
+        pub fn inv(self) -> Self { self.pow(M::m() - 2) }
+    }
+    impl<M: Mod> Default for ModInt<M> {
+        fn default() -> Self { Self::new_internal(0) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Add<T> for ModInt<M> {
+        type Output = Self;
+        fn add(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x + other.x;
+            if sum >= M::m() { sum -= M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Sub<T> for ModInt<M> {
+        type Output = Self;
+        fn sub(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x - other.x;
+            if sum < 0 { sum += M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Mul<T> for ModInt<M> {
+        type Output = Self;
+        fn mul(self, other: T) -> Self { ModInt::new(self.x * other.into().x % M::m()) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> AddAssign<T> for ModInt<M> {
+        fn add_assign(&mut self, other: T) { *self = *self + other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> SubAssign<T> for ModInt<M> {
+        fn sub_assign(&mut self, other: T) { *self = *self - other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> MulAssign<T> for ModInt<M> {
+        fn mul_assign(&mut self, other: T) { *self = *self * other; }
+    }
+    impl<M: Mod> Neg for ModInt<M> {
+        type Output = Self;
+        fn neg(self) -> Self { ModInt::new(0) - self }
+    }
+    impl<M> ::std::fmt::Display for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            self.x.fmt(f)
+        }
+    }
+    impl<M: Mod> ::std::fmt::Debug for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            let (mut a, mut b, _) = red(self.x, M::m());
+            if b < 0 {
+                a = -a;
+                b = -b;
+            }
+            write!(f, "{}/{}", a, b)
+        }
+    }
+    impl<M: Mod> From<i64> for ModInt<M> {
+        fn from(x: i64) -> Self { Self::new(x) }
+    }
+    // Finds the simplest fraction x/y congruent to r mod p.
+    // The return value (x, y, z) satisfies x = y * r + z * p.
+    fn red(r: i64, p: i64) -> (i64, i64, i64) {
+        if r.abs() <= 10000 {
+            return (r, 1, 0);
+        }
+        let mut nxt_r = p % r;
+        let mut q = p / r;
+        if 2 * nxt_r >= r {
+            nxt_r -= r;
+            q += 1;
+        }
+        if 2 * nxt_r <= -r {
+            nxt_r += r;
+            q -= 1;
+        }
+        let (x, z, y) = red(nxt_r, r);
+        (x, y - q * z, z)
+    }
+} // mod mod_int
+
+macro_rules! define_mod {
+    ($struct_name: ident, $modulo: expr) => {
+        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        struct $struct_name {}
+        impl mod_int::Mod for $struct_name { fn m() -> i64 { $modulo } }
+    }
+}
+const MOD: i64 = 998_244_353;
+define_mod!(P, MOD);
+type MInt = mod_int::ModInt<P>;
+
+fn mul((a, b): (MInt, MInt), (c, d): (MInt, MInt)) -> (MInt, MInt) {
+    (a * c + b * d * 5, a * d + b * c)
+}
+
+fn pow(a: (MInt, MInt), mut e: i64) -> (MInt, MInt) {
+    let mut prod = (MInt::new(1), MInt::new(0));
+    let mut cur = a;
+    while e > 0 {
+        if e % 2 == 1 {
+            prod = mul(prod, cur);
+        }
+        cur = mul(cur, cur);
+        e /= 2;
+    }
+    prod
+}
+
+// 合計が N である奇数からなる列の個数は fib[N] である。
+// Tags: inclusion-exclusion, algebraic-numbers
+fn main() {
+    input! {
+        n: usize, s: i64,
+        a: [i64; n],
+    }
+    let mut a = a;
+    a.insert(0, 0);
+    a.push(s);
+    let n = a.len();
+    let mut dp = vec![MInt::new(0); n];
+    let mut acc_0 = vec![(MInt::new(0), MInt::new(0)); n];
+    let mut acc_1 = vec![(MInt::new(0), MInt::new(0)); n];
+    dp[0] -= 1;
+    let inv5 = MInt::new(5).inv();
+    acc_0[0] = (0.into(), -inv5);
+    acc_1[0] = (0.into(), inv5);
+    let inv2 = MInt::new(2).inv();
+    let phi = (inv2, inv2);
+    let phibar = (inv2, -inv2);
+    let invphi = (-inv2, inv2);
+    let invphibar = (-inv2, -inv2);
+    for i in 1..n {
+        let tmp1 = mul(pow(phi, a[i]), acc_0[i - 1]);
+        let tmp2 = mul(pow(phibar, a[i]), acc_1[i - 1]);
+        dp[i] = -(tmp1.0 + tmp2.0);
+        let tmp = mul(pow(invphi, a[i]), (0.into(), dp[i] * inv5));
+        acc_0[i] = (acc_0[i - 1].0 + tmp.0, acc_0[i - 1].1 + tmp.1);
+        let tmp = mul(pow(invphibar, a[i]), (0.into(), -dp[i] * inv5));
+        acc_1[i] = (acc_1[i - 1].0 + tmp.0, acc_1[i - 1].1 + tmp.1);
+    }
+    println!("{}", dp[n - 1]);
+}
