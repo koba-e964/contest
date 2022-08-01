01c2846cb2070ea545bacc6dd1b7a9ca8bce1b89
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2022-08-01 21:29:43 +0900

    Add opencup/22-china/h.rs

--- opencup/22-china/h.rs
+++ opencup/22-china/h.rs
@@ -1,3 +1,29 @@
+use std::io::Read;
+
+fn get_word() -> String {
+    let stdin = std::io::stdin();
+    let mut stdin=stdin.lock();
+    let mut u8b: [u8; 1] = [0];
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(16);
+        loop {
+            let res = stdin.read(&mut u8b);
+            if res.unwrap_or(0) == 0 || u8b[0] <= b' ' {
+                break;
+            } else {
+                buf.push(u8b[0]);
+            }
+        }
+        if buf.len() >= 1 {
+            let ret = String::from_utf8(buf).unwrap();
+            return ret;
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn get<T: std::str::FromStr>() -> T { get_word().parse().ok().unwrap() }
+
 // Dinic's algorithm for maximum flow problem.
 // This implementation uses O(n) stack space.
 // Verified by:
@@ -94,12 +120,10 @@ impl<T> Dinic<T>
         }
     }
     pub fn add_edge(&mut self, from: usize, to: usize, cap: T) {
-        let added_from = Edge {
-            to: to, cap: cap,
-            rev: self.graph[to].len() };
-        let added_to = Edge {
-            to: from, cap: self.zero,
-            rev: self.graph[from].len() };
+        let added_from = Edge { to: to, cap: cap,
+                            rev: self.graph[to].len() };
+        let added_to = Edge { to: from, cap: self.zero,
+                            rev: self.graph[from].len() };
         self.graph[from].push(added_from);
         self.graph[to].push(added_to);
     }
@@ -118,6 +142,74 @@ impl<T> Dinic<T>
             self.iter.resize(n, 0);
             let f = self.dfs(t, s, None, &mut level);
             flow += f;
+        }
+    }
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| solve()).unwrap().join().unwrap();
+}
+
+fn solve() {
+    let t: usize = get();
+    for _ in 0..t {
+        let n: usize = get();
+        let m: usize = get();
+        let mut s: Vec<Vec<char>> = (0..n).map(|_| get_word().chars().collect()).collect();
+        for i in 0..n {
+            for j in 0..m {
+                if (i + j) % 2 == 0 { continue; }
+                if s[i][j] == 'W' { s[i][j] = 'B'; }
+                else if s[i][j] == 'B' { s[i][j] = 'W'; }
+            }
+        }
+        let mut din = Dinic::new(2 + n * m + 2 * (n - 1) * (m - 1), 0i32);
+        const INF: i32 = 1 << 28;
+        for i in 0..n {
+            for j in 0..m {
+                let x = i * m + j;
+                if s[i][j] == 'W' {
+                    din.add_edge(0, 2 + x, INF);
+                }
+                if s[i][j] == 'B' {
+                    din.add_edge(2 + x, 1, INF);
+                }
+            }
+        }
+        for i in 0..n - 1 {
+            for j in 0..m - 1 {
+                let x = i * (m - 1) + j;
+                din.add_edge(0, 2 + n * m + x, 1);
+                din.add_edge(2 + n * m + (n - 1) * (m - 1) + x, 1, 1);
+                for a in 0..2 {
+                    for b in 0..2 {
+                        let y = 2 + (i + a) * m + (j + b);
+                        din.add_edge(2 + n * m + x, y, 1);
+                        din.add_edge(y, 2 + n * m + (n - 1) * (m - 1) + x, 1);
+                    }
+                }
+            }
+        }
+        let (ma, cut) = din.max_flow(0, 1);
+        println!("{}", 2 * (n - 1) * (m - 1) - ma as usize);
+        let mut ans = vec![vec!['W'; m]; n];
+        for v in 2..2 + n * m {
+            let x = (v - 2) / m;
+            let y = (v - 2) % m;
+            if cut.is_t[v] {
+                ans[x][y] = 'B';
+            }
+        }
+        for i in 0..n {
+            for j in 0..m {
+                if (i + j) % 2 == 0 { continue; }
+                if ans[i][j] == 'W' { ans[i][j] = 'B'; }
+                else if ans[i][j] == 'B' { ans[i][j] = 'W'; }
+            }
+            println!("{}", ans[i].iter().cloned().collect::<String>());
         }
     }
 }
