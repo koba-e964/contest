3581e77896b92093f04b4e3cc1b631bd1cba8705
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2022-08-26 11:48:20 +0900

    Add atcoder/arc112/a.rs atcoder/arc112/b.rs atcoder/arc112/remain.txt

---
+++ atcoder/arc112/b.rs
@@ -0,0 +1,63 @@
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::*;
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => { ($(read_value!($next, $t)),*) };
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+    ($next:expr, usize1) => (read_value!($next, usize) - 1);
+    ($next:expr, [ $t:tt ]) => {{
+        let len = read_value!($next, usize);
+        read_value!($next, [$t; len])
+    }};
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+fn main() {
+    input!(b: i64, c: i64);
+    let mut rng = vec![];
+    rng.push((b - c / 2, b));
+    rng.push((-b - (c - 1) / 2, -b + (c - 1) / 2));
+    if c >= 2 {
+        let r = (c - 2) / 2;
+        rng.push((b - r, b + r));
+    }
+    rng.sort();
+    let mut cur = i64::min_value();
+    let mut tot = 0;
+    for &(l, r) in &rng {
+        cur = max(cur, l);
+        tot += r + 1 - cur;
+        cur = r + 1;
+    }
+    println!("{}", tot);
+}
