8c25bde2629dd96846a088c9b2259301b37393c6
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2022-09-17 20:08:19 +0900

    Add functions to comm/geo.rs

--- comm/geo.rs
+++ comm/geo.rs
@@ -1,15 +1,15 @@
 type Coord = i64; // the type of coordinates
-type P = (Coord, Coord);
+type P<C = Coord> = (C, C);
 
-fn inn((ax, ay): P, (bx, by): P) -> Coord {
+fn inn<C: std::ops::Add<Output = C> + std::ops::Mul<Output = C>>((ax, ay): P<C>, (bx, by): P<C>) -> C {
     ax * bx + ay * by
 }
 
-fn det((ax, ay): P, (bx, by): P) -> Coord {
+fn det<C: std::ops::Sub<Output = C> + std::ops::Mul<Output = C>>((ax, ay): P<C>, (bx, by): P<C>) -> C {
     ax * by - ay * bx
 }
 
-fn sub((ax, ay): P, (bx, by): P) -> P {
+fn sub<C: std::ops::Sub<Output = C>>((ax, ay): P<C>, (bx, by): P<C>) -> P<C> {
     (ax - bx, ay - by)
 }
 
@@ -88,4 +88,48 @@ fn arg_sort(xy: &mut [(Coord, Coord, i32)]) {
             || 0.cmp(&(a.0 * b.1 - a.1 * b.0))
                 .then_with(|| a.2.cmp(&b.2)))
     });
+}
+
+// segments
+
+type Segment<C = Coord> = (P<C>, P<C>);
+
+// Verified by: https://atcoder.jp/contests/math-and-algorithm/submissions/34916093
+fn nearest(a: P<f64>, (b, c): Segment<f64>) -> P<f64> {
+    let bc = sub(b, c);
+    let ac = sub(a, c);
+    let x = inn(bc, bc);
+    let y = inn(bc, ac);
+    if y < 0.0 {
+        c
+    } else if y > x {
+        b
+    } else {
+        let r = y as f64 / x as f64;
+        (c.0 * (1.0 - r) + b.0 * r, c.1 * (1.0 - r) + b.1 * r)
+    }
+}
+
+fn ccw(a: P<i64>, b: P<i64>, c: P<i64>) -> i8 {
+    let b = sub(b, a);
+    let c = sub(c, a);
+    if det(b, c) > 0 {
+        return 1;
+    }
+    if det(b, c) < 0 {
+        return -1;
+    }
+    if inn(b, c) < 0 {
+        return 2;
+    }
+    if inn(b, c) < inn(c, c) {
+        return -2;
+    }
+    0
+}
+
+// Verified by: https://atcoder.jp/contests/math-and-algorithm/submissions/34916560
+fn intersect(s: Segment<i64>, t: Segment<i64>) -> bool {
+    ccw(s.0, s.1, t.0) * ccw(s.0, s.1, t.1) <= 0 &&
+        ccw(t.0, t.1, s.0) * ccw(t.0, t.1, s.1) <= 0
 }
