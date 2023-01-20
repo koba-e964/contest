f3d466041b5dd1645f0d55e2657dbb2f40372316
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2023-01-21 00:25:57 +0900

    Add atcoder/abc272/remain.txt atcoder/abc273/a.rs atcoder/abc273/b.rs atcoder/abc273/c.rs atcoder/abc273/remain.txt atcoder/abc274/remain.txt atcoder/abc276/remain.txt atcoder/abc277/remain.txt atcoder/abc278/remain.txt atcoder/abc279/remain.txt atcoder/abc280/remain.txt

---
+++ atcoder/abc273/c.rs
@@ -0,0 +1,52 @@
+use std::io::{Write, BufWriter};
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+fn main() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {($($format:tt)*) => (let _ = write!(out,$($format)*););}
+    input! {
+        n: usize,
+        a: [i64; n],
+    }
+    let mut b = a.clone();
+    b.sort(); b.dedup();
+    let mut ans = vec![0; n];
+    for a in &a {
+        let idx = b.binary_search(a).unwrap();
+        ans[b.len() - idx - 1] += 1;
+    }
+    for i in 0..n {
+        puts!("{}\n", ans[i]);
+    }
+}
+
