1a0659f1074f45badc1acb5e1613d8e200ba990d
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2023-07-03 22:02:12 +0900

    Add yukicoder/2370.rs

---
+++ yukicoder/2370.rs
@@ -0,0 +1,58 @@
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes.by_ref().map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr,) => {};
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+    ($next:expr, $t:ty) => ($next().parse::<$t>().expect("Parse error"));
+}
+
+// https://yukicoder.me/problems/no/2370 (3)
+// O(NK log N) でできる。ソートをマージソートのマージに置き換えれば O(NK) にもなるはず。
+fn main() {
+    input! {
+        n: usize, k: usize,
+        a: [i64; n],
+    }
+    let mut buff = 0;
+    let mut a = a;
+    for v in &mut a {
+        if *v < 0 {
+            buff += *v;
+            *v *= -1;
+        }
+    }
+    let mut dp = vec![0];
+    for i in 0..n {
+        let mut ep = dp.clone();
+        for &v in &dp {
+            ep.push(v + a[i]);
+        }
+        ep.sort(); ep.reverse();
+        ep.truncate(k);
+        dp = ep;
+    }
+    println!("{}", dp[k - 1] + buff);
+}
