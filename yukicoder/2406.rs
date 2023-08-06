b2386981c438aaef26fe5b4eb2764822fdf85ac9
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2023-08-07 03:40:59 +0900

    Add yukicoder/2404.rs yukicoder/2406.rs

---
+++ yukicoder/2406.rs
@@ -0,0 +1,167 @@
+use std::io::Read;
+
+fn get_word() -> String {
+    let stdin = std::io::stdin();
+    let mut stdin=stdin.lock();
+    let mut u8b: [u8; 1] = [0];
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(16);
+        loop {
+            let res = stdin.read(&mut u8b);
+            if res.unwrap_or(0) == 0 || u8b[0] <= b' ' {
+                break;
+            } else {
+                buf.push(u8b[0]);
+            }
+        }
+        if buf.len() >= 1 {
+            let ret = String::from_utf8(buf).unwrap();
+            return ret;
+        }
+    }
+}
+
+fn get<T: std::str::FromStr>() -> T { get_word().parse().ok().unwrap() }
+
+/// Verified by https://atcoder.jp/contests/abc198/submissions/21774342
+mod mod_int {
+    use std::ops::*;
+    pub trait Mod: Copy { fn m() -> i64; }
+    #[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct ModInt<M> { pub x: i64, phantom: ::std::marker::PhantomData<M> }
+    impl<M: Mod> ModInt<M> {
+        // x >= 0
+        pub fn new(x: i64) -> Self { ModInt::new_internal(x % M::m()) }
+        fn new_internal(x: i64) -> Self {
+            ModInt { x: x, phantom: ::std::marker::PhantomData }
+        }
+        pub fn pow(self, mut e: i64) -> Self {
+            debug_assert!(e >= 0);
+            let mut sum = ModInt::new_internal(1);
+            let mut cur = self;
+            while e > 0 {
+                if e % 2 != 0 { sum *= cur; }
+                cur *= cur;
+                e /= 2;
+            }
+            sum
+        }
+        #[allow(dead_code)]
+        pub fn inv(self) -> Self { self.pow(M::m() - 2) }
+    }
+    impl<M: Mod> Default for ModInt<M> {
+        fn default() -> Self { Self::new_internal(0) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Add<T> for ModInt<M> {
+        type Output = Self;
+        fn add(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x + other.x;
+            if sum >= M::m() { sum -= M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Sub<T> for ModInt<M> {
+        type Output = Self;
+        fn sub(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x - other.x;
+            if sum < 0 { sum += M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Mul<T> for ModInt<M> {
+        type Output = Self;
+        fn mul(self, other: T) -> Self { ModInt::new(self.x * other.into().x % M::m()) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> AddAssign<T> for ModInt<M> {
+        fn add_assign(&mut self, other: T) { *self = *self + other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> SubAssign<T> for ModInt<M> {
+        fn sub_assign(&mut self, other: T) { *self = *self - other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> MulAssign<T> for ModInt<M> {
+        fn mul_assign(&mut self, other: T) { *self = *self * other; }
+    }
+    impl<M: Mod> Neg for ModInt<M> {
+        type Output = Self;
+        fn neg(self) -> Self { ModInt::new(0) - self }
+    }
+    impl<M> ::std::fmt::Display for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            self.x.fmt(f)
+        }
+    }
+    impl<M: Mod> From<i64> for ModInt<M> {
+        fn from(x: i64) -> Self { Self::new(x) }
+    }
+} // mod mod_int
+
+macro_rules! define_mod {
+    ($struct_name: ident, $modulo: expr) => {
+        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        pub struct $struct_name {}
+        impl mod_int::Mod for $struct_name { fn m() -> i64 { $modulo } }
+    }
+}
+const MOD: i64 = 998_244_353;
+define_mod!(P, MOD);
+type MInt = mod_int::ModInt<P>;
+
+// Depends on MInt.rs
+fn fact_init(w: usize) -> (Vec<MInt>, Vec<MInt>) {
+    let mut fac = vec![MInt::new(1); w];
+    let mut invfac = vec![0.into(); w];
+    for i in 1..w {
+        fac[i] = fac[i - 1] * i as i64;
+    }
+    invfac[w - 1] = fac[w - 1].inv();
+    for i in (0..w - 1).rev() {
+        invfac[i] = invfac[i + 1] * (i as i64 + 1);
+    }
+    (fac, invfac)
+}
+
+fn calc(n: i64, a: i64, fac: &[MInt], invfac: &[MInt]) -> MInt {
+    if a.abs() > n || (n + a) % 2 != 0 {
+        return 0.into();
+    }
+    let n = n as usize;
+    let a = a.abs() as usize;
+    fac[n] * invfac[(n + a) / 2] * invfac[(n - a) / 2]
+}
+
+// https://yukicoder.me/problems/no/2406 (3.5)
+// a = x + y, b = x - y とすると、操作では a や b が**独立に** 1 増えたり減ったりする。
+// x^2 - y^2 = ab = M となる a, b それぞれに対して確率を求めれば良い。
+// その確率は f(a, N)f(b, N) である。
+// ここで f(s, N) = |s| <= N && (s + N) % 2 == 0 ? C(N, (N-s)/2)/2^N : 0 である。
+// -> WA。 M = 0 のときの a, b の列挙に問題があった。0 の約数列挙を行うことはできない。
+fn main() {
+    let n: i64 = get();
+    let m: i64 = get();
+    let (fac, invfac) = fact_init(n as usize + 1);
+    let p2 = MInt::new(2).inv().pow(n as i64);
+    if m == 0 {
+        let mut ans = MInt::new(0);
+        let tmp = calc(n, 0, &fac, &invfac) * p2;
+        ans += tmp * 2 - tmp * tmp;
+        println!("{}", ans);
+        return;
+    }
+    let mut pairs = vec![];
+    for d in 1..1_000_001 {
+        if m % d == 0 {
+            pairs.push((d, m / d));
+            pairs.push((m / d, d));
+            pairs.push((-d, -m / d));
+            pairs.push((-m / d, -d));
+        }
+    }
+    pairs.sort(); pairs.dedup();
+    let mut ans = MInt::new(0);
+    for (a, b) in pairs {
+        ans += calc(n, a, &fac, &invfac) * calc(n, b, &fac, &invfac) * p2 * p2;
+    }
+    println!("{}", ans);
+}
