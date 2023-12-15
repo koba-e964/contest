917cec9e3f6933f7a977973670d891f4bf387c16
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2023-12-15 11:57:32 +0900

    Add yukicoder/2578.rs yukicoder/2586.rs

---
+++ yukicoder/2578.rs
@@ -0,0 +1,322 @@
+use std::io::Read;
+
+fn get_word() -> String {
+    let stdin = std::io::stdin();
+    let mut stdin=stdin.lock();
+    let mut u8b: [u8; 1] = [0];
+    loop {
+        let mut buf: Vec<u8> = Vec::with_capacity(16);
+        loop {
+            let res = stdin.read(&mut u8b);
+            if res.unwrap_or(0) == 0 || u8b[0] <= b' ' {
+                break;
+            } else {
+                buf.push(u8b[0]);
+            }
+        }
+        if buf.len() >= 1 {
+            let ret = String::from_utf8(buf).unwrap();
+            return ret;
+        }
+    }
+}
+
+fn get<T: std::str::FromStr>() -> T { get_word().parse().ok().unwrap() }
+
+/// Verified by https://atcoder.jp/contests/abc198/submissions/21774342
+mod mod_int {
+    use std::ops::*;
+    pub trait Mod: Copy { fn m() -> i64; }
+    #[derive(Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct ModInt<M> { pub x: i64, phantom: ::std::marker::PhantomData<M> }
+    impl<M: Mod> ModInt<M> {
+        // x >= 0
+        pub fn new(x: i64) -> Self { ModInt::new_internal(x % M::m()) }
+        fn new_internal(x: i64) -> Self {
+            ModInt { x: x, phantom: ::std::marker::PhantomData }
+        }
+        pub fn pow(self, mut e: i64) -> Self {
+            debug_assert!(e >= 0);
+            let mut sum = ModInt::new_internal(1);
+            let mut cur = self;
+            while e > 0 {
+                if e % 2 != 0 { sum *= cur; }
+                cur *= cur;
+                e /= 2;
+            }
+            sum
+        }
+        #[allow(dead_code)]
+        pub fn inv(self) -> Self { self.pow(M::m() - 2) }
+    }
+    impl<M: Mod> Default for ModInt<M> {
+        fn default() -> Self { Self::new_internal(0) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Add<T> for ModInt<M> {
+        type Output = Self;
+        fn add(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x + other.x;
+            if sum >= M::m() { sum -= M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Sub<T> for ModInt<M> {
+        type Output = Self;
+        fn sub(self, other: T) -> Self {
+            let other = other.into();
+            let mut sum = self.x - other.x;
+            if sum < 0 { sum += M::m(); }
+            ModInt::new_internal(sum)
+        }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> Mul<T> for ModInt<M> {
+        type Output = Self;
+        fn mul(self, other: T) -> Self { ModInt::new(self.x * other.into().x % M::m()) }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> AddAssign<T> for ModInt<M> {
+        fn add_assign(&mut self, other: T) { *self = *self + other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> SubAssign<T> for ModInt<M> {
+        fn sub_assign(&mut self, other: T) { *self = *self - other; }
+    }
+    impl<M: Mod, T: Into<ModInt<M>>> MulAssign<T> for ModInt<M> {
+        fn mul_assign(&mut self, other: T) { *self = *self * other; }
+    }
+    impl<M: Mod> Neg for ModInt<M> {
+        type Output = Self;
+        fn neg(self) -> Self { ModInt::new(0) - self }
+    }
+    impl<M> ::std::fmt::Display for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            self.x.fmt(f)
+        }
+    }
+    impl<M: Mod> ::std::fmt::Debug for ModInt<M> {
+        fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+            let (mut a, mut b, _) = red(self.x, M::m());
+            if b < 0 {
+                a = -a;
+                b = -b;
+            }
+            write!(f, "{}/{}", a, b)
+        }
+    }
+    impl<M: Mod> From<i64> for ModInt<M> {
+        fn from(x: i64) -> Self { Self::new(x) }
+    }
+    // Finds the simplest fraction x/y congruent to r mod p.
+    // The return value (x, y, z) satisfies x = y * r + z * p.
+    fn red(r: i64, p: i64) -> (i64, i64, i64) {
+        if r.abs() <= 10000 {
+            return (r, 1, 0);
+        }
+        let mut nxt_r = p % r;
+        let mut q = p / r;
+        if 2 * nxt_r >= r {
+            nxt_r -= r;
+            q += 1;
+        }
+        if 2 * nxt_r <= -r {
+            nxt_r += r;
+            q -= 1;
+        }
+        let (x, z, y) = red(nxt_r, r);
+        (x, y - q * z, z)
+    }
+} // mod mod_int
+
+macro_rules! define_mod {
+    ($struct_name: ident, $modulo: expr) => {
+        #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        pub struct $struct_name {}
+        impl mod_int::Mod for $struct_name { fn m() -> i64 { $modulo } }
+    }
+}
+const MOD: i64 = 998_244_353;
+define_mod!(P, MOD);
+type MInt = mod_int::ModInt<P>;
+
+// https://judge.yosupo.jp/submission/5155
+mod pollard_rho {
+    /// binary gcd
+    pub fn gcd(mut x: i64, mut y: i64) -> i64 {
+        if y == 0 { return x; }
+        if x == 0 { return y; }
+        let k = (x | y).trailing_zeros();
+        y >>= k;
+        x >>= x.trailing_zeros();
+        while y != 0 {
+            y >>= y.trailing_zeros();
+            if x > y { let t = x; x = y; y = t; }
+            y -= x;
+        }
+        x << k
+    }
+
+    fn add_mod(x: i64, y: i64, n: i64) -> i64 {
+        let z = x + y;
+        if z >= n { z - n } else { z }
+    }
+
+    fn mul_mod(x: i64, mut y: i64, n: i64) -> i64 {
+        assert!(x >= 0);
+        assert!(x < n);
+        let mut sum = 0;
+        let mut cur = x;
+        while y > 0 {
+            if (y & 1) == 1 { sum = add_mod(sum, cur, n); }
+            cur = add_mod(cur, cur, n);
+            y >>= 1;
+        }
+        sum
+    }
+
+    fn mod_pow(x: i64, mut e: i64, n: i64) -> i64 {
+        let mut prod = if n == 1 { 0 } else { 1 };
+        let mut cur = x % n;
+        while e > 0 {
+            if (e & 1) == 1 { prod = mul_mod(prod, cur, n); }
+            e >>= 1;
+            if e > 0 { cur = mul_mod(cur, cur, n); }
+        }
+        prod
+    }
+
+    pub fn is_prime(n: i64) -> bool {
+        if n <= 1 { return false; }
+        let small = [2, 3, 5, 7, 11, 13];
+        if small.iter().any(|&u| u == n) { return true; }
+        if small.iter().any(|&u| n % u == 0) { return false; }
+        let mut d = n - 1;
+        let e = d.trailing_zeros();
+        d >>= e;
+        // https://miller-rabin.appspot.com/
+        let a = [2, 325, 9375, 28178, 450775, 9780504, 1795265022];
+        a.iter().all(|&a| {
+            if a % n == 0 { return true; }
+            let mut x = mod_pow(a, d, n);
+            if x == 1 { return true; }
+            for _ in 0..e {
+                if x == n - 1 {
+                    return true;
+                }
+                x = mul_mod(x, x, n);
+                if x == 1 { return false; }
+            }
+            x == 1
+        })
+    }
+
+    fn pollard_rho(n: i64, c: &mut i64) -> i64 {
+        // An improvement with Brent's cycle detection algorithm is performed.
+        // https://maths-people.anu.edu.au/~brent/pub/pub051.html
+        if n % 2 == 0 { return 2; }
+        loop {
+            let mut x: i64; // tortoise
+            let mut y = 2; // hare
+            let mut d = 1;
+            let cc = *c;
+            let f = |i| add_mod(mul_mod(i, i, n), cc, n);
+            let mut r = 1;
+            // We don't perform the gcd-once-in-a-while optimization
+            // because the plain gcd-every-time algorithm appears to
+            // outperform, at least on judge.yosupo.jp :)
+            while d == 1 {
+                x = y;
+                for _ in 0..r {
+                    y = f(y);
+                    d = gcd((x - y).abs(), n);
+                    if d != 1 { break; }
+                }
+                r *= 2;
+            }
+            if d == n {
+                *c += 1;
+                continue;
+            }
+            return d;
+        }
+    }
+
+    /// Outputs (p, e) in p's ascending order.
+    pub fn factorize(x: i64) -> Vec<(i64, usize)> {
+        if x <= 1 { return vec![]; }
+        let mut hm = std::collections::HashMap::new();
+        let mut pool = vec![x];
+        let mut c = 1;
+        while let Some(u) = pool.pop() {
+            if is_prime(u) {
+                *hm.entry(u).or_insert(0) += 1;
+                continue;
+            }
+            let p = pollard_rho(u, &mut c);
+            pool.push(p);
+            pool.push(u / p);
+        }
+        let mut v: Vec<_> = hm.into_iter().collect();
+        v.sort();
+        v
+    }
+} // mod pollard_rho
+
+// https://yukicoder.me/problems/no/2578 (4)
+// Solved with hints
+// m <= 10^18 なので m の約数は 10^5 個程度あるので、約数をキーに持つ単純な DP だとうまくいかない。
+// g(t) := \sum{lcm | t} \prod W とすると、g(t) = \prod{a_i | t} (W_i + 1) である。(足すものが乗法的であるため分配法則が使える。)
+// {m の約数} のように線型束の直積になっている束について、包除原理は (1 -1 0 0 ...) の直積である。
+// つまり、 f(m) = \sum_{u|m, u is squarefree} (-1)^{#primes(u)} g(t/u) である。
+// これによって、2^(m の素因数の個数) で手間を抑えられるので、高速に計算できる。
+// https://gist.github.com/koba-e964/2de3a6480749241f424c4e110a440503 によれば m の素因数の個数は 15 個以下であるため、2^15 * n 程度の手間である。
+// -> TLE。高速ゼータ変換を使って (m の素因数の個数)n にする必要があった。
+// Tags: product-of-lattices, inclusion-exclusion-principle, zeta-transformation
+fn main() {
+    let t: usize = get();
+    let m: i64 = get();
+    let pe = pollard_rho::factorize(m);
+    let l = pe.len();
+    for _ in 0..t {
+        let n: usize = get();
+        let b: i64 = get();
+        let c: i64 = get();
+        let d: i64 = get();
+        let a: Vec<i64> = (0..n).map(|_| get()).collect();
+        let mut w = vec![MInt::new(b); n];
+        for i in 1..n {
+            w[i] = w[i - 1] * c + d;
+        }
+        let mut dp = vec![MInt::new(1); 1 << l];
+        for i in 0..n {
+            if m % a[i] != 0 {
+                continue;
+            }
+            let rem = m / a[i];
+            let mut bits = 0;
+            for j in 0..l {
+                if rem % pe[j].0 == 0 {
+                    bits |= 1 << j;
+                }
+            }
+            dp[bits] *= w[i] + 1;
+        }
+        for i in 0..l {
+            for bits in 0usize..1 << l {
+                if (bits & 1 << i) == 0 {
+                    dp[bits] = dp[bits] * dp[bits ^ 1 << i];
+                }
+            }
+        }
+        let mut tot = MInt::new(0);
+        for bits in 0..1usize << l {
+            if bits.count_ones() % 2 == 0 {
+                tot += dp[bits];
+            } else {
+                tot -= dp[bits];
+            }
+        }
+        if m == 1 {
+            tot -= 1; // exclude the empty set
+        }
+        println!("{}", tot);
+    }
+}
