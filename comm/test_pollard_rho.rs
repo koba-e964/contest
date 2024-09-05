d8f887bd220fbb109a740f81f3027f95fc4cad91
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2024-09-05 19:10:26 +0900

    Add testcases to test_pollard_rho.rs

--- comm/test_pollard_rho.rs
+++ comm/test_pollard_rho.rs
@@ -3,6 +3,11 @@ include!("pollard_rho.rs");
 fn main() {
     use pollard_rho::*;
     assert_eq!(factorize(4681), vec![(31, 1), (151, 1)]);
+    // https://lpha-z.hatenablog.com/entry/2023/01/15/231500
+    assert_eq!(factorize(124376107291), vec![(352523, 1), (352817, 1)]);
+    assert_eq!(factorize(273772559), vec![(15881, 1), (17239, 1)]);
+    assert_eq!(factorize(2059), vec![(29, 1), (71, 1)]);
+    assert_eq!(factorize(385515865499), vec![(599477, 1), (643087, 1)]);
 
     // Performs Eratosthenes sieve
     const W: usize = 1_000_000;
