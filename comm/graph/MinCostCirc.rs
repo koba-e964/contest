a86f23c8b55eeea3789129dfc588b42c3d76a9fd
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2025-10-11 14:36:19 +0900

    Add atcoder/practice2/e-alt2.rs

--- comm/graph/MinCostCirc.rs
+++ comm/graph/MinCostCirc.rs
@@ -4,7 +4,7 @@
 // ref: https://people.orie.cornell.edu/dpw/orie633/LectureNotes/lecture11.pdf
 // Depends on: graph/MinCostFlow.rs
 // Verified by:
-// - https://atcoder.jp/contests/practice2/submissions/70013398
+// - https://atcoder.jp/contests/practice2/submissions/70013984
 pub struct MinCostCirc {
     mcf: MinCostFlow,
     sup: Vec<Cap>,
@@ -24,13 +24,33 @@ impl MinCostCirc {
     }
     pub fn add_edge(&mut self, a: usize, b: usize, (dem, cap): (Cap, Cap), cost: Cost) {
         assert!(dem <= cap);
+        if cost >= 0 {
+            self.add_edge_inner(a, b, (dem, cap), cost, false);
+        } else {
+            self.add_edge_inner(b, a, (-cap, -dem), -cost, true);
+        }
+    }
+    fn add_edge_inner(
+        &mut self, a: usize, b: usize,
+        (dem, cap): (Cap, Cap), cost: Cost, flipped: bool,
+    ) {
+        assert!(dem <= cap);
         assert!(cost >= 0);
         self.cost += dem as Cost * cost;
-        let index = self.mcf.graph[2 + a].len();
+        let index = self.mcf.graph[2 + if flipped { b } else { a }].len();
         self.mcf.add_edge(2 + a, 2 + b, cap - dem, cost);
         self.sup[b] += dem;
         self.sup[a] -= dem;
-        self.edges.push((a, index, cap));
+        if flipped {
+            // Let e' = (a -> b) and e = (b -> a). (Note that the original edge was b -> a.)
+            // If after min_cost() e.cap = x holds,
+            // the actual flow on the edge e is cap - x = cap - e.cap
+            self.edges.push((b, index, -dem));
+        } else {
+            // If after min_cost() e.cap = cap - dem - x holds,
+            // the actual flow is dem + x = cap - e.cap = 
+            self.edges.push((a, index, cap));
+        }
     }
     pub fn min_cost(&mut self) -> Option<Cost> {
         let s: Cap = self.sup.iter().sum();
@@ -62,8 +82,9 @@ impl MinCostCirc {
         let mut ans = vec![];
         for &(i, j, cap) in &self.edges {
             let e = &self.mcf.graph[2 + i][j];
-            if e.cap != cap && e.to >= 2 {
-                ans.push((i, e.to - 2, cap - e.cap));
+            let amount = cap - e.cap;
+            if amount != 0 && e.to >= 2 {
+                ans.push((i, e.to - 2, amount));
             }
         }
         ans
